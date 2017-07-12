f0dab0abb70c9b56f56400d6c8bfed0f233588b1
Romain WEEGER <romain.weeger@gmail.com>
2017-07-12 07:00:24 +0200

    Try to update deployment...

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Install git.
-apt-get install git -yqq
-
-# Add git repo.
-git remote remove github
-git remote add github git@github.com:wexample/scripts.git
-
 # Add SSH and prevent host checking.
 apt-get install openssh-client -yqq
 mkdir -p ~/.ssh
@@ -16,5 +9,13 @@ eval $(ssh-agent -s)
 # Add ssh user.
 ssh-add <(echo "$STAGING_PRIVATE_KEY")
 
+# Install git.
+apt-get install git -yqq
+
+# Add git repo.
+git remote remove github
+git remote add github git@github.com:wexample/scripts.git
+
 # Push on git repo.
+git add -A
 git push -u github master
