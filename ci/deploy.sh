2510140852027894c403e215241055aaa6bbe6fa
Romain WEEGER <romain.weeger@gmail.com>
2017-07-12 06:26:02 +0200

    Try to deploy on github

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+## Install global packages
+apt-get update -yqq
+
+
+
+
+
 # Install git.
 apt-get install git -yqq
 
