3e0dd2a623bd462c62548c6753cc2f035851d81e
Romain WEEGER <romain.weeger@gmail.com>
2017-07-12 07:17:47 +0200

    Try to update deployment...

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -12,10 +12,6 @@ ssh-add <(echo "$STAGING_PRIVATE_KEY")
 # Install git.
 apt-get install git -yqq
 
-# Add git repo.
-git remote remove github
-git remote add github git@github.com:wexample/scripts.git
-
 # Push on git repo.
 git add -A
 git push -u github master
