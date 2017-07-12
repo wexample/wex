f2768aa3f3deefcc6be4124be7bd7723d5e7953b
Romain WEEGER <romain.weeger@gmail.com>
2017-07-12 06:18:07 +0200

    Try to deploy on github

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,5 +7,8 @@ apt-get install git -yqq
 git remote remove github
 git remote add github git@github.com:wexample/scripts.git
 
+# Add host.
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
 # Push on git repo.
 git push -u github master
