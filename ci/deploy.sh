9dbc8693ac6cadd06240d90a630aaeb3520631df
Romain WEEGER <romain.weeger@gmail.com>
2017-07-30 22:06:35 +0200

    Debug deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -10,8 +10,9 @@ w=wexample.sh
 curl https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/$w | tr -d '\015' > $w
 
 echo "   > Load sshRemoveHostChecking.sh"
-# Remove warning on new SSH host.
-bash $w -s=sshRemoveHostChecking -rm
+mkdir -p ~/.ssh
+  eval $(ssh-agent -s)
+  [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
 echo "   > ssh-add"
 # Add ssh user.
@@ -21,15 +22,5 @@ echo "   > install git"
 # Install git.
 apt-get install git -yqq
 
-# Add git repo.
-git remote remove github
-git remote add github git@github.com:wexample/scripts.git
-# Remove branch if exists.
-git branch -d temp
-# Use temp branch to attach head.
-git branch temp
-git checkout temp
-git branch -f master temp
-git checkout master
-# Push on git repo.
-git push github master
+# Remove warning on new SSH host.
+bash $w -s=gitlabDeployGithub -rm
