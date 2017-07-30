5c3863f6550ad3f9a6c0c3f400a44e7c7000546f
Romain WEEGER <romain.weeger@gmail.com>
2017-07-30 21:49:13 +0200

    deploy.sh update

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-# Add SSH and prevent host checking.
-apt-get install openssh-client -yqq
+# Install required packages
+apt-get install openssh-client git curl -yqq
 
-mkdir -p ~/.ssh
-eval $(ssh-agent -s)
-[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+# Same script for multiple sub scripts
+w=wexample.sh
+curl https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/$w | tr -d '\015' > $w
+
+# Remove warning on new SSH host.
+bash $w -s=sshRemoveHostChecking -rm
 
 # Add ssh user.
 ssh-add <(echo "$STAGING_PRIVATE_KEY")
 
-# Install git.
-apt-get install git -yqq
+# Deploy on Github.
+bash $w -s=gitlabDeployGithub -a="git@github.com:wexample/scripts.git" -rm
 
-# Add git repo.
-git remote remove github
-git remote add github git@github.com:wexample/scripts.git
-# Remove branch if exists.
-git branch -d temp
-# Use temp branch to attach head.
-git branch temp
-git checkout temp
-git branch -f master temp
-git checkout master
-# Push on git repo.
-git push github master
+# Remove wexample.sh
+rm -rf $w
