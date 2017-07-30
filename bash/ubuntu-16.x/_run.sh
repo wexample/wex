015fa09d8f01f31b4dce923e2592e40a327b0f68
Romain WEEGER <romain.weeger@gmail.com>
2017-07-30 17:29:51 +0200

    Debug script loading

--- bash/ubuntu-16.x/_run.sh
+++ bash/ubuntu-16.x/_run.sh
@@ -2,4 +2,4 @@
 
 # This method is used to execute any wexample script
 # from an environment without wexample scripts installed.
-s=helloWorld && w=wexample.sh && curl https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/$w | tr -d '\015' > $w && bash $w -s=$s -i && rm -rf $w
+s=helloWorld && w=wexample.sh && curl https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/$w | tr -d '\015' > $w && bash $w -s=$s -i -rm && rm -rf $w
