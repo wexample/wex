29ee0d8163e108f897a7c08abfb5dfed700f200e
Romain WEEGER <romain.weeger@gmail.com>
2017-07-30 22:00:16 +0200

    Test log

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,16 +3,20 @@
 # Add SSH and prevent host checking.
 apt-get install openssh-client -yqq
 
+echo "   > Load wexample.sh"
 # Wexample loader for multiple scripts.
 w=wexample.sh
 curl https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/$w | tr -d '\015' > $w
 
+echo "   > Load sshRemoveHostChecking.sh"
 # Remove warning on new SSH host.
 bash $w -s=sshRemoveHostChecking -rm
 
+echo "   > ssh-add"
 # Add ssh user.
 ssh-add <(echo "$STAGING_PRIVATE_KEY")
 
+echo "   > install git"
 # Install git.
 apt-get install git -yqq
 
