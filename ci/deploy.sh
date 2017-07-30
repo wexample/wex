ff4c81597f4014e0b36c256ac5d4206afde227f4
Romain WEEGER <romain.weeger@gmail.com>
2017-07-30 22:40:21 +0200

    Remove comments

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,19 +4,17 @@
 apt-get install openssh-client -yqq
 apt-get install curl -yqq
 
-echo "   > Load wexample.sh"
 # Wexample loader for multiple scripts.
 w=wexample.sh
 curl https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/$w | tr -d '\015' > $w
 
-echo "   > Load sshRemoveHostChecking.sh"
-bash $w -s=sshRemoveHostChecking -rm
+mkdir -p ~/.ssh
+eval $(ssh-agent -s)
+[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
-echo "   > ssh-add"
 # Add ssh user.
 ssh-add <(echo "$STAGING_PRIVATE_KEY")
 
-echo "   > install git"
 # Install git.
 apt-get install git -yqq
 
