f0c715f69daa233130e45dcd3acca1d82ffc0828
Romain Weeger <romain.weeger@gmail.com>
2017-07-31 10:52:01 +0200

    Try to make more stuffs

--- bash/ubuntu-16.x/gitlabDeployGithub.sh
+++ bash/ubuntu-16.x/gitlabDeployGithub.sh
@@ -2,6 +2,19 @@
 
 gitlabDeployGithub() {
   destRepoUrl=${1}
+  
+  # Add SSH and prevent host checking.
+  apt-get install openssh-client -yqq
+
+  mkdir -p ~/.ssh
+  eval $(ssh-agent -s)
+  [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+
+  # Add ssh user.
+  ssh-add <(echo "$STAGING_PRIVATE_KEY")
+
+  # Install git.
+  apt-get install git -yqq
 
   # Add git repo.
   echo "Remove repo (old)"
