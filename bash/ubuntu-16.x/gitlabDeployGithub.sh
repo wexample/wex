3692a7ccc148aedb0b6734c3dea41ff0ccbadbb8
Romain WEEGER <romain.weeger@gmail.com>
2017-07-31 10:37:48 +0200

    Various comments

--- bash/ubuntu-16.x/gitlabDeployGithub.sh
+++ bash/ubuntu-16.x/gitlabDeployGithub.sh
@@ -4,17 +4,22 @@ gitlabDeployGithub() {
   destRepoUrl=${1}
 
   # Add git repo.
+  echo "Remove repo (old)"
   git remote remove github
   git remote add github "${destRepoUrl}"
+  echo "Create temp branch and checkout"
   # Remove branch if exists.
   git branch -d temp
   # Use temp branch to attach head.
   git branch temp
   git checkout temp
+  echo "Merge with master"
   git branch -f master temp
+  echo "Go to master"
   git checkout master
-  # Get last updates on git repo.
-  git pull github master
   # Push on git repo.
   git push github master
+  echo "Remove repo"
+  git remote remove github
+  git remote add github "${destRepoUrl}"
 }
