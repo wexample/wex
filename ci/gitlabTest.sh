dbe9298708a33ec432e273a5960881ffbaccd1f2
Romain WEEGER <romain.weeger@gmail.com>
2017-07-31 10:12:30 +0200

    Merge files

--- ci/gitlabTest.sh
+++ ci/gitlabTest.sh
@@ -14,3 +14,23 @@ curl https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/
 
 # Remove warning on new SSH host.
 bash $w -s=gitlabInstallComposer -rm
+
+# Run tests.
+bash bash/ubuntu-16.x/tests/_run.sh
+
+# Deploy to GitHub
+# Add SSH and prevent host checking.
+apt-get install openssh-client -yqq
+
+mkdir -p ~/.ssh
+eval $(ssh-agent -s)
+[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+
+# Add ssh user.
+ssh-add <(echo "$STAGING_PRIVATE_KEY")
+
+# Install git.
+apt-get install git -yqq
+
+# Remove warning on new SSH host.
+bash $w -s=gitlabDeployGithub -a="git@github.com:wexample/scripts.git" -rm
