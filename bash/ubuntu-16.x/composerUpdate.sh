bbc25b99c594fce721f2bdfa4135fa0e483698a0
Romain WEEGER <romain.weeger@gmail.com>
2017-08-03 15:11:37 +0200

    Debug

--- bash/ubuntu-16.x/composerUpdate.sh
+++ bash/ubuntu-16.x/composerUpdate.sh
@@ -3,6 +3,6 @@
 # Should be ran inside a composer project.
 composerUpdate() {
   # Install all project dependencies
-  php composer.phar clear-cache -q
-  php composer.phar update -q
+  composer clear-cache -q
+  composer update -q
 }
