938a031982c08609326285b6d03732e6b05c0f15
Romain WEEGER <romain.weeger@gmail.com>
2017-08-03 13:18:55 +0200

    Include composer into php7 image

--- ci/gitlabTest.sh
+++ ci/gitlabTest.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# TODO We may use different images for each tests (drush && drupal / composer && Sympfony / etc...)
+
 # Wexample loader for multiple scripts.
 w=wexample.sh
 curl -sS https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/$w | tr -d '\015' > $w
 
 # Remove warning on new SSH host.
 bash $w -s=gitlabInit -rm
-bash $w -s=composerInstall -rm
 
 # Run tests.
 bash bash/ubuntu-16.x/tests/_run.sh
