c2f2817775d8a8b937d824ae9ae92674f3a8055f
Romain WEEGER <romain.weeger@gmail.com>
2017-08-04 12:23:08 +0200

    Linux global installer v1

---
+++ bash/ubuntu-16.x/wexampleInstall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+wexampleInstall() {
+  cd /opt
+  # Create dir
+  mkdir wexample
+  # Get whole repository.
+  git clone ${WEX_URL_GITHUB}scripts.git .
+  # Add to path
+  bash/ubuntu-16.x/wexample.sh fileTestAppend ~/.bashrc export PATH=/opt/wexample/bash/ubuntu-16.x/
+}
