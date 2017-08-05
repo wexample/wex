6923b54db82266d6dd6e4e0bd1c2294cd84246c8
Romain WEEGER <romain.weeger@gmail.com>
2017-08-05 15:07:09 +0200

    debug install

--- bash/ubuntu-16.x/wexampleVersion.sh
+++ bash/ubuntu-16.x/wexampleVersion.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 wexampleVersion() {
-  echo '1.'$(git rev-list --all --count --git-dir=${WEX_LOCAL_DIR}.git --work-tree=${WEX_LOCAL_DIR})
+  cd ${WEX_LOCAL_DIR}
+  echo '1.'$(git rev-list --all --count)
 }
