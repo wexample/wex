508505975bad262cdadfd5516baf2b6678274f53
Romain WEEGER <romain.weeger@gmail.com>
2017-08-05 15:47:23 +0200

    Update docker

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker build -t wexample/wexubuntu16:latest wexubuntu16
-docker build -t wexample/wexwebserver:latest wexwebserver
-docker build -t wexample/wexphp7:latest wexphp7
+docker build -t wexample/wexubuntu16:latest wexubuntu16 --no-cache
+docker build -t wexample/wexwebserver:latest wexwebserver --no-cache
+docker build -t wexample/wexphp7:latest wexphp7 --no-cache
 
 docker login
 
