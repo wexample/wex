9f1db335a35d4e0091ecaf4ce198a8c9674b0275
Romain WEEGER <romain.weeger@gmail.com>
2017-08-05 14:44:35 +0200

    debug install

--- bash/ubuntu-16.x/bashAddToPath.sh
+++ bash/ubuntu-16.x/bashAddToPath.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 bashAddToPath() {
-  # Add now.
-  export PATH=$PATH:"${1}"
-  # Add to path.
-  bash ${WEX_DIR_ROOT}'wexample.sh' fileTextAppend ~/.bashrc 'export PATH=$PATH:'${1}
+  if [[ ":$PATH:" != *":${1}"* ]]; then
+    # Add now.
+    export PATH=$PATH:"${1}"
+    # Add to path.
+    bash ${WEX_DIR_ROOT}'wexample.sh' fileTextAppend ~/.bashrc 'export PATH=$PATH:'${1}
+  fi
 }
