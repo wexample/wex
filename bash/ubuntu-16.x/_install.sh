947154aab18660e98145203c3f552d128fe44c69
Romain WEEGER <romain.weeger@gmail.com>
2017-08-05 14:37:47 +0200

    debug install

--- bash/ubuntu-16.x/_install.sh
+++ bash/ubuntu-16.x/_install.sh
@@ -3,6 +3,8 @@
 WEX_DIR_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 WEX_URL_GITHUB="https://github.com/wexample/"
 WEX_URL_SCRIPTS="https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/"
+WEX_LOCAL_DIR="/opt/wexample/"
+WEX_SCRIPTS_DIR=${WEX_LOCAL_DIR}"bash/ubuntu-16.x/"
 
 cd /opt
 # Remove if exists.
--- bash/ubuntu-16.x/_variables.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-WEX_URL_GITHUB="https://github.com/wexample/"
-WEX_URL_SCRIPTS="https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/"
-WEX_LOCAL_DIR="/opt/wexample/"
-WEX_SCRIPTS_DIR=${WEX_LOCAL_DIR}"bash/ubuntu-16.x/"
