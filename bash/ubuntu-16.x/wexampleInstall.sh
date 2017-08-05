0d7575260739ec122be1e7f990bc562f9204a6e6
Romain Weeger <romain.weeger@gmail.com>
2017-08-05 10:11:35 +0200

    Update wexampleInstall.sh

--- bash/ubuntu-16.x/wexampleInstall.sh
+++ bash/ubuntu-16.x/wexampleInstall.sh
@@ -7,5 +7,5 @@ wexampleInstall() {
   # Get whole repository.
   git clone ${WEX_URL_GITHUB}scripts.git wexample
   # Add to path
-  bash wexample/bash/ubuntu-16.x/wexample.sh fileTextAppend ~/.bashrc export PATH=/opt/wexample/bash/ubuntu-16.x/
+  bash wexample/bash/ubuntu-16.x/wexample.sh fileTextAppend ~/.bashrc "export PATH=/opt/wexample/bash/ubuntu-16.x/"
 }
