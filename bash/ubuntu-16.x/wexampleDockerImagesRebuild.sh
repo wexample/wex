d653ee7261bbc38f7c598f1f49780418c2e6388f
Romain WEEGER <romain.weeger@gmail.com>
2017-08-09 08:25:01 +0200

    Deploy && apache mysql conf

--- bash/ubuntu-16.x/wexampleDockerImagesRebuild.sh
+++ bash/ubuntu-16.x/wexampleDockerImagesRebuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wexampleDockerImagesRebuild() {
-  docker build -t wexample/wexubuntu16:latest docker/wexubuntu16
-  docker build -t wexample/wexwebserver:latest docker/wexwebserver
-  docker build -t wexample/wexphp7:latest docker/wexphp7
+  docker build -t wexample/wexubuntu16:latest wexubuntu16
+  docker build -t wexample/wexwebserver:latest wexwebserver
+  docker build -t wexample/wexphp7:latest wexphp7
 }
--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+. ../bash/ubuntu-16.x/wexample.sh
+
 wexample wexampleDockerImagesRebuild
 
-wexxample wexampleDockerImagesDeploy
-
+wexample wexampleDockerImagesDeploy
