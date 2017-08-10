e58e05501f29fc58d6f8b97cc1f96bcca1b5d0a2
Romain WEEGER <romain.weeger@gmail.com>
2017-08-10 07:36:53 +0200

    WP debug

--- php/wordpress4SettingsToBash.php
+++ php/wordpress4SettingsToBash.php
@@ -3,7 +3,21 @@
 // Get path as first option.
 $settingFile = $argv[1];
 
+$dirname = dirname($settingFile);
+
+# wp-settings.php is required by wp-config.php
+# So prevent to load it, copy it.
+copy($dirname.'/wp-settings.php', $dirname.'/_wp-settings.php');
+# Add a commented temp file
+file_put_contents(
+  $dirname.'/wp-settings.php',
+  '<?php // This is a temp file used by wexample to read WP connexion data and prevent site launching.'
+);
+# Load config.
 require $settingFile;
+# Revert changes.
+copy($dirname.'/_wp-settings.php', $dirname.'/wp-settings.php');
+unlink($dirname.'/_wp-settings.php');
 
 echo('WEBSITE_SETTINGS_HOST="'.DB_HOST.'"; ');
 if (defined('DB_PORT')) {
