1807ce12ea64021e65655689f21fd8d6a746307d
Romain WEEGER <romain.weeger@gmail.com>
2017-08-12 18:27:21 +0200

    Improve wexample install

--- bash/ubuntu-16.x/bashAddToPath.sh
+++ bash/ubuntu-16.x/bashAddToPath.sh
@@ -3,7 +3,7 @@
 bashAddToPath() {
   if [[ ":$PATH:" != *":${1}"* ]]; then
     # Add now.
-    export PATH=$PATH:"${1}"
+    export PATH=$PATH":${1}"
     # Add to path.
     bash ${WEX_DIR_BASH_UBUNTU16}'wexample.sh' fileTextAppend ~/.bashrc 'export PATH=$PATH:'${1}
   fi
