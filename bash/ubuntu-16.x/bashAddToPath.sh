f685dbdba01264e82c698a0c7fef6cbaabcb74fe
Romain WEEGER <romain.weeger@gmail.com>
2017-08-13 10:29:11 +0200

    Install improvement

--- bash/ubuntu-16.x/bashAddToPath.sh
+++ bash/ubuntu-16.x/bashAddToPath.sh
@@ -2,9 +2,10 @@
 
 bashAddToPath() {
   if [[ ":$PATH:" != *":${1}"* ]]; then
+    command='export PATH="'$PATH:${1}'"'
     # Print command to add, need to execute it into global context.
-    echo $("export PATH=$PATH:${1}")
+    echo ${command}
     # Add to path.
-    bash ${WEX_DIR_BASH_UBUNTU16}'wexample.sh' fileTextAppendOnce ~/.bashrc 'export PATH=$PATH:'${1}
+    bash ${WEX_DIR_BASH_UBUNTU16}'wexample.sh' fileTextAppendOnce ~/.bashrc ${command}
   fi
 }
