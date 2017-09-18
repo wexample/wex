2428625d6513b4c55b841be30b3815b0c13d06de
Romain WEEGER <romain.weeger@gmail.com>
2017-09-18 12:55:47 +0200

    Improve up

--- bash/ubuntu-16.x/wexampleLocalUpdate.sh
+++ bash/ubuntu-16.x/wexampleLocalUpdate.sh
@@ -3,7 +3,13 @@
 wexampleLocalUpdate() {
   GIT_EXISTS=$(wexample gitExists)
   if [ ${GIT_EXISTS} == true ];then
+    # Go to wexample install dir.
     cd ${WEX_LOCAL_DIR}
+    # Override changes and pull.
+    git reset --hard
     git pull origin master
+    # Git all needed wrights.
+    chown root:root -R *
+    chmod 755 -r *
   fi
 }
