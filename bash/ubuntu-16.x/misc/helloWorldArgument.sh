2599da315e5a6c8450a0374f3265ba608520afee
Romain WEEGER <romain.weeger@gmail.com>
2017-09-22 03:20:52 +0200

    Args

--- bash/ubuntu-16.x/misc/helloWorldArgument.sh
+++ bash/ubuntu-16.x/misc/helloWorldArgument.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+miscHelloWorldArgumentArgs() {
+ _ARGUMENTS=(
+   [0]='name n "Name of user" true'
+   [1]='group g "Name of group" true'
+   [2]='bye b "Bye" false'
+ )
+}
+
 miscHelloWorldArgument() {
-  echo "Hello World! - ${1} - ${2}"
+  echo "Hello World! - You are ${NAME} from ${GROUP}"
+  if [ ! -z ${BYE+x} ]; then
+    echo "Bye!"
+  fi
 }
