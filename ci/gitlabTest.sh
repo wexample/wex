ed12ed103f74e09395de8b24cac37c1bbd2e10e7
Romain WEEGER <romain.weeger@gmail.com>
2017-09-25 10:08:02 +0200

    CI

--- ci/gitlabTest.sh
+++ ci/gitlabTest.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Wexample loader for multiple scripts.
-w=wexample.sh
-curl -sS https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/$w | tr -d '\015' > $w
-
-bash $w gitlab/init
-
-bash $w composer/update
-
 # Run tests.
 bash bash/ubuntu-16.x/tests/_run.sh
 
