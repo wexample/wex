506946291e3e56d29593aa947121b0aa3e2c5f9a
Romain WEEGER <romain.weeger@gmail.com>
2017-09-25 11:04:29 +0200

    CI

--- bash/ubuntu-16.x/_installLocal.sh
+++ bash/ubuntu-16.x/_installLocal.sh
@@ -7,6 +7,7 @@ WEX_SCRIPTS_DIR=${WEX_LOCAL_DIR}"bash/ubuntu-16.x/"
 chmod +x "${WEX_LOCAL_DIR}bash/wexample"
 # Add to PATH, will return global command to export var.
 # TODO temp
+ls -la
 echo bash ${WEX_SCRIPTS_DIR}"wexample/wexample.sh" system/pathAdd -p="${WEX_LOCAL_DIR}bash"
 pathCommand=$(bash ${WEX_SCRIPTS_DIR}"wexample/wexample.sh" system/pathAdd -p="${WEX_LOCAL_DIR}bash")
 # Add to global PATH.
