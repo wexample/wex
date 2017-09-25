6da7e751dc2be406d693886211bde4a32f53bc22
Romain WEEGER <romain.weeger@gmail.com>
2017-09-25 10:43:15 +0200

    CI

--- bash/ubuntu-16.x/_install.sh
+++ bash/ubuntu-16.x/_install.sh
@@ -17,10 +17,10 @@ git clone ${WEX_URL_GITHUB}scripts.git wexample
 # Add permission to execute
 chmod +x "${WEX_LOCAL_DIR}bash/wexample"
 # Add to PATH, will return global command to export var.
-pathCommand=$(bash ${WEX_SCRIPTS_DIR}"wexample.sh" systemPathAdd "${WEX_LOCAL_DIR}bash")
+pathCommand=$(bash ${WEX_SCRIPTS_DIR}"wexample/wexample.sh" system/pathAdd "${WEX_LOCAL_DIR}bash")
 # Add to global PATH.
 eval ${pathCommand}
 
 # Say Hi.
-echo "Wexample Script installed at vesion v"$(wexample wexampleVersion)
-wexample wexampleLogo
+echo "Wexample Script installed at vesion v"$(wex wexample/version)
+wex wexample/logo
