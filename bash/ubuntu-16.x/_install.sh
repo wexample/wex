051287342e46848d8eaab84feac770295b982c40
Romain WEEGER <romain.weeger@gmail.com>
2017-09-25 14:22:23 +0200

    install debug

--- bash/ubuntu-16.x/_install.sh
+++ bash/ubuntu-16.x/_install.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-WEX_DIR_BASH_UBUNTU16="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
-WEX_DIR_ROOT=${WEX_DIR_BASH_UBUNTU16}"../../"
+WEX_LOCAL_DIR="/opt/wexample/"
 WEX_URL_GITHUB="https://github.com/wexample/"
-WEX_URL_SCRIPTS="https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/"
 
 cd /opt
 # Remove if exists.
-rm -rf wexample
+rm -rf ${WEX_LOCAL_DIR}
 # Create dir.
-mkdir wexample
+mkdir ${WEX_LOCAL_DIR}
 # Get whole repository.
 git clone ${WEX_URL_GITHUB}scripts.git wexample
 # Install locally
-bash ${WEX_DIR_BASH_UBUNTU16}_installLocal.sh
+bash ${WEX_LOCAL_DIR}bash/ubuntu-16x/_installLocal.sh
