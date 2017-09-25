a02c3f8a3bb79c0feacdf39660ac884d35718126
Romain WEEGER <romain.weeger@gmail.com>
2017-09-25 13:03:25 +0200

    CI

--- ci/gitlabTest.sh
+++ ci/gitlabTest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#
-## Remove built in scripts
-#rm -rf /opt/wexample
-## Copy current (let a copy in place for ci tools)
-#cp -r /builds/wexample-public/scripts /opt/wexample
-## Go to
-#cd /opt/wexample
-#
+
+# Remove built in scripts
+rm -rf /opt/wexample
+# Copy current (let a copy in place for ci tools)
+cp -r /builds/wexample-public/scripts /opt/wexample
+# Go to
+cd /opt/wexample
+
 
 # Load wexample.
 bash ./bash/ubuntu-16.x/_installLocal.sh
