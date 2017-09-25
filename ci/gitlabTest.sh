878d352f5da254419be8ebb6dd99f355aacca11e
Romain WEEGER <romain.weeger@gmail.com>
2017-09-25 10:26:29 +0200

    CI

--- ci/gitlabTest.sh
+++ ci/gitlabTest.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Run tests.
-bash bash/ubuntu-16.x/_tests/_run.sh
-
 # Load wexample.
 bash bash/ubuntu-16.x/wexample/wexample.sh
+
+# Init gitlab
+wex gitlab/init
+
+# Install composer : we use some libraries in PHP scripts
+wex composer/pharInstall
+
+# Run all tests
+wexTest
 
 # Deploy to GitHub
 wex gitlab/deployGithub -r="git@github.com:wexample/scripts.git"
