e11a65e5aa3a3be65fc0409d04ae1a7f86844395
Romain WEEGER <romain.weeger@gmail.com>
2017-09-25 10:14:19 +0200

    CI

--- ci/gitlabTest.sh
+++ ci/gitlabTest.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-ls -la
-
 # Run tests.
-bash bash/ubuntu-16.x/tests/_run.sh
+bash bash/ubuntu-16.x/_tests/_run.sh
 
-wex=bash/ubuntu-16.x/wexample/wexample.sh
+# Load wexample.
+bash bash/ubuntu-16.x/wexample/wexample.sh
 
 # Deploy to GitHub
 wex gitlab/deployGithub -r="git@github.com:wexample/scripts.git"
