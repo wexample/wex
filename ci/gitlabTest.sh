7af4ba438c779256a93b6396ce48d6b8368ef0a3
Romain WEEGER <romain.weeger@gmail.com>
2017-09-25 13:01:53 +0200

    CI

--- ci/gitlabTest.sh
+++ ci/gitlabTest.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
+#
+## Remove built in scripts
+#rm -rf /opt/wexample
+## Copy current (let a copy in place for ci tools)
+#cp -r /builds/wexample-public/scripts /opt/wexample
+## Go to
+#cd /opt/wexample
+#
+## Load wexample.
+#bash ./bash/ubuntu-16.x/_installLocal.sh
+#
+## Init gitlab
+#wex gitlab/init
+#
+## Install composer : we use some libraries in PHP scripts
+#wex composer/pharInstall
+#
+## Run all tests
+##bash bash/ubuntu-16.x/_tests/_run.sh
+#
+#cd /builds/wexample-public/scripts
 
-# Remove built in scripts
-rm -rf /opt/wexample
-# Copy current (let a copy in place for ci tools)
-cp -r /builds/wexample-public/scripts /opt/wexample
-# Go to
-cd /opt/wexample
+# Add SSH key.
+apt-get install openssh-client -yqq
+mkdir -p ~/.ssh
+eval $(ssh-agent -s)
+[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
-# Load wexample.
-bash ./bash/ubuntu-16.x/_installLocal.sh
-
-# Init gitlab
-wex gitlab/init
-
-# Install composer : we use some libraries in PHP scripts
-wex composer/pharInstall
-
-# Run all tests
-#bash bash/ubuntu-16.x/_tests/_run.sh
-
-cd /builds/wexample-public/scripts
+# Add ssh user.
+ssh-add <(echo "$STAGING_PRIVATE_KEY")
 
 # Deploy to GitHub
 wex gitlab/deployGithub -r="git@github.com:wexample/scripts.git" -k=${STAGING_PRIVATE_KEY}
