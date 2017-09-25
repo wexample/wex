5f25571eaddaf0c2d3d889dfafa69e5879eabcec
Romain WEEGER <romain.weeger@gmail.com>
2017-09-25 13:08:24 +0200

    CI

--- ci/gitlabTest.sh
+++ ci/gitlabTest.sh
@@ -27,7 +27,7 @@ apt-get install openssh-client -yqq
 mkdir -p ~/.ssh
 eval $(ssh-agent -s)
 [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-
+echo ${STAGING_PRIVATE_KEY}"!!!"
 # Add ssh user.
 ssh-add <(echo "$STAGING_PRIVATE_KEY")
 
