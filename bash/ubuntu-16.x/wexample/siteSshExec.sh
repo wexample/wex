874bcb993033732770f25e4c0f57140f8bb4ecbe
Romain WEEGER <romain.weeger@gmail.com>
2017-09-26 05:56:55 +0200

    debug sh

--- bash/ubuntu-16.x/wexample/siteSshExec.sh
+++ bash/ubuntu-16.x/wexample/siteSshExec.sh
@@ -9,6 +9,7 @@ wexampleSiteSshExecArgs() {
 
 wexampleSiteSshExec() {
   wex wexample/siteGetDeployCredentials -d=${DIR}
-
-  ssh -t -p${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_IPV4} "cd ${PROD_PATH_ROOT} && ${WEX_DIR_BASH_UBUNTU16}_exec.sh ${COMMAND}"
+  # Go do site path
+  # Then execute script from _exec.sh tool of wexample.
+  ssh -t -p${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_IPV4} "cd ${PROD_PATH_ROOT} && sudo bash ${WEX_DIR_BASH_UBUNTU16}_exec.sh ${COMMAND}"
 }
