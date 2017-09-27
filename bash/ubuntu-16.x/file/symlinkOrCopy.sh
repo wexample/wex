4e633589ffc2146f254256e94700cdacf4496798
Romain WEEGER <romain.weeger@gmail.com>
2017-09-27 17:32:15 +0200

    fileSymlinkOrCopy -r

--- bash/ubuntu-16.x/file/symlinkOrCopy.sh
+++ bash/ubuntu-16.x/file/symlinkOrCopy.sh
@@ -4,10 +4,20 @@ fileSymlinkOrCopyArgs() {
   _ARGUMENTS=(
     [0]='from f "Symlink location" true'
     [1]='target t "Target" true'
+    [2]='recursive r "Recursive : create subfolder if not exists" false'
   )
 }
 
 fileSymlinkOrCopy() {
+
+  # Create sub directory
+  if [ -z "${RECURSIVE+x}" ];then
+    # Create whole path
+    mkdir -p ${TARGET}
+    # Remove just last piece.
+    rm -rf ${TARGET}
+  fi;
+
   # Symlink does not exists
   if [ ! -L ${TARGET} ]; then
     # Try to make a symlink, may fail on Windows docker context.
