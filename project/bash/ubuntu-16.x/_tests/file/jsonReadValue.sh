5c4c06dcd2c7f0e1375d838267c9db96f945782a
Romain WEEGER <romain.weeger@gmail.com>
2017-11-19 19:50:48 +0100

    Debug json read

--- project/bash/ubuntu-16.x/_tests/file/jsonReadValue.sh
+++ project/bash/ubuntu-16.x/_tests/file/jsonReadValue.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 fileJsonReadValueTest() {
-  wex file/jsonReadValue -f="${_TEST_RUN_DIR_SAMPLES}/jsonSample.json" -k="nested > thirdNested > leaf"
+  VALUE=$(wex file/jsonReadValue -f="${_TEST_RUN_DIR_SAMPLES}jsonSample.json" -k="simpleValue")
+  wexampleTestAssertEqual "${VALUE}" "value"
 }
--- project/bash/ubuntu-16.x/file/jsonReadValue.sh
+++ project/bash/ubuntu-16.x/file/jsonReadValue.sh
@@ -8,6 +8,8 @@ fileJsonReadValueArgs() {
 }
 
 fileJsonReadValue() {
+  # Handle OS specific path
+  FILE=$(wex path/safe -p="${FILE}")
   # Try with python
   if [[ $(wex package/exists -n=python) == true ]]; then
     cat ${FILE} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'${KEY}'"]'
