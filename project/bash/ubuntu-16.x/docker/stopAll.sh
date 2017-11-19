b0570cb74889a9c1f690e46f70e05e9b112d3066
Romain WEEGER <romain.weeger@gmail.com>
2017-11-19 16:21:40 +0100

    cleaner stop

--- project/bash/ubuntu-16.x/docker/stopAll.sh
+++ project/bash/ubuntu-16.x/docker/stopAll.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
 dockerStopAll() {
-  # Stop all
-  docker stop $(docker ps -qa)
-  # Remove
-  docker rm $(docker ps -qa) -f
+  # Containers
+  CONTAINERS=($(docker ps -qa))
+  if (( ${#CONTAINERS[@]} > 0 ));then
+    # Stop all
+    docker stop ${CONTAINERS[@]}
+    # Remove
+    docker rm ${CONTAINERS[@]} -f
+  fi;
+
+  # Networks
+  # List all networks except builtin
+  NETWORKS=($(docker network ls -q --filter type=custom))
+  if (( ${#NETWORKS[@]} > 0 ));then
+    # Remove networks
+    docker network rm ${NETWORKS[@]}
+  fi;
 }
