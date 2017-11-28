6d6018916790c9aec204d4c89e1b279c0a952792
Romain WEEGER <romain.weeger@gmail.com>
2017-11-28 12:22:27 +0100

    mysql/go debug

--- project/bash/wexample/mysql/go.sh
+++ project/bash/wexample/mysql/go.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 mysqlGo() {
-  SITE_NAME=$(wex site/config -k=name)
+  . ${WEX_WEXAMPLE_SITE_CONFIG}
 
   # We may merge behavior with exec.sh
   # We are not into the web container.
   if [[ $(wex docker/isEnv) == false ]]; then
     # Run itself into container, see below.
-    docker exec ${SITE_NAME}_web sh -c "cd /var/www/html/ && wex mysql/exec -c=\"${COMMAND}\""
+    docker exec -it ${SITE_NAME}_web sh -c "cd /var/www/html/ && wex mysql/go"
   else
     mysql $(wex mysql/loginCommand)
   fi;
