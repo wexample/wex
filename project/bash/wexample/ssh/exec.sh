bf261f3b26c7cf024efd0079b5c2daee835537c6
Romain WEEGER <romain.weeger@gmail.com>
2017-12-04 22:32:28 +0100

    test

--- project/bash/wexample/ssh/exec.sh
+++ project/bash/wexample/ssh/exec.sh
@@ -11,5 +11,6 @@ sshExec() {
   wex wexample::site/deployCredentials -d=${DIR}
   # Go do site path
   # Then execute script from _exec.sh tool of wexample.
-  ssh -t -p${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_IPV4} "cd ${DEPLOY_PATH_ROOT} && sudo bash ${WEX_DIR_BASH_DEFAULT}_exec.sh ${SHELL_SCRIPT}"
+  echo ".... WHAT ...."
+  #ssh -t -p${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_IPV4} "cd ${DEPLOY_PATH_ROOT} && sudo bash ${WEX_DIR_BASH_DEFAULT}_exec.sh ${SHELL_SCRIPT}"
 }
