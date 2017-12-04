199d49fa064f38a6832e99ea795520f2e7617bbe
Romain WEEGER <romain.weeger@gmail.com>
2017-12-04 14:00:29 +0100

    Test changes

--- ci/test.sh
+++ ci/test.sh
@@ -10,16 +10,22 @@ cd /opt/wexample
 # Add permission to execute
 chmod -R +x "/opt/wexample"
 # Load wexample.
-. ./bash/ubuntu-16.x/_installLocal.sh
+. ./project/bash/ubuntu-16.x/_installLocal.sh
 
 # Init gitlab
-wex gitlab/init
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
 
-## Install composer : we use some libraries in PHP scripts
-# TODO wex composer/pharInstall
-#
-## Run all tests
-##bash bash/ubuntu-16.x/_tests/_run.sh
+set -xe
+
+# Install global packages
+apt-get update -yqq
+
+# Install composer : we use some libraries in PHP scripts
+wex composer/pharInstall
+
+# Run all tests
+bash bash/ubuntu-16.x/_tests/_run.sh
 
 # Deploy on GitHub
 wex wexample::gitlab/deployGithub -r="git@github.com:wexample/scripts.git"
