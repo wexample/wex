ae27942d6d761e8885c4d2b5d5cb392f6f201175
Romain WEEGER <romain.weeger@gmail.com>
2017-12-04 11:45:21 +0100

    Add log && perfs optimization

--- project/bash/wexample/db/go.sh
+++ project/bash/wexample/db/go.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-mysqlGo() {
+dbGo() {
   # We may merge behavior with exec.sh
   # We are not into the web container.
   if [[ $(wex docker/isEnv) == false ]]; then
     . ${WEX_WEXAMPLE_SITE_CONFIG}
     # Run itself into container, see below.
-    docker exec -it ${SITE_NAME}_web sh -c "cd /var/www/html/ && wex mysql/go"
+    docker exec -it ${SITE_NAME}_web sh -c "cd /var/www/html/ && wex db/go"
   else
     mysql $(wex mysql/loginCommand)
   fi;
