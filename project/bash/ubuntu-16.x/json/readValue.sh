2e0a543fea641ed82bafbd5cab03c3dd32b7774d
Romain WEEGER <romain.weeger@gmail.com>
2017-12-07 10:26:47 +0100

    bash version

--- project/bash/ubuntu-16.x/json/readValue.sh
+++ project/bash/ubuntu-16.x/json/readValue.sh
@@ -12,11 +12,5 @@ jsonReadValue() {
   FILE=$(wex path/safe -p="${FILE}")
   # Double slashes for windows like paths.
   FILE=$(echo "${FILE}" | sed 's/\\/\\\\/g')
-  # Try with python
-  if [[ $(wex package/exists -n=python) == true ]]; then
-    cat ${FILE} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'${KEY}'"]'
-  # Try with PHP
-  elif [[ $(wex package/exists -n=php) == true ]]; then
-    php -r 'echo (json_decode(file_get_contents("'${FILE}'"), JSON_OBJECT_AS_ARRAY))["'${KEY}'"];'
-  fi
+  sed -n "s/^[ ]*\"${KEY}\":[ ]*\"\(.*\)\",*/\1/p" ${FILE}
 }
