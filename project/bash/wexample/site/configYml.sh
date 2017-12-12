21caad68b383832c08872fe2a7bd3b57587757c0
Romain Weeger <romain.weeger@gmail.com>
2017-12-12 08:56:44 +0100

    allow specify yml file

--- project/bash/wexample/site/configYml.sh
+++ project/bash/wexample/site/configYml.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+siteConfigYmlArgs() {
+  _ARGUMENTS=(
+    [0]='file f "Docker compose file" false',
+  )
+}
+
 siteConfigYml() {
+  # Allow specified file
+  if [ -z "${FILE+x}" ]; then
+    FILE=${WEX_WEXAMPLE_SITE_COMPOSE_BUILD_YML}
+  fi;
+
   # Parse yml file built by docker compose.
-  wex yml/parseFile -f=${WEX_WEXAMPLE_SITE_COMPOSE_BUILD_YML}
+  wex yml/parseFile -f=${FILE}
 }
--- project/bash/wexample/site/containers.sh
+++ project/bash/wexample/site/containers.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
+siteContainersArgs() {
+  _ARGUMENTS=(
+    [0]='remove_prefix rm "Remove the name of the site from container name" false',
+    [1]='file f "Docker compose file" false',
+  )
+}
+
 siteContainers() {
-  CONFIG_YML=$(wex site/configYml)
+  CONFIG_YML=$(wex site/configYml -f=${FILE})
   SEARCH_VAR="container_name"
   CONTAINERS=()
 
+  # Remove prefix
+  if [ "${REMOVE_PREFIX}" == true ];then
+    . ${WEX_WEXAMPLE_SITE_CONFIG}
+    REGEX="s/^services_\(.*\)_\?${SEARCH_VAR}\=\"${SITE_NAME}_\(.*\)\"\$/\2/p"
+  else
+    REGEX="s/^services_\(.*\)_\?${SEARCH_VAR}\=\"\?\(.*\)\"\$/\2/p"
+  fi
+
   for COMPOSE_VAR in ${CONFIG_YML[@]}
   do
-    CONTAINER_NAME=$(sed -n "s/^services_\(.*\)_\?${SEARCH_VAR}\=\"\?\(.*\)\"\$/\2/p" <<< ${COMPOSE_VAR})
+    CONTAINER_NAME=$(sed -n ${REGEX} <<< ${COMPOSE_VAR})
     if [[ ${CONTAINER_NAME} ]];then
       CONTAINERS+=(${CONTAINER_NAME})
     fi
