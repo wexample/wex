4c14b7079ea0ea8ddae8ebd0fd73393819171d28
Romain WEEGER <romain.weeger@gmail.com>
2017-12-17 09:19:34 +0100

    db/exec debug

--- project/bash/wexample/db/exec.sh
+++ project/bash/wexample/db/exec.sh
@@ -12,7 +12,7 @@ dbExec() {
   # We are not into the web container.
   if [[ $(wex docker/isEnv) == false ]]; then
     # Run itself into container, see below.
-    docker exec ${SITE_NAME}_web sh -c "cd /var/www/html/ && wex mysql/exec -c=\"${COMMAND}\""
+    docker exec ${SITE_NAME}_web sh -c "cd /var/www/html/ && wex db/exec -c=\"${COMMAND}\""
   else
     mysql $(wex mysql/loginCommand) -e "${COMMAND}"
   fi;
