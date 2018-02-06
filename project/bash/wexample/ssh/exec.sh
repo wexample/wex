df3ba8d5e72da9bb7f788d03b684616b7bc2d9e0
Romain WEEGER <romain.weeger@gmail.com>
2018-02-06 15:08:38 +0100

    undo

--- project/bash/wexample/ssh/exec.sh
+++ project/bash/wexample/ssh/exec.sh
@@ -13,8 +13,8 @@ sshExec() {
   wex env/credentials -e=${ENVIRONMENT} -u=${SSH_USERNAME} -pk=${SSH_PRIVATE_KEY}
   # Go do site path
   # Then execute script from _exec.sh tool of wexample.
-  echo COMMAND='sudo cd '${SITE_PATH_ROOT}' && sudo bash '${WEX_LOCAL_DIR}'project/bash/ubuntu-16.x/_exec.sh "'${SHELL_SCRIPT}'"'
-  COMMAND='sudo cd '${SITE_PATH_ROOT}' && sudo bash '${WEX_LOCAL_DIR}'project/bash/ubuntu-16.x/_exec.sh "'${SHELL_SCRIPT}'"'
+  echo COMMAND='cd '${SITE_PATH_ROOT}' && sudo bash '${WEX_LOCAL_DIR}'project/bash/ubuntu-16.x/_exec.sh "'${SHELL_SCRIPT}'"'
+  COMMAND='cd '${SITE_PATH_ROOT}' && sudo bash '${WEX_LOCAL_DIR}'project/bash/ubuntu-16.x/_exec.sh "'${SHELL_SCRIPT}'"'
   ssh -oLogLevel=QUIET -i${SITE_PRIVATE_KEY} -t -p${SITE_PORT} ${SITE_USERNAME}@${SITE_IPV4} ${COMMAND}
   # Prevent to set credentials globally
   wex env/credentialsClear
