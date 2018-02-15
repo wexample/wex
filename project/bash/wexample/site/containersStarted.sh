177246d61b8856d37a2726b988932b3bc7023cb5
Romain WEEGER <romain.weeger@gmail.com>
2018-02-15 18:53:02 +0100

    All option

--- project/bash/wexample/site/containersStarted.sh
+++ project/bash/wexample/site/containersStarted.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+siteContainersStartedArgs() {
+  _ARGUMENTS=(
+    [0]='all a "All containers runs" false',
+  )
+}
+
 siteContainersStarted() {
   # Get site name.
   CONTAINERS=$(wex site/containers)
   CONTAINERS=$(wex array/join -a="${CONTAINERS}" -s=",")
   # Expect all containers runs.
-  wex docker/containerStarted -n="${CONTAINERS}" -a
+  wex docker/containerStarted -n="${CONTAINERS}" -a=${ALL}
 }
