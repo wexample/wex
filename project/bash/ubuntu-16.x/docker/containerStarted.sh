8964e01e46906cadba67bcc84301d73d9da5c5cb
Romain WEEGER <romain.weeger@gmail.com>
2018-02-15 18:49:56 +0100

    Support "all" option

--- project/bash/ubuntu-16.x/docker/containerStarted.sh
+++ project/bash/ubuntu-16.x/docker/containerStarted.sh
@@ -3,27 +3,50 @@
 dockerContainerStartedArgs() {
   _ARGUMENTS=(
     [0]='names n "Names of containers separated by a comma. Return true only if one container is started." true',
+    [1]='all a "All containers runs" false',
   )
 }
 
 dockerContainerStarted() {
-  RUNNING=$(docker ps -q)
-  NAMES=$(wex text/split -t=${NAMES} -s=",")
+  local RUNNING=$(docker ps -q)
+  local NAMES=$(wex text/split -t=${NAMES} -s=",")
+  local ALL_RUNS=true
 
-  # Inspect stopped containers
-  for CONTAINER_ID in ${RUNNING[@]}
+  # Allow several names.
+  for NAME in ${NAMES[@]}
   do
-    CONTAINER_NAME=$(wex docker/containerName -i=${CONTAINER_ID})
-    # Allow several names.
-    for NAME in ${NAMES[@]}
+    local RUNS=false
+
+    # Inspect not stopped containers
+    for CONTAINER_ID in ${RUNNING[@]}
     do
-      # One container is running.
+      local CONTAINER_NAME=$(wex docker/containerName -i=${CONTAINER_ID})
+      # Searched is running.
       if [[ ${CONTAINER_NAME} == ${NAME} ]];then
-        echo true
-        return
+        RUNS=true
       fi
     done;
+
+    # Expecting all runs.
+    if [[ ${ALL} == true ]];then
+      if [[ ${RUNS} == false ]];then
+        echo false
+        return
+      fi
+    # Only one can run
+    elif [[ ${RUNS} == true ]];then
+      echo true
+      return
+    fi
   done;
 
-  echo false
+  # All containers should runs,
+  # any has been detected as not running.
+  if [[ ${ALL} == true ]];then
+    echo true
+  # One container must run,
+  # any has been detected as running.
+  else
+    echo false
+  fi
 }
