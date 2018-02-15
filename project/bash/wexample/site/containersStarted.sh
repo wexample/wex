eb03e74ac820d3a2fff20c5f39baca8f3a1f093f
Romain WEEGER <romain.weeger@gmail.com>
2018-02-15 18:51:29 +0100

    Containers started for site

--- project/bash/wexample/site/containersStarted.sh
+++ project/bash/wexample/site/containersStarted.sh
@@ -4,5 +4,6 @@ siteContainersStarted() {
   # Get site name.
   CONTAINERS=$(wex site/containers)
   CONTAINERS=$(wex array/join -a="${CONTAINERS}" -s=",")
-  wex docker/containerStarted -n="${CONTAINERS}"
+  # Expect all containers runs.
+  wex docker/containerStarted -n="${CONTAINERS}" -a
 }
