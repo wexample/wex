aa01473c33f4d190ca3d5e5dd3337b81afa3df86
Romain WEEGER <romain.weeger@gmail.com>
2018-02-26 17:34:48 +0100

    FTP improve

---
+++ project/bash/wexample/ftp/userAdd.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ftpUserAddArgs() {
+  _ARGUMENTS=(
+    [0]='ftp_username u "FTP Username" true'
+    [1]='directory pk "Directory related to site root" true'
+  )
+}
+
+ftpUserAdd() {
+  # Load site name.
+  . ${WEX_WEXAMPLE_SITE_CONFIG}
+
+  # Check ftp container exists
+  if [ $(wex docker/containerExists -c=${SITE_NAME}_ftp) == false ];then
+    return
+  fi
+
+  # Exec into container
+  docker exec ${SITE_NAME}_ftp /bin/bash -c "pure-pw useradd ${FTP_USERNAME} -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -u ftpuser -d /var/www/${DIRECTORY}"
+}
