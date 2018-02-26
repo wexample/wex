2f063bd5b4967dcf63261c87670d648de0db05f7
Romain WEEGER <romain.weeger@gmail.com>
2018-02-26 17:43:33 +0100

    userAdd

--- project/bash/wexample/ftp/userAdd.sh
+++ project/bash/wexample/ftp/userAdd.sh
@@ -17,5 +17,5 @@ ftpUserAdd() {
   fi
 
   # Exec into container
-  docker exec ${SITE_NAME}_ftp /bin/bash -c "pure-pw useradd ${FTP_USERNAME} -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -u ftpuser -d /var/www/${DIRECTORY}"
+  pure-pw useradd ${FTP_USERNAME} -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -u ftpuser -d /var/www/${DIRECTORY} | docker exec ${SITE_NAME}_ftp /bin/bash
 }
