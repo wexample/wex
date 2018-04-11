af0c6486b67804843c71b33d55587a6d888e64cc
Romain Weeger <romain.weeger@gmail.com>
2018-04-11 17:50:02 +0200

    Change repo path

--- project/bash/ubuntu-16.x/_examples.sh
+++ project/bash/ubuntu-16.x/_examples.sh
@@ -2,7 +2,7 @@
 
 # This method is used to execute any wexample script
 # from an environment without wexample scripts installed.
-s=helloWorld && w=wex && curl https://raw.githubusercontent.com/wexample/scripts/master/bash/$w | tr -d '\015' > $w && bash $w -s=$s -rm && rm -rf $w
+s=helloWorld && w=wex && curl https://raw.githubusercontent.com/wexample/scripts/master/project/bash/$w | tr -d '\015' > $w && bash $w -s=$s -rm && rm -rf $w
 
 # Wexample loader for multiple scripts.
 w=wex
--- project/bash/ubuntu-16.x/_installRemote.sh
+++ project/bash/ubuntu-16.x/_installRemote.sh
@@ -2,4 +2,4 @@
 # Source this file to execute install and add wexample to PATH
 
 # Install wexample
-w=_install.sh && curl https://raw.githubusercontent.com/wexample/scripts/master/bash/ubuntu-16.x/$w | tr -d '\015' > $w && . $w && rm -rf $w
+w=_install.sh && curl https://raw.githubusercontent.com/wexample/scripts/master/project/bash/ubuntu-16.x/$w | tr -d '\015' > $w && . $w && rm -rf $w
