40060ca9642f5e35c57b997142fe17b1cfd58c30
Romain Weeger <romain.weeger@gmail.com>
2018-04-14 15:44:59 +0200

    Local var get

--- project/bash/ubuntu-16.x/var/localGet.sh
+++ project/bash/ubuntu-16.x/var/localGet.sh
@@ -13,28 +13,30 @@ varLocalGet() {
   local LOCAL_STORAGE_FILE=${WEX_DIR_TMP}variablesLocalStorage
 
   if [ -f ${LOCAL_STORAGE_FILE} ];then
-    # Eval whole file.
-    eval $(cat ${LOCAL_STORAGE_FILE})
-    # Get value
-    local VALUE=$(eval 'echo ${'${NAME}'}')
-
-    # Value not found.
-    if [ "${VALUE}" == "" ]; then
-      # Asking user enabled.
-      if [ ! -z "${ASK+x}" ]; then
-        local OPTIONS=''
-        if [ ! -z "${PASSWORD+x}" ]; then
-          OPTIONS='-s'
-        fi
-
-        read ${OPTIONS} -p "${ASK} : " VALUE
-      else
-        VALUE=DEFAULT
-      fi
-      # Store value.
-      wex var/localSet -n=${NAME} -v=${VALUE}
-    fi;
-
-    echo ${VALUE}
+    touch ${LOCAL_STORAGE_FILE}
   fi
+
+  # Eval whole file.
+  eval $(cat ${LOCAL_STORAGE_FILE})
+  # Get value
+  local VALUE=$(eval 'echo ${'${NAME}'}')
+
+  # Value not found.
+  if [ "${VALUE}" == "" ]; then
+    # Asking user enabled.
+    if [ ! -z "${ASK+x}" ]; then
+      local OPTIONS=''
+      if [ ! -z "${PASSWORD+x}" ]; then
+        OPTIONS='-s'
+      fi
+
+      read ${OPTIONS} -p "${ASK} : " VALUE
+    else
+      VALUE=DEFAULT
+    fi
+    # Store value.
+    wex var/localSet -n=${NAME} -v=${VALUE}
+  fi;
+
+  echo ${VALUE}
 }
