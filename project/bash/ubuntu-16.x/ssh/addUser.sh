acb1e9b7e6eb9fff90b7fd509dfdfea6547eb25b
Romain Weeger <romain.weeger@gmail.com>
2018-04-16 06:56:10 +0200

    Add SSH user

--- project/bash/ubuntu-16.x/ssh/addUser.sh
+++ project/bash/ubuntu-16.x/ssh/addUser.sh
@@ -2,7 +2,7 @@
 
 sshAddUserArgs() {
   _ARGUMENTS=(
-    [0]='user u "User" true'
+    [0]='user_ssh u "User" true'
     [1]='group g "Host" true'
     [2]='key k "SSH Key to use" true'
   )
@@ -10,17 +10,17 @@ sshAddUserArgs() {
 
 sshAddUser() {
   if [ "${GROUP}" == "" ];then
-    local GROUP=${USER}
+    local GROUP=${USER_SSH}
   fi
 
   # Create SSH dir.
-  mkdir -p /home/${USER}/.ssh
-  chown ${USER}:${GROUP} /home/${USER}/.ssh
-  chmod 700 /home/${USER}/.ssh
+  mkdir -p /home/${USER_SSH}/.ssh
+  chown ${USER_SSH}:${GROUP} /home/${USER_SSH}/.ssh
+  chmod 700 /home/${USER_SSH}/.ssh
 
   # Add wexample SSH key
-  wex file/textAppend -f=/home/${USER}/.ssh/authorized_keys -l="${KEY}"
+  wex file/textAppend -f=/home/${USER_SSH}/.ssh/authorized_keys -l="${KEY}"
 
-  chown ${USER}:${GROUP} /home/${USER}/.ssh/authorized_keys
-  chmod 600 /home/${USER}/.ssh/authorized_keys
+  chown ${USER_SSH}:${GROUP} /home/${USER_SSH}/.ssh/authorized_keys
+  chmod 600 /home/${USER_SSH}/.ssh/authorized_keys
 }
