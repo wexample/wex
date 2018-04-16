37e898554463d7254c7bf509624f8fbf7b49fa7b
Romain Weeger <romain.weeger@gmail.com>
2018-04-16 09:32:28 +0200

    Deploy improvement

--- project/bash/wexample/site/deploy.sh
+++ project/bash/wexample/site/deploy.sh
@@ -15,7 +15,7 @@ siteDeploy() {
   local PROD_IPV4=$(wex json/readValue -f=wex.json -k=prod.ipv4)
 
   # There is a production site configured in wex.json.
-  if [ "${PROD_IPV4}" == "" ]; then
+  if [ "${PROD_IPV4}" != "" ]; then
     # Update on production server
     # User must have access to execute scripts.
     # Use : sudo visudo
