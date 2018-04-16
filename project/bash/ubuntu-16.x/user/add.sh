db3447154e295cb8361788a42d01c9db2e15adb0
Romain Weeger <romain.weeger@gmail.com>
2018-04-16 10:53:27 +0200

    User add

--- project/bash/ubuntu-16.x/user/add.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-userAdd() {
-  user=${1}
-  password=${2}
-  group=${3}
-  # Create a new super user.
-  useradd -g ${group} -ms /bin/bash -p $(echo ${password} | openssl passwd -1 -stdin) ${user}
-}
