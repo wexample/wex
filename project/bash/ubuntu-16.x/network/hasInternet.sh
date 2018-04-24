ef011999637b7556dcca38d56727d66030e00f05
Romain Weeger <romain.weeger@gmail.com>
2018-04-24 18:55:25 +0200

    Improve internet detect

--- project/bash/ubuntu-16.x/network/hasInternet.sh
+++ project/bash/ubuntu-16.x/network/hasInternet.sh
@@ -2,11 +2,19 @@
 
 networkHasInternet() {
   local IP=$(ip r | grep default | cut -d ' ' -f 3)
-
+  local RETURN='false'
   if [ "${IP}" != "" ];then
-    ping -q -w 1 -c 1 ${IP} > /dev/null && echo true || echo false
-    return
+    local RETURN=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo true || echo error)
+
+    if [ ${RETURN} == 'error' ];then
+      if ping -c 1 wexample.com >> /dev/null 2>&1; then
+        RETURN=true
+      # Okay, try with a serious website
+      elif ping -c 1 google.com >> /dev/null 2>&1; then
+        RETURN=true
+      fi
+    fi
   fi
 
-  echo false
+  echo ${RETURN}
 }
