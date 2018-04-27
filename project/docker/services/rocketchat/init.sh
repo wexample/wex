6a82d2898c6b6c0210f916156cb4cc7ab2fde46d
Romain Weeger <romain.weeger@gmail.com>
2018-04-27 09:08:19 +0200

    Last user creation tries

--- project/docker/services/rocketchat/init.sh
+++ project/docker/services/rocketchat/init.sh
@@ -17,8 +17,14 @@ rocketchatInit() {
   . ./tmp/config
 
   # Create replica set
-  docker exec ${NAME}_mongo mongo localhost:${SITE_PORT_RANGE}70/rocketchat --eval "rs.initiate({ _id: \"rs0\", members: [ { _id: 0, host: \"localhost:"${SITE_PORT_RANGE}"70\" } ]})"
+  docker exec ${SITE_NAME}_mongo mongo localhost:${SITE_PORT_RANGE}70/rocketchat --eval "rs.initiate({ _id: \"rs0\", members: [ { _id: 0, host: \"localhost:"${SITE_PORT_RANGE}"70\" } ]})"
+
+  # Import admin and wexbot user
+  # - admin / password
+  # - hubot / pass given in yml file
+  #docker cp ${WEX_DIR_ROOT}docker/services/rocketchat/users.json ${SITE_NAME}_mongo:/
+  #docker exec ${SITE_NAME}_mongo mongoimport --port 7070 --db rocketchat --collection users --file /users.json
+  #docker exec ${SITE_NAME}_mongo rm /users.json
 
   wex site/stop
-
 }
