0597420022f4c01fc58706febae1366336e3a6af
Romain Weeger <romain.weeger@gmail.com>
2018-05-03 14:22:08 +0200

    deploy debug mode

--- project/bash/wexample/site/deploy.sh
+++ project/bash/wexample/site/deploy.sh
@@ -3,10 +3,33 @@
 siteDeployArgs() {
   _ARGUMENTS=(
     [0]='dir d "Root directory of site" false'
+    [1]='upwex upwex "Use last version of wexample scripts (debug mode)" false'
   )
 }
 
 siteDeploy() {
+
+  if [ ! -z "${UPWEX+x}" ]; then
+
+    # Save current dir.
+    CURRENT_DIR=$(realpath ./)
+
+    # Reinstall wexample from github.
+    rm -rf /opt/wexample
+    cd /opt/
+    mkdir wexample
+    cd wexample
+    git clone https://github.com/wexample/scripts.git .
+    . project/bash/default/_installLocal.sh
+
+    # Go back to project.
+    cd ${CURRENT_DIR}
+    # Relaunch with debug mode.
+    wex site/deploy --debug
+
+    return
+  fi;
+
   # Use current dir by default
   if [ -z "${DIR+x}" ]; then
     DIR=./
