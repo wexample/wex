d4a8826d2a7dd5d50106040c1833e2d103c3bc5d
Romain Weeger <romain.weeger@gmail.com>
2018-05-03 14:43:15 +0200

    Dep

--- project/bash/wexample/remote/exec.sh
+++ project/bash/wexample/remote/exec.sh
@@ -13,6 +13,7 @@ remoteExecArgs() {
 }
 
 remoteExec() {
+  echo wex remote/connexion -e=${ENVIRONMENT} ${WEX_ARGUMENTS}
   # Prevent to set credentials globally
   local SSH_CONNEXION=$(wex remote/connexion -e=${ENVIRONMENT} ${WEX_ARGUMENTS})
 
