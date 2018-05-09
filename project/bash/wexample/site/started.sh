999feda42159e876d8ac6376d77ee3cdfdede89a
Romain Weeger <romain.weeger@gmail.com>
2018-05-09 17:17:24 +0200

    Optim

--- project/bash/wexample/site/started.sh
+++ project/bash/wexample/site/started.sh
@@ -18,13 +18,13 @@ siteStarted() {
   fi
 
   # Config file exists
-  if [[ -f ${DIR_SITE}${WEX_WEXAMPLE_SITE_CONFIG} ]];then
+  if [ -f ${DIR_SITE}${WEX_WEXAMPLE_SITE_CONFIG} ];then
     # Load config
     . ${DIR_SITE}${WEX_WEXAMPLE_SITE_CONFIG}
     # Started && into server
-    if [ ${STARTED} == true ] && [[ $(wex file/lineExists -f=${WEX_WEXAMPLE_DIR_PROXY_TMP}sites -l=$(realpath ${DIR_SITE})"/") == true ]];then
+    if [ ${STARTED} == true ] && [ $(wex file/lineExists -f=${WEX_WEXAMPLE_DIR_PROXY_TMP}sites -l=$(realpath ${DIR_SITE})"/") == true ];then
       # Check if containers are started if expected.
-      if [[ ${IGNORE_CONTAINERS} != true ]];then
+      if [ "${IGNORE_CONTAINERS}" != true ];then
         # At least on container should run.
         # Return true or false.
         wex containers/started
