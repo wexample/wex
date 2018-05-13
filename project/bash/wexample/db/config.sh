64670097f42a22677420041a7e698d932ae14b40
Romain Weeger <romain.weeger@gmail.com>
2018-05-13 17:53:21 +0200

    db/config

--- project/bash/wexample/db/config.sh
+++ project/bash/wexample/db/config.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 
 dbConfig() {
-  # TODO : Handle framework detection
+  # Load framework settings
+  wex framework/settings -d=./project/
 
+  # No settings found from framework.
+  if [ "${SITE_DB_NAME}" == "" ];then
   . .wex
 
+  local DB_TYPE=$(wex db/detect)
+
   # Export default credentials if not found in framework.
-  export SITE_DB_HOST=${NAME}_postgres
+  export SITE_DB_HOST=${NAME}_${DB_TYPE}
   export SITE_DB_PORT=5432
   export SITE_DB_NAME=${NAME}
   export SITE_DB_USER=root
   export SITE_DB_PASSWORD="thisIsAReallyNotSecurePassword!"
 
-  local POSTGRES_CONFIG=''
-  POSTGRES_CONFIG+="\nSITE_DB_HOST="${SITE_DB_HOST}
-  POSTGRES_CONFIG+="\nSITE_DB_PORT="${SITE_DB_PORT}
-  POSTGRES_CONFIG+="\nSITE_DB_NAME="${SITE_DB_NAME}
-  POSTGRES_CONFIG+="\nSITE_DB_USER="${SITE_DB_USER}
-  POSTGRES_CONFIG+="\nSITE_DB_PASSWORD="${SITE_DB_PASSWORD}
+  local DB_CONFIG=''
+  DB_CONFIG+="\nSITE_DB_HOST="${SITE_DB_HOST}
+  DB_CONFIG+="\nSITE_DB_PORT="${SITE_DB_PORT}
+  DB_CONFIG+="\nSITE_DB_NAME="${SITE_DB_NAME}
+  DB_CONFIG+="\nSITE_DB_USER="${SITE_DB_USER}
+  DB_CONFIG+="\nSITE_DB_PASSWORD="${SITE_DB_PASSWORD}
 
-  echo ${POSTGRES_CONFIG}
+  echo ${DB_CONFIG}
+  fi
 }
