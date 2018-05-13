12fb79198a745f6c259ea755a4bd00c9fe3e311a
Romain Weeger <romain.weeger@gmail.com>
2018-05-13 17:52:44 +0200

    ports/opened

--- project/bash/default/ports/opened.sh
+++ project/bash/default/ports/opened.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+portsOpenedArgs() {
+  _ARGUMENTS=(
+    [0]='separator s "Separator" false'
+  )
+}
+
 portsOpened() {
-  netstat -ntlp | grep LISTEN
+  if [ -z "${SEPARATOR+x}" ];then
+    # Default space separator
+    SEPARATOR=" "
+  fi;
+
+  netstat -tuwanp4 | awk '{print $4}' | grep ':' | cut -d ":" -f 2 | sort | uniq | sed -e ':a' -e 'N' -e '$!ba' -e "s/\n/${SEPARATOR}/g"
 }
