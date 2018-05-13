b4708ad96980af62f4aa2e8d2ae70b5d9a4271a8
Romain Weeger <romain.weeger@gmail.com>
2018-05-13 18:51:05 +0200

    rocketckat ok with new ports management

--- project/docker/services/rocketchat/init.sh
+++ project/docker/services/rocketchat/init.sh
@@ -17,13 +17,13 @@ rocketchatInit() {
   . ./tmp/config
 
   # Create replica set
-  docker exec ${SITE_NAME}_mongo mongo localhost:${SITE_PORT_RANGE}70/rocketchat --eval "rs.initiate({ _id: \"rs0\", members: [ { _id: 0, host: \"localhost:"${SITE_PORT_RANGE}"70\" } ]})"
+  docker exec ${SITE_NAME}_mongo mongo localhost:27017/rocketchat --eval "rs.initiate({ _id: \"rs0\", members: [ { _id: 0, host: \"localhost:27017\" } ]})"
 
   # Import default database
   # - admin / password
   # - hubot / pass given in yml file
   docker cp ${WEX_DIR_ROOT}docker/services/rocketchat/fixtures ${SITE_NAME}_mongo:/
-  docker exec ${SITE_NAME}_mongo mongorestore --port ${SITE_PORT_RANGE}70 /fixtures
+  docker exec ${SITE_NAME}_mongo mongorestore --port 27017 /fixtures
   docker exec ${SITE_NAME}_mongo rm -rf /fixtures
 
   wex site/stop
