e0c3ba39b9f51b990f04aeade937ab9f1548dd16
Romain Weeger <romain.weeger@gmail.com>
2018-05-15 06:50:18 +0200

    Deploy key big bug

--- project/bash/wexample/gitlab/sshInit.sh
+++ project/bash/wexample/gitlab/sshInit.sh
@@ -7,10 +7,5 @@ gitlabSshInit() {
   # It allow deployment system to use server rsa key pair for deployment.
   local DEPLOY_KEY="/root/.ssh/host/id_rsa"
 
-  # The variable name is set into project variables settings.
-  echo "${STAGING_PRIVATE_KEY}" > ${DEPLOY_KEY};
-  # SSH expect restricted access
-  chmod 400 ${DEPLOY_KEY}
-
-  wex default::gitlab/sshInit -k=${DEPLOY_KEY}
+  wex default::gitlab/sshInit -k="/root/.ssh/host/id_rsa"
 }
