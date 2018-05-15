5742c8bb9ca7dc844b16c158f3564101d2d9a6f8
Romain Weeger <romain.weeger@gmail.com>
2018-05-15 06:33:13 +0200

    Deployment key share

--- project/bash/wexample/gitlab/sshInit.sh
+++ project/bash/wexample/gitlab/sshInit.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
 gitlabSshInit() {
-  if [ -z "${STAGING_PRIVATE_KEY+x}" ]; then
-    echo "Missing secret CI variable STAGING_PRIVATE_KEY, set it in [gitlab project]/settings/ci_cd"
-    gitlab-ctl stop
-    exit
-  fi;
-
-  # Save deployment key,
-  # we can't pass key by argument directly,
-  # the key is not recognized, maybe quotes or line ending issue,
-  # so we use a local file.
-  DEPLOY_KEY="/deployKey"
+  # Gitlab runner configuration must have a shared folder
+  # declared into /etc/gitlab-runner/config.toml
+  # volumes = [ ... ,"/root/.ssh:/root/.ssh/host/"]
+  # It allow deployment system to use server rsa key pair for deployment.
+  local DEPLOY_KEY="/root/.ssh/host/id_rsa"
 
   # The variable name is set into project variables settings.
   echo "${STAGING_PRIVATE_KEY}" > ${DEPLOY_KEY};
