44c935112c5112d3738ccf0549d06f2749b03821
Romain Weeger <romain.weeger@gmail.com>
2018-05-20 10:16:11 +0200

    New pull with no inception

--- project/bash/wexample/site/pull.sh
+++ project/bash/wexample/site/pull.sh
@@ -5,44 +5,23 @@ sitePull() {
 
   # Used in production to retrieve changes when tests are passed.
 
-  # This file should be executed from the site root.
+  # Update GIT
+  wex git/pullTree
+  # Manage permissions.
+  chown -R www-data:www-data ./
 
-  # We are in the host server.
-  if [ $(wex docker/isEnv) == false ]; then
-    # Stop during pull
-    wex site/stop
+  # Per framework pull behavior.
+  # Detect used frameworks
+  FRAMEWORKS=($(wex framework/list -d=./project/))
 
-    # Update GIT
-    wex git/pullTree
+  for FRAMEWORK in ${FRAMEWORKS[@]}
+  do
+    FRAMEWORK=framework$(wexUpperCaseFirstLetter ${FRAMEWORK})
+    # Pull script exists
+    if [ -f ${WEX_DIR_BASH}"wexample/"${FRAMEWORK}"/pull.sh" ];then
+      wex wexample::${FRAMEWORK}/pull
+    fi
+  done;
 
-    # Run itself into container, see below.
-    wex site/exec -c="wex wexample::site/pull"
-
-    # Restart.
-    wex site/start
-  else
-
-    SITE_PATH_ROOT=/var/www/html/
-
-    # Go to current (container) folder.
-    cd ${SITE_PATH_ROOT}
-
-    # Manage permissions.
-    chown -R www-data:www-data ./
-
-    # Per framework pull behavior.
-    # Detect used frameworks
-    FRAMEWORKS=($(wex framework/list -d=./project/))
-
-    for FRAMEWORK in ${FRAMEWORKS[@]}
-    do
-      FRAMEWORK=framework$(wexUpperCaseFirstLetter ${FRAMEWORK})
-      # Pull script exists
-      if [ -f ${WEX_DIR_BASH}"wexample/"${FRAMEWORK}"/pull.sh" ];then
-        wex wexample::${FRAMEWORK}/pull
-      fi
-    done;
-
-    wex ci/exec -c=pull
-  fi;
+  wex ci/exec -c=pull
 }
