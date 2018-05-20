fec11f257d1627179422dd0e218ce4e4bec3da6e
Romain Weeger <romain.weeger@gmail.com>
2018-05-20 11:12:23 +0200

    Site env && zip module

--- project/services/web/config.sh
+++ project/services/web/config.sh
@@ -8,5 +8,21 @@ webConfig() {
   # cron
   wex service/templates -s=cron -e=false
 
+  # Create ini file.
+  . ${WEX_DIR_ROOT}services/mysql/config.sh
+  local ACCESS=($(mysqlConfigAccess))
+  local INI=./tmp/php.env.ini
+  local SITE_ENV=$(wex site/env)
+
+  echo '; Auto generated configuration' > ${INI}
+  echo '[site]' >> ${INI}
+  echo 'SITE_ENV = "'${SITE_ENV}'"' >> ${INI}
+  echo '[mysql]' >> ${INI}
+  echo 'MYSQL_DB_HOST = "'${ACCESS[0]}'"' >> ${INI}
+  echo 'MYSQL_DB_PORT = "'${ACCESS[1]}'"' >> ${INI}
+  echo 'MYSQL_DB_NAME = "'${ACCESS[2]}'"' >> ${INI}
+  echo 'MYSQL_DB_USER = "'${ACCESS[3]}'"' >> ${INI}
+  echo 'MYSQL_DB_PASSWORD = "'${ACCESS[4]}'"' >> ${INI}
+
   echo "\nSITE_CONTAINER=web"
 }
