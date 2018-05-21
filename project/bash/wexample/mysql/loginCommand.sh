8e2b2d66426a74946b5fa2634cf00d8941cf420e
Romain Weeger <romain.weeger@gmail.com>
2018-05-21 11:07:34 +0200

    login from conatiner

--- project/bash/wexample/mysql/loginCommand.sh
+++ project/bash/wexample/mysql/loginCommand.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 mysqlLoginCommand() {
+  local BASE_PATH=''
+  if [ $(wex docker/isEnv) == true ];then
+    BASE_PATH='/var/www/html/'
+  else
+    BASE_PATH='./'
+  fi
   # Load credentials stored into config
   wex config/load
-  echo --defaults-extra-file=./tmp/mysql.cnf ${MYSQL_DB_NAME}
+  echo --defaults-extra-file=${BASE_PATH}tmp/mysql.cnf ${MYSQL_DB_NAME}
 }
