4ec643e28b275ef5a887dbc7bbebcb3b5fd2f7b3
Romain Weeger <romain.weeger@gmail.com>
2018-05-21 16:35:04 +0200

    pull up

--- project/services/wordpress/sitePull.sh
+++ project/services/wordpress/sitePull.sh
@@ -6,7 +6,7 @@ wordpressSitePull() {
   chown -R www-data:www-data ./wordpress/
   # Do not allow to change main folders by FTP,
   # because it will be lost as mounted docker volumes.
-  chmod -R 666 ./wordpress/public
+  chmod -R 777 ./wordpress/public
   # Access for FTP transfer
   chmod -R 777 ./wordpress/public/*
 }
