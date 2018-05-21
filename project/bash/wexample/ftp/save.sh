7a56fafb70ce828862499bdfb8688b383a05bc1c
Romain Weeger <romain.weeger@gmail.com>
2018-05-21 16:10:36 +0200

    ftp save

--- project/bash/wexample/ftp/save.sh
+++ project/bash/wexample/ftp/save.sh
@@ -4,7 +4,7 @@ ftpSave() {
   # Load site name.
   . ${WEX_WEXAMPLE_SITE_CONFIG}
 
-  docker exec -ti wex_ftp "cp /etc/pure-ftpd/passwd/${SITE_NAME}.passwd /var/www/${SITE_NAME}/ftp/passwd/"
+  docker exec -ti wex_ftp cp /etc/pure-ftpd/passwd/${SITE_NAME}.passwd /var/www/${SITE_NAME}/ftp/passwd/
 
   # Save.
   git add ./ftp/passwd/
--- project/bash/wexample/ftp/userAdd.sh
+++ project/bash/wexample/ftp/userAdd.sh
@@ -5,6 +5,7 @@ ftpUserAddArgs() {
     [0]='ftp_username u "FTP Username" true'
     [1]='password p "Password" true'
     [2]='directory d "Directory related to site root (ex : project/files)" true'
+    [3]='save s "Push changes in site repository" true'
   )
 }
 
@@ -12,8 +13,13 @@ ftpUserAdd() {
   # Load site name.
   . ${WEX_WEXAMPLE_SITE_CONFIG}
 
-  # Exec into container
-  docker exec -ti wex_ftp /bin/bash -c "(echo ${PASSWORD}; echo ${PASSWORD}) |  pure-pw useradd ${FTP_USERNAME} -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -u ftpuser -d /var/www/${SITE_NAME}/${DIRECTORY}"
+  local PASS_LOCATION=/etc/pure-ftpd/passwd/${SITE_NAME}.passwd
 
-  #wex ftp/save
+  docker exec -ti wex_ftp touch ${PASS_LOCATION}
+  # Exec into container
+  docker exec -ti wex_ftp /bin/bash -c "(echo ${PASSWORD}; echo ${PASSWORD}) |  pure-pw useradd ${FTP_USERNAME} -f ${PASS_LOCATION} -m -u ftpuser -d /var/www/${SITE_NAME}/${DIRECTORY}"
+
+  if [ "${SAVE}" == true ];then
+    wex ftp/save
+  fi
 }
