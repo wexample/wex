7f0f1afa652c7c3618059de2483c67e0bb6637f9
Romain Weeger <romain.weeger@gmail.com>
2018-05-21 21:02:09 +0200

    Mariadb

--- project/services/mariadb/config.sh
+++ project/services/mariadb/config.sh
@@ -3,5 +3,16 @@
 mariadbConfig() {
   wex service/templates -s=mariadb -e=cnf.json
 
-  wex db/config
+  # Create ini file.
+  . ${WEX_DIR_ROOT}services/mysql/config.sh
+  local ACCESS=($(mysqlConfigAccess))
+  local MARIADB_CONFIG=''
+
+  MARIADB_CONFIG+="\nMARIADB_DB_HOST="${ACCESS[0]}
+  MARIADB_CONFIG+="\nMARIADB_DB_PORT="${ACCESS[1]}
+  MARIADB_CONFIG+="\nMARIADB_DB_NAME="${ACCESS[2]}
+  MARIADB_CONFIG+="\nMARIADB_DB_USER="${ACCESS[3]}
+  MARIADB_CONFIG+="\nMARIADB_DB_PASSWORD=${ACCESS[4]}"
+
+  echo ${MARIADB_CONFIG}
 }
