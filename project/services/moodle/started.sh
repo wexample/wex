14832df99bda9908a4664d6f1a90001315f78a58
Romain Weeger <romain.weeger@gmail.com>
2018-05-24 22:59:12 +0200

    various

--- project/services/moodle/started.sh
+++ project/services/moodle/started.sh
@@ -5,4 +5,8 @@ moodleStarted() {
   . ${WEX_DIR_ROOT}services/web/started.sh
 
   webStarted
+
+  # Give access.
+  wex site/exec -c="chmod 755 /var/www/html/moodledata"
+  wex site/exec -c="chown www-data:www-data /var/www/html/moodledata"
 }
