cb406f4a112a4fae9ef2d4995e4dd0f7423a2c65
Romain Weeger <romain.weeger@gmail.com>
2018-05-28 15:20:43 +0200

    started.sh changes

--- project/services/mysql/started.sh
+++ project/services/mysql/started.sh
@@ -7,7 +7,8 @@ mysqlStarted() {
 
     echo "Set MySQL site.cnf access"
 
-    docker exec ${SITE_NAME}_mysql chmod -rw /etc/mysql/conf.d/site.cnf
+    docker exec ${SITE_NAME}_mysql chmod a-rw /var/www/tmp/mysql.cnf
+    docker exec ${SITE_NAME}_mysql chmod a-rw /etc/mysql/conf.d/site.cnf
     docker exec ${SITE_NAME}_mysql service mysql restart
   fi
 }
