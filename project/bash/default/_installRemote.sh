039f0b1c0f1b8c992adac95a33950ee101e41911
Romain Weeger <romain.weeger@gmail.com>
2018-06-09 15:18:02 +0200

    Minor

--- project/bash/default/_installRemote.sh
+++ project/bash/default/_installRemote.sh
@@ -2,4 +2,4 @@
 # Source this file to execute install and add wexample to PATH
 
 # Install wexample
-w=_install.sh && curl https://raw.githubusercontent.com/wexample/scripts/master/project/bash/default/$w | tr -d '\015' > $w && . $w && rm -rf $w
+w=_install.sh && curl https://raw.githubusercontent.com/wexample/scripts/master/project/bash/default/$w | tr -d '\015' > $w && . $w && rm $w
