0181e0dd959a04ad58ce74b2c22953ae9ce4b32b
Romain Weeger <romain.weeger@gmail.com>
2018-06-09 15:18:52 +0200

    Install

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-. ./project/bash/default/_installLocal.sh
+# Executing this file :
+# w=install.sh && curl https://raw.githubusercontent.com/wexample/scripts/master/project/$w | tr -d '\015' > $w && . $w && rm $w
+
+# Get last production version.
+curl -Lo /opt/wexample.zip https://github.com/wexample/scripts/archive/master.zip
+# Extract
+unzip /opt/wexample.zip -d /opt/
+# Move
+mv /opt/scripts-master/ /tmp/wexample
+# Install locally.
+bash /opt/wexample/project/bash/default/_installLocal.sh
