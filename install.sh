2acb68ca60f8846ca15fab03c555ffc530c5cddf
Romain Weeger <romain.weeger@gmail.com>
2018-06-11 14:49:57 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ curl -Lo /opt/wexample.zip https://github.com/wexample/scripts/archive/master.zi
 # Extract
 unzip /opt/wexample.zip -d /opt/
 # Move
-mv /opt/scripts-master/ /tmp/wexample
+mv /opt/scripts-master/ /opt/wexample
 # Install locally.
 bash /opt/wexample/project/bash/default/_installLocal.sh
