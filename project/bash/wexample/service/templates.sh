00266e1349229465352f57429c191f5eca989897
Romain Weeger <romain.weeger@gmail.com>
2018-06-12 09:54:30 +0200

    Fix files detection

--- project/bash/wexample/service/templates.sh
+++ project/bash/wexample/service/templates.sh
@@ -10,7 +10,7 @@ serviceTemplatesArgs() {
 serviceTemplates() {
   FOLDER="./"${SECTION}
   SECTION_FILES=$(ls ${FOLDER})
-  FOUND=()
+  local NAMES_PROCESSED=()
 
   # Get site env name.
   local SITE_ENV=$(wex site/env)
@@ -19,38 +19,36 @@ serviceTemplates() {
     SPLIT=($(wex text/split -s="." -t=${FILE}))
     BASE_NAME=${SPLIT[0]}
 
+    # Base file ex container.ext
+    local CONF_VAR_NAME=${SPLIT[@]}
+
+    # There is more than two pieces
+    if [ "${SPLIT[2]}" != "" ];then
+      # Second part si equal to
+      if [ "${SPLIT[1]}" == ${SITE_ENV} ];then
+        # Remove env name
+        CONF_VAR_NAME=${SPLIT[0]}" "${SPLIT[2]}
+        # This is a strange.name.ext
+      else
+        CONF_VAR_NAME=false
+      fi
+    fi
+
     # One execution only by base name,
     # Search for file variations inside it.
-    if [ $(wex array/contains -a="${FOUND[@]}" -i=${BASE_NAME}) == false ];then
-      # Base file ex container.ext
-      local CONF_VAR_NAME=${SPLIT[@]}
+    if [ "${CONF_VAR_NAME}" != false ] && [[ ! " ${NAMES_PROCESSED[@]} " =~ " ${SPLIT[0]} " ]];then
+      # Save as found
+      NAMES_PROCESSED+=(${SPLIT[0]})
 
-       # There is more than two pieces
-      if [ "${SPLIT[2]}" != "" ];then
-        # Second part si equal to
-        if [ "${SPLIT[1]}" == ${SITE_ENV} ];then
-          # Remove env name
-          CONF_VAR_NAME=${SPLIT[0]}" "${SPLIT[2]}
-          # This is a strange.name.ext
-        else
-          CONF_VAR_NAME=false
-        fi
-      fi
+     # Return to array
+      CONF_VAR_NAME=(${CONF_VAR_NAME})
+      # Append folder name in second position
+      CONF_VAR_NAME="${SPLIT[0]} ${SECTION} ${CONF_VAR_NAME[@]:1}"
+      CONF_VAR_NAME=$(wex array/join -a="${CONF_VAR_NAME}" -s="_")
+      CONF_VAR_NAME=${CONF_VAR_NAME^^}
 
-      if [ "${CONF_VAR_NAME}" != false ];then
-        # Save as found
-        FOUND+=(${BASE_NAME})
-
-        # Return to array
-        CONF_VAR_NAME=(${CONF_VAR_NAME})
-        # Append folder name in second position
-        CONF_VAR_NAME="${SPLIT[0]} ${SECTION} ${CONF_VAR_NAME[@]:1}"
-        CONF_VAR_NAME=$(wex array/join -a="${CONF_VAR_NAME}" -s="_")
-        CONF_VAR_NAME=${CONF_VAR_NAME^^}
-
-        # Not already found.
-        echo "\nCONF_"${CONF_VAR_NAME}'='$(realpath ${FOLDER}'/'${FILE})
-      fi
+     # Not already found.
+      echo "\nCONF_"${CONF_VAR_NAME}'='$(realpath ${FOLDER}'/'${FILE})
     fi
   done
 }
