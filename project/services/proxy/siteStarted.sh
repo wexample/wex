c462872f93da7bc28bc9e13f362224ad828b18d1
Romain Weeger <romain.weeger@gmail.com>
2018-07-01 17:26:43 +0200

    FTP debug

--- project/services/proxy/siteStarted.sh
+++ project/services/proxy/siteStarted.sh
@@ -4,11 +4,14 @@ proxySiteStarted() {
   . ${WEX_WEXAMPLE_SITE_CONFIG}
 
   # Copy FTP access to FTP global container.
-  local FTP_PASSWD=./ftp/passwd/${SITE_NAME}.passwd
+  local FTP_PASSWD_LOCAL=./ftp/passwd/${SITE_NAME}.passwd
   # Access file exists.
-  if [ -f ${FTP_PASSWD} ];then
-    docker cp ${FTP_PASSWD} wex_ftp:/etc/pure-ftpd/passwd/${SITE_NAME}.passwd
+  if [ -f ${FTP_PASSWD_LOCAL} ];then
+    # File in ftp container.
+    local FTP_PASSWD=/etc/pure-ftpd/passwd/${SITE_NAME}.passwd
+    # Copy.
+    docker cp ${FTP_PASSWD_LOCAL} wex_ftp:${FTP_PASSWD}
     # Reload FTP service.
-    docker exec wex_ftp service pure-ftpd force-reload
+    docker exec wex_ftp chmod 600 ${FTP_PASSWD} && service pure-ftpd force-reload
   fi
 }
