9490cc012f6ac5cdfb99329b91205422b9e91bf3
Romain Weeger <romain.weeger@gmail.com>
2018-09-22 09:10:44 +0200

    Access rights

--- project/bash/default/frameworkComposer1/update.sh
+++ project/bash/default/frameworkComposer1/update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 frameworkComposer1Update() {
+  # TODO Agnostic path
+  # Files access
+  chown www-data:www-data /var/www/html/project/composer.lock
   chown -R www-data:www-data /var/www/html/project/vendor
   # Execute composer command as www-data user.
   su - www-data -s /bin/bash -c 'cd /var/www/html/project/ && composer install'
