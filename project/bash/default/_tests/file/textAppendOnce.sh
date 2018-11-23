5349b31d3226d1a9baaa0a4deee06f5b21796c31
Romain Weeger <romain.weeger@gmail.com>
2018-11-23 04:24:39 +0100

    wexTestSampleDiff

--- project/bash/default/_tests/file/textAppendOnce.sh
+++ project/bash/default/_tests/file/textAppendOnce.sh
@@ -1,28 +1,17 @@
 #!/usr/bin/env bash
 
 fileTextAppendOnceTest() {
-  filePath=$(wexTestSampleInit "fileTextSample1.txt")
+  filePath=$(wexTestSampleInit fileTextSample1.txt)
 
   original=$(< ${filePath})
 
   # Try to append a line which exists
   wex file/textAppendOnce -f=${filePath} -l="[INNER LINE]"
-  # No change expected
-  modified=$(< ${filePath})
-  differences=$(diff <(echo "${original}") <(echo "${modified}"))
-  if [[ ${differences} != '' ]]; then
-    wexTestError "Differences found tyring insert existing line"
-    echo ${differences}
-  fi
+  wexTestSampleDiff fileTextSample1.txt false "Trying insert existing line"
 
   # Try to append a line which looks like another on but not exactly
   wex file/textAppendOnce -f=${filePath} -l="\n[INNER LINE"
-  # File should have changed
-  modified=$(< ${filePath})
-  differences=$(diff <(echo "${original}") <(echo "${modified}"))
-  if [ ${#differences} == 0 ]; then
-    wexTestError "File not changed when expected"
-  fi
+  wexTestSampleDiff fileTextSample1.txt true "Line inserted"
 
   # Try to append config line
   configValue="true; # With an unsupported comment"
