5414006a5a486a8420313a4cb67f7be36aa979f9
Romain Weeger <romain.weeger@gmail.com>
2019-01-04 14:45:59 +0100

    sed i textReplace

--- project/bash/default/file/textReplace.sh
+++ project/bash/default/file/textReplace.sh
@@ -11,7 +11,8 @@ fileTextReplace() {
   # To linux lines ending
   ORIGINAL=$(wex file/convertLinesToUnix -f=${FILE})
   # Execute common sed
-  sed -i "${REGEX}" ${FILE}
+  sed -i"${WEX_SED_I_ORIG_EXT}" -e "${REGEX}" ${FILE}
+  rm ${FILE}"${WEX_SED_I_ORIG_EXT}"
   # Revert lines encoding format.
   wex file/convertLinesFormat -f=${FILE} -t=${ORIGINAL}
 }
