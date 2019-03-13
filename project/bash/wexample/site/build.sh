78b1905f527de4c4c9de1e651ac044ca17373ee2
Romain Weeger <romain.weeger@gmail.com>
2019-03-13 15:38:15 +0100

    Install && update

--- project/bash/wexample/site/build.sh
+++ project/bash/wexample/site/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 siteBuild() {
+  # Clear site cache.
+  wex cache/clear
+  # Rebuild assets.
   wex hook/exec -c=build
+  # Reset permissions.
+  wex site/perms
 }
---
+++ project/services/symfony4/install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# TODO Not tested
+symfony4install() {
+  . .env
+  . .wex
+
+  # Composer install / update.
+  local ACTION="update"
+  if [ "${SITE_ENV}" = "prod" ];then
+    ACTION="install"
+  fi;
+  wex site/exec -l -c="composer "${ACTION}
+
+  # NPM
+  local OPTION=""
+  if [ "${SITE_ENV}" = "prod" ];then
+    OPTION="--production"
+  fi;
+  wex site/exec -l -c="npm install "${OPTION}
+
+  # Encore.
+  if [ "${SITE_ENV}" = "dev" ] && [ $(wex site/exec -c="wex file/exists -f=/var/www/html/project/node_modules/.bin/encore") == true ];then
+    wex site/exec -l -c="yarn run encore dev"
+  fi
+
+  # Fill up Symfony .env file with db URL
+  wex config/setValue -f=./project/.env -k=DATABASE_URL -s="=" -v="mysql://root:${MYSQL_PASSWORD}@${NAME}_mysql:3306/${NAME}"
+
+  # Assets symlinks.
+  wex cli/exec -c="assets:install --symlink public"
+
+  # CKEditor install
+  if [ $(wex dir/exists -d="project/vendor/friendsofsymfony/ckeditor-bundle") == true ];then
+    wex cli/exec -c="ckeditor:install"
+  fi
+
+  # Rebuild and clear caches.
+  wex site/build
+}
