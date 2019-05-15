f04b1f130b047e93098d3cd3a5ed3f85f2c451c7
Lionel Péramo <contact@lionel-peramo.com>
2019-05-15 09:47:01 +0200

    sync wp core version at start

--- project/services/web/init.sh
+++ project/services/web/init.sh
@@ -3,4 +3,5 @@
 webInit() {
   . .wex
   sed -i 's/domain.com/'${PROD_DOMAIN_MAIN}'/g' ./apache/web.prod.conf
+  echo "WP_CORE_VERSION=4.9.5" >> .wex
 }
--- project/services/wordpress4/started.sh
+++ project/services/wordpress4/started.sh
@@ -5,4 +5,12 @@ wordpress4Started() {
   . ${WEX_DIR_ROOT}services/web/started.sh
 
   webStarted
+
+  . .wex
+
+  # There is a different core version.
+  if [ "${WP_CORE_VERSION}" != "" ] && [ "${WP_CORE_VERSION}" != $(wex wordpress/version) ];then
+    # Update core.
+    wex wordpress/changeCore -v=${WP_CORE_VERSION}
+  fi
 }
