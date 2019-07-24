8436b8d0d855b364a25ba90b46cd649d8178032c
Romain Weeger <romain.weeger@gmail.com>
2019-07-24 16:00:28 +0200

    Gitlab informations

---
+++ project/services/gitlab/siteMigrate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# TODO This script is not used, we may automate migration in the future.
+
+# Gitlab does not support properly to be copy / pasted from a server to another
+# Instead you should make a back file (wex gitlab/backup) then restore it on the new environment
+# /!\ You can't change the gitlab version during migration.
---
+++ project/services/gitlab/versionUpdate.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# TODO This script is not used, we may automate migration in the future.
+
+# To update Gitlab, just change the version of your actively running instance in the .wex file.
