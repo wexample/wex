0835a26c10764619ddb19039e0dc547c8e2851ea
Romain Weeger <romain.weeger@gmail.com>
2019-07-28 21:10:39 +0200

    fix db/exec

--- project/bash/wexample/db/exec.sh
+++ project/bash/wexample/db/exec.sh
@@ -9,6 +9,6 @@ dbExecArgs() {
 dbExec() {
   . ${WEX_WEXAMPLE_SITE_CONFIG}
   local LOGIN=$(wex mysql/loginCommand)
-  local QUERY="mysql --defaults-extra-file=/var/www/tmp/mysql.cnf -s -N ${SITE_NAME} -e \""${COMMAND}"\""
+  local QUERY="mysql --defaults-extra-file=/var/www/tmp/mysql.cnf -s -N ${SITE_NAME_INTERNAL} -e \""${COMMAND}"\""
   docker exec ${SITE_NAME}_mysql sh -c "${QUERY}"
 }
