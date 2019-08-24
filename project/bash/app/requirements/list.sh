73b884b5f7f2464c7e28446d0e39238db6376be7
Romain Weeger <romain.weeger@gmail.com>
2019-08-24 11:40:10 +0200

    app requirements list

---
+++ project/bash/app/requirements/list.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+requirementsListArgs() {
+  _DESCRIPTION='Display installed requirement for some wex features'
+}
+
+requirementsList() {
+  . ${WEX_DIR_BASH}colors.sh
+
+  _requirementsListRow $(wex package/exists -n docker) "docker" "Core containers manager"
+  _requirementsListRow $(wex package/exists -n zip) "zip" "Use for backups management"
+}
+
+_requirementsListRow() {
+  local EXISTS=${1}
+  local NAME=${2}
+  local DESC=${3}
+
+  if [ "${EXISTS}" == "true" ];then
+    local ICON=${WEX_COLOR_GREEN}✓
+  else
+    local ICON=${WEX_COLOR_RED}x
+  fi
+
+  printf "   "${ICON}${WEX_COLOR_RESET}"\t${NAME}\t${WEX_COLOR_CYAN}${DESC}${WEX_COLOR_RESET}\n"
+}
