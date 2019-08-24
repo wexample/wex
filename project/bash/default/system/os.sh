e3bb97a6439d723b2ad28eddf663c906cdde8c49
Romain Weeger <romain.weeger@gmail.com>
2019-08-24 12:32:54 +0200

    Various install methods

---
+++ project/bash/default/system/os.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+systemOs() {
+  case "$(uname -s)" in
+    Darwin)
+      echo 'mac'
+      ;;
+    Linux)
+      echo 'linux'
+      ;;
+    CYGWIN*|MINGW32*|MINGW64*|MSYS*)
+      echo 'windows'
+      ;;
+    # Add here more strings to compare
+    # See correspondence table at the bottom of this answer
+    *)
+      echo 'other OS'
+      ;;
+  esac
+}
