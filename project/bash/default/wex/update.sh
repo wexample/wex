85efedf385cec54cdaf8206e38bb68e3488eebe5
Romain Weeger <romain.weeger@gmail.com>
2019-08-25 11:55:42 +0200

    Allow to update to specific branch

--- project/bash/default/wex/update.sh
+++ project/bash/default/wex/update.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 
+wexUpdateArgs() {
+  _DESCRIPTION="Update core"
+  _ARGUMENTS=(
+    [0]='branch b "Switch to specified branch, by default stay on current one" false'
+  )
+}
+
 wexUpdate() {
   # Git expected.
   GIT_EXISTS=$(wex package/exists -n=git)
+
   if [[ ${GIT_EXISTS} == true ]];then
+
     # Go to wexample install dir.
     cd ${WEX_DIR_ROOT}../
-    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    # Choose branch
+    if [ "${BRANCH}" == "" ];then
+      BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    fi;
+
     # Override changes and pull.
     wex git/resetHard
     git fetch --tags
+
     # Get only last data
     git pull origin ${BRANCH}
     git update-index --chmod=+x
+
     # Info
     echo "Wexample is up-to-date at v"$(wex wex/version)
   fi
