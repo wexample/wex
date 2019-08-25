dd12db8f50ce24ed79e161b0e2ac399e54e43cf1
Romain Weeger <romain.weeger@gmail.com>
2019-08-25 18:37:10 +0200

    Update

--- project/migration/3.1.sh
+++ project/migration/3.1.sh
@@ -8,8 +8,14 @@ _wexMigrateCore() {
 }
 
 _wexMigrateApp() {
-  echo "APP"
+  # Replace ${SITE_NAME} by ${SITE_NAME_INTERNAL}
+  # into docker compose files.
+  if [ -d ./docker/ ];then
+    local YMLS=($(ls ./docker/*.yml))
 
-  # TODO SITE_NAME_INTERNAL
-  # TODO site/test to app/test into script folder
+    for YML in "${YMLS[@]}"
+    do
+      wex file/textReplace -f=${YML} -r=s/\$\{SITE_NAME\}/\$\{SITE_NAME_INTERNAL\}/g
+    done
+  fi
 }
