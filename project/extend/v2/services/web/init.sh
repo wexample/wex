c78002b44b8e9cf377a5f22718fed7c63e075710
Romain Weeger <romain.weeger@gmail.com>
2019-09-02 17:31:49 +0200

    macos support

--- project/extend/v2/services/web/init.sh
+++ project/extend/v2/services/web/init.sh
@@ -2,6 +2,6 @@
 
 webInit() {
   . .wex
-  sed -i${WEX_DIR_TMP_BKP} 's/domain.com/'${PROD_DOMAIN_MAIN}'/g' ./apache/web.prod.conf
+  sed -i"${WEX_SED_I_ORIG_EXT}" 's/domain.com/'${PROD_DOMAIN_MAIN}'/g' ./apache/web.prod.conf
   echo "WP_CORE_VERSION=4.9.5" >> .wex
 }
