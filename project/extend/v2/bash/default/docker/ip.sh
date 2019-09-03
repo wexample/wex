4fe8b4ae54a3e10b852e4cf5b395d1004b832831
weeger <romain.weeger@gmail.com>
2019-09-03 17:01:58 +0200

    Backport docker/ip to v2

--- project/extend/v2/bash/default/docker/ip.sh
+++ project/extend/v2/bash/default/docker/ip.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 dockerIp() {
+  # Docker IP is only localhost
+  if [[ $(wex system/osName) == 'mac' ]];then
+    echo "localhost"
+    return
+  fi
+
   if [[ $(command -v docker-machine) ]];then
     echo $(docker-machine ip)
   else
