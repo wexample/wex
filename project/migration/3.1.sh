a5ed0c2f5cee129ab4d71a174f53129b3d9a02dd
Romain Weeger <romain.weeger@gmail.com>
2019-09-10 14:26:07 +0200

    Site name internal comes back

--- project/migration/3.1.sh
+++ project/migration/3.1.sh
@@ -9,16 +9,15 @@ _wexMigrateCore() {
   bash /opt/wex/install
 }
 
-# TODO Disabled until migration of all site/* to app/*
-#_wexMigrateApp() {
+_wexMigrateApp() {
   # Replace ${SITE_NAME} by ${SITE_NAME_INTERNAL}
   # into docker compose files.
-  #if [ -d ./docker/ ];then
-    #local YMLS=($(ls ./docker/*.yml))
+  if [ -d ./docker/ ];then
+    local YMLS=($(ls ./docker/*.yml))
 
-    #for YML in "${YMLS[@]}"
-    #do
-    #  wex file/textReplace -f=${YML} -r=s/\$\{SITE_NAME\}/\$\{SITE_NAME_INTERNAL\}/g
-    #done
-  #fi
-#}
+    for YML in "${YMLS[@]}"
+    do
+      wex file/textReplace -f=${YML} -r=s/\$\{SITE_NAME\}/\$\{SITE_NAME_INTERNAL\}/g
+    done
+  fi
+}
