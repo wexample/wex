afcd19f622597f1beeeda2fe57294a5e72d5a809
Romain Weeger <romain.weeger@gmail.com>
2019-09-10 15:35:09 +0200

    Fix file access

--- project/migration/3.1.sh
+++ project/migration/3.1.sh
@@ -8,16 +8,3 @@ _wexMigrateCore() {
   # Reinstall.
   bash /opt/wex/install
 }
-
-_wexMigrateApp() {
-  # Replace ${SITE_NAME} by ${SITE_NAME_INTERNAL}
-  # into docker compose files.
-  if [ -d ./docker/ ];then
-    local YMLS=($(ls ./docker/*.yml))
-
-    for YML in "${YMLS[@]}"
-    do
-      wex file/textReplace -f=${YML} -r=s/\$\{SITE_NAME\}/\$\{SITE_NAME_INTERNAL\}/g
-    done
-  fi
-}
