70e844a5167d001e586ab6f0129eb7aed1a14882
Romain Weeger <romain.weeger@gmail.com>
2019-09-11 15:40:22 +0200

    Messages changes

--- project/extend/v2/services/mysql/started.sh
+++ project/extend/v2/services/mysql/started.sh
@@ -3,7 +3,7 @@
 mysqlStarted() {
   . ${WEX_WEXAMPLE_SITE_CONFIG}
 
-  _wexMessage "Set MySQL site.cnf access"
+  _wexMessage "Lock MySQL site.cnf access"
 
   docker exec ${SITE_NAME_INTERNAL}_mysql chmod a-rw /var/www/tmp/mysql.cnf
   docker exec ${SITE_NAME_INTERNAL}_mysql chmod a-rw /etc/mysql/conf.d/site.cnf
--- project/extend/v2/services/mysql/stop.sh
+++ project/extend/v2/services/mysql/stop.sh
@@ -3,7 +3,7 @@
 mysqlStop() {
   . ${WEX_WEXAMPLE_SITE_CONFIG}
 
-  _wexMessage "Set MySQL site.cnf access"
+  _wexMessage "Free MySQL site.cnf access"
 
   docker exec ${SITE_NAME_INTERNAL}_mysql chmod 777 /var/www/tmp/mysql.cnf
   docker exec ${SITE_NAME_INTERNAL}_mysql chmod 777 /etc/mysql/conf.d/site.cnf
