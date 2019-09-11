8afc53bfc4848cae332303e4353aeda20085b68d
Romain Weeger <romain.weeger@gmail.com>
2019-09-11 15:17:54 +0200

    Better laravel watcher start

--- project/extend/v2/services/laravel5/watcherStart.sh
+++ project/extend/v2/services/laravel5/watcherStart.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-wex site/exec -l -c="yarn watch"
+laravel5WatcherStart() {
+  local WATCH="yarn watch"
+
+  # Try to use yarn on host if exists,
+  # it improve compatibility with editors
+  # and performance improvement.
+  if [ $(wex package/exists -n=yarn) == true ];then
+    cd ./project
+    ${WATCH}
+  else
+    _wexMessage "To improve performance an compatibility you may install yarn to your host machine" "It will be as file watcher"
+    wex site/exec -l -c="${WATCH}"
+  fi
+}
