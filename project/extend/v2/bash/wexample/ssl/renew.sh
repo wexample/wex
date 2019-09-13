65ad6ece2640b6fa02c573e658b2fcbf85044632
Romain Weeger <romain.weeger@gmail.com>
2019-09-13 09:16:59 +0200

    Fix ssl/renew

--- project/extend/v2/bash/wexample/ssl/renew.sh
+++ project/extend/v2/bash/wexample/ssl/renew.sh
@@ -3,10 +3,12 @@
 sslRenew() {
   # Start server.
   if [ $(wex server/started) == false ];then
-    wex server/start
+    return
   fi
+  . ${WEX_WEXAMPLE_DIR_PROXY} .env
+  local PROXY_NAME=${WEX_WEXAMPLE_PROXY_CONTAINER}_${SITE_ENV}
   # SSL container may sleep.
-  docker start ${WEX_WEXAMPLE_PROXY_CONTAINER}_certs
+  docker start ${PROXY_NAME}_certs
   # Force renew.
-  docker exec ${WEX_WEXAMPLE_PROXY_CONTAINER}_certs /app/force_renew
+  docker exec ${PROXY_NAME}_certs /app/force_renew
 }
