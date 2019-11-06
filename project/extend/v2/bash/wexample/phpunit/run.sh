e7193ee31e6d5adfe661120ed53788a0dea6b423
Romain Weeger <romain.weeger@gmail.com>
2019-11-06 17:47:49 +0100

    phpunit run improvement

--- project/extend/v2/bash/wexample/phpunit/run.sh
+++ project/extend/v2/bash/wexample/phpunit/run.sh
@@ -20,5 +20,11 @@ phpunitRun() {
     local FILTER="--filter ${METHOD} ${CLASS} ../${FILE}"
   fi
 
-  wex site/exec -l --non_interactive -c="./vendor/bin/phpunit ${FILTER}"
+  local TMP_DIR=/var/www/html/tmp/
+
+  mkdir -p ./tmp/phpunit
+
+  # TODO Not working with older versions but should be tested on recent installs :
+  # --log-junit ${TMP_DIR}phpunit-report.xml --coverage-html ${TMP_DIR}phpunit
+  wex site/exec -l --non_interactive -c="./vendor/bin/phpunit ${FILTER} "
 }
