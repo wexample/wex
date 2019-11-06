4166dda5f618e1fa7759676aa48bb1c8c390edea
Romain Weeger <romain.weeger@gmail.com>
2019-11-06 17:53:32 +0100

    Useless code

--- project/extend/v2/services/web/init.sh
+++ project/extend/v2/services/web/init.sh
@@ -4,6 +4,4 @@ webInit() {
   . .wex
   sed -i"${WEX_SED_I_ORIG_EXT}" 's/domain.com/'${PROD_DOMAIN_MAIN}'/g' ./apache/web.prod.conf
   rm ./apache/web.prod.conf${WEX_SED_I_ORIG_EXT}
-
-  echo "WP_CORE_VERSION=4.9.5" >> .wex
 }
