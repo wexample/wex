97a872efb53ab262e116519149073b4eb3527021
Romain Weeger <romain.weeger@gmail.com>
2020-02-27 13:41:49 +0100

    Drafts and todos

--- project/extend/draft/openFtpPort.sh
+++ project/extend/draft/openFtpPort.sh
@@ -1,10 +1,14 @@
 
 # Open 21
 ufw allow 21/tcp
+# Or range : ufw allow 30000:30059/tcp
 
 # Reload (may broke ssh connexion)
 ufw disable
 ufw enable
+
+# Check status
+ufw status verbose
 
 # Now, restart containers to listen port again
 # You can test with :
