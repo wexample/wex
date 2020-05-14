53c4c190735d8c06cc526bff98eb4acdc38a6c6d
Romain Weeger <romain.weeger@gmail.com>
2020-05-14 14:00:47 +0200

    Laravel cache clear

--- project/extend/v2/services/laravel5/cacheClear.sh
+++ project/extend/v2/services/laravel5/cacheClear.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 laravel5CacheClear() {
+  wex site/exec -l -c="php artisan config:clear"
   wex site/exec -l -c="php artisan cache:clear"
+  wex site/exec -l -c="php artisan route:clear"
 }
