a35634f38e1a7511882adacd46e61a0b4ef5b962
Romain Weeger <romain.weeger@gmail.com>
2020-07-09 09:10:23 +0200

    Improve code format command

--- project/bash/app/code/format.sh
+++ project/bash/app/code/format.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 codeFormatArgs() {
+  _DESCRIPTION="Launch the code formating command for the app"
   _ARGUMENTS=(
     [0]='commit c "Commit changes" false'
   )
@@ -10,6 +11,10 @@ codeFormat() {
   wex hook/exec -c=codeFormat
 
   if [ "${COMMIT}" != "" ];then
+    # Go to the project folder in case of root dir
+    # has not the same git repo as the project content.
+    cd ${WEX_WEXAMPLE_APP_DIR_PROJECT}
+
     git commit -am "Code auto format"
   fi
 }
--- project/bash/app/init.sh
+++ project/bash/app/init.sh
@@ -3,5 +3,6 @@
 # Place here namespace specific data
 # that should not be accessed by other namespaces.
 
-export WEX_WEXAMPLE_SITE_DIR_TMP=./tmp/
-export WEX_WEXAMPLE_SITE_CONFIG=${WEX_WEXAMPLE_SITE_DIR_TMP}config
+export WEX_WEXAMPLE_APP_DIR_TMP=./tmp/
+export WEX_WEXAMPLE_APP_DIR_PROJECT=./project/
+export WEX_WEXAMPLE_APP_CONFIG=${WEX_WEXAMPLE_APP_DIR_TMP}config
