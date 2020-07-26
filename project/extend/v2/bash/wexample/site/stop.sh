3361b6fea06c2ff5b94c177febe1d80626a7afb5
Romain Weeger <romain.weeger@gmail.com>
2020-07-26 19:40:29 +0200

    Minor fixes

--- project/extend/v2/bash/wexample/site/stop.sh
+++ project/extend/v2/bash/wexample/site/stop.sh
@@ -4,7 +4,7 @@ siteStop() {
   # Execute services scripts if exists
   wex hook/exec -c="stop"
   # Write config file, indicates started=stop and recreate yml file if missing.
-  wex config/write -s=false
+  wex config/write -s=false -nr
   # Use previously generated yml file.
   docker-compose -f ${WEX_WEXAMPLE_SITE_COMPOSE_BUILD_YML} down
   # Reload file
@@ -12,5 +12,5 @@ siteStop() {
   # Rebuild hosts
   wex hosts/update
   # Execute services scripts if exists
-  wex hook/exec -c="stopped"
+  wex hook/exec -c="appStopped"
 }
