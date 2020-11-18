50f82659ce084fbba95cdb88a2f7c15b753ecbe4
root <root@vps-0c95847a.vps.ovh.net>
2020-11-18 10:58:21 +0100

    fix ssl renew

--- project/extend/v2/bash/wexample/ssl/renew.sh
+++ project/extend/v2/bash/wexample/ssl/renew.sh
@@ -5,7 +5,7 @@ sslRenew() {
   if [ $(wex server/started) == false ];then
     return
   fi
-  . ${WEX_WEXAMPLE_DIR_PROXY} .env
+  . ${WEX_WEXAMPLE_DIR_PROXY}.env
   local PROXY_NAME=${WEX_WEXAMPLE_PROXY_CONTAINER}_${SITE_ENV}
   # SSL container may sleep.
   docker start ${PROXY_NAME}_certs
