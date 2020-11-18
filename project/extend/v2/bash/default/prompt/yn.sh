8600f4496be0a4ff0077aad004ea409daf00c0f3
Romain Weeger <romain.weeger@gmail.com>
2020-11-18 14:21:35 +0100

    Improve prompt

--- project/extend/v2/bash/default/prompt/yn.sh
+++ project/extend/v2/bash/default/prompt/yn.sh
@@ -2,16 +2,18 @@
 
 promptYnArgs() {
   _ARGUMENTS=(
-    [0]='question q "Question to ask user" true'
+    'question q "Question to ask user" true'
   )
 }
 
 promptYn() {
   while true; do
-    read -p "${QUESTION} [Y/n]: " yn
+    read -rp "$(echo -e "${WEX_COLOR_CYAN}${QUESTION} ${WEX_COLOR_GRAY}[Y/n]${WEX_COLOR_RESET}: ")" yn
     case $yn in
         [Nn]* ) echo false; break;;
-        * ) echo true; break;;
+        [Yy]* ) echo true; break;;
+        "" ) echo true; break;;
+        * ) wex prompt/yn -q="${QUESTION}"; break;;
     esac
   done
 }
