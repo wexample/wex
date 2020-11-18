51b909f33adfcab162dea121772b86993ea0f86a
Romain Weeger <romain.weeger@gmail.com>
2020-11-18 11:24:59 +0100

    Support custom domains

--- project/extend/v2/services/phpmyadmin/config.sh
+++ project/extend/v2/services/phpmyadmin/config.sh
@@ -4,4 +4,16 @@ phpmyadminConfig() {
   # Need php/phpmyadmin.ini even
   # if web container does not exists
   wex service/templates -s=php -e=ini
+
+  . .env
+  . .wex
+
+  local SITE_ENV_MAJ=${SITE_ENV^^}
+  local DOMAIN=$(eval 'echo ${'${SITE_ENV_MAJ}'_DOMAIN_PMA}')
+
+  if [ "${DOMAIN}" = '' ] && [ "${SITE_ENV}" = "local" ];then
+    DOMAIN=pma.${SITE_NAME}.wex
+  fi
+
+  echo "\nDOMAIN_PMA=${DOMAIN}"
 }
