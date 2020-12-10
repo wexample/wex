287baa5f4214d67c418fc6ea5266de841dea2309
Romain Weeger <romain.weeger@gmail.com>
2020-12-10 11:02:34 +0100

    WP update

--- project/bash/app/wordpress/update.sh
+++ project/bash/app/wordpress/update.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 wordpressUpdate() {
+  # TODO remove -l option when migrated.
   wex site/exec -l -c="wp core update --allow-root"
   wex site/exec -l -c="wp plugin update --all --allow-root"
   wex site/exec -l -c="wp core update-db --allow-root"
+  wex site/exec -l -c="wp core update-db --network --allow-root"
   wex site/exec -l -c="wp theme update --all --allow-root"
   wex site/exec -l -c="wp language core update --allow-root"
-  # Woo commerce db
-  # wex site/exec -l -c="wp wc update --allow-root"
 }
