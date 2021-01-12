92e6efd1ab78f9544757e9cf57dbc505fdc2a685
Romain Weeger <romain.weeger@gmail.com>
2021-01-12 12:58:45 +0100

    Rollback docker/stopAll

---
+++ project/bash/default/docker/stopAll.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+dockerStopAllArgs() {
+  _DESCRIPTION="Stops every running Docker container / network / volume"
+}
+
+dockerStopAll() {
+  # Containers
+  local CONTAINERS
+  CONTAINERS=($(docker ps -qa))
+  if (( ${#CONTAINERS[@]} > 0 ));then
+    # Stop all
+    docker stop "${CONTAINERS[@]}"
+    # Remove
+    docker rm "${CONTAINERS[@]}" -f
+  fi;
+
+  # Networks
+  # List all networks except builtin
+  local NETWORKS
+  NETWORKS=($(docker network ls -q --filter type=custom))
+  if (( ${#NETWORKS[@]} > 0 ));then
+    # Remove networks
+    docker network rm "${NETWORKS[@]}"
+  fi;
+
+  # Volumes
+  local VOLUMES
+  VOLUMES=$(docker volume ls -q)
+
+  if (( ${#VOLUMES[@]} > 1 ));then
+    docker volume rm "${VOLUMES[@]}"
+  fi;
+}
