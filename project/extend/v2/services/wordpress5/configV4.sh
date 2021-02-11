1d6d665430fae98a92eae9c06e06846e6ef52cd2
Romain Weeger <romain.weeger@gmail.com>
2021-02-11 14:46:03 +0100

    Various WP config

--- project/extend/v2/services/wordpress5/configV4.sh
+++ project/extend/v2/services/wordpress5/configV4.sh
@@ -2,7 +2,11 @@
 
 wordpress5ConfigV4() {
   # TODO Import and rewrite v3 config.
-  echo "  > Wordpress container set to wordpress5"
+  echo "  > Set SITE_CONTAINER to wordpress5"
   # Change main container name only.
   wex config/changeValue -f=tmp/config -k=SITE_CONTAINER -v="wordpress5" -s="="
+
+  . .wex
+  # Set values
+  wex config/setValue -f=tmp/config -k=WP_DB_TABLE_PREFIX -v="${WP_DB_TABLE_PREFIX}" -s="="
 }
--- project/extend/v2/services/wordpress5/started.sh
+++ project/extend/v2/services/wordpress5/started.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
 wordpress5Started() {
-    # Same config as web
-  . ${WEX_DIR_ROOT}services/web/started.sh
-
-  webStarted
-
   . .wex
 
-  # There is a different core version.
-  if [ "${WP_CORE_VERSION}" != "" ] && [ "${WP_CORE_VERSION}" != "$(wex wordpress/version)" ];then
-    _wexLog "Upgrading wordpress core to ${WP_CORE_VERSION}"
-    # Update core.
-    wex wordpress/changeCore -v="${WP_CORE_VERSION}"
-  fi
-
-  wex config/changeValue -f=.wex -k=WP_CORE_VERSION -v="${WP_CORE_VERSION}" -s
+  wex site/serve
 }
