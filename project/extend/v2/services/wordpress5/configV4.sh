068164ef5efb132e078f93b954f14c33011ad5ee
Romain Weeger <romain.weeger@gmail.com>
2021-02-13 17:50:02 +0100

    Fix config write error

--- project/extend/v2/services/wordpress5/configV4.sh
+++ project/extend/v2/services/wordpress5/configV4.sh
@@ -2,7 +2,7 @@
 
 wordpress5ConfigV4() {
   # TODO Import and rewrite v3 config.
-  _wexLog "Set SITE_CONTAINER to wordpress5"
+  echo "Set SITE_CONTAINER to wordpress5"
   # Change main container name only.
   wex config/changeValue -f=tmp/config -k=SITE_CONTAINER -v="wordpress5" -s="="
 
