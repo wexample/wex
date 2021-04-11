9c632d8274bedaa19e9bda8cc9832d9ee05e59fc
Romain Weeger <romain.weeger@gmail.com>
2021-04-11 12:43:21 +0200

    Prettier code formatter

--- project/extend/v2/services/web/codeFormat.sh
+++ project/extend/v2/services/web/codeFormat.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 webCodeFormat()  {
+  cd './project' || return;
+  yarn prettier --write .
+  cd '../' || return;
+
   wex site/exec -l -c="./vendor/bin/php-cs-fixer fix --config ./.php_cs.dist"
 }
