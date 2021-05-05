f47f2f7e2f92b753a094beffa238ef6b3275c227
root <root@ns3018940.ip-151-80-23.eu>
2021-05-05 14:37:35 +0000

    Sonar fixes

--- project/extend/v2/services/sonarqube/perms.sh
+++ project/extend/v2/services/sonarqube/perms.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 sonarqubePerms() {
-  wex site/exec -c="chown -R www-data:www-data /var/www/html/project/src"
+  echo "No sonar perms change..."
+  #wex site/exec -c="chown -R www-data:www-data /var/www/html/project/src"
 }
