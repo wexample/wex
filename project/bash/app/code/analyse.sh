27584e07f62c4f7c23abfdf8db79ae19a1c6429e
Romain Weeger <romain.weeger@gmail.com>
2021-05-28 16:45:17 +0200

    Todo

--- project/bash/app/code/analyse.sh
+++ project/bash/app/code/analyse.sh
@@ -8,7 +8,16 @@ codeAnalyseArgs() {
 }
 
 codeAnalyse() {
+  # TODO use new version
+  # https://docs.sonarqube.org/latest/analysis/scan/sonarscanner/
+  #docker run \
+  #    --rm \
+  #    -e SONAR_HOST_URL="https://sonar.wexample.com" \
+  #    -e SONAR_LOGIN="f298f259bcf263ca75f307ad9ef8cbcafc619669" \
+  #    -v "${pwd}:/usr/src" \
+  #    sonarsource/sonar-scanner-cli
+
   # Use sonar scanner.
-  docker run -ti -v $(pwd):/usr/src newtmitch/sonar-scanner \
-      -Dproject.settings=./sonarqube/${NAME}.properties
+  echo docker run -ti -v $(pwd):/usr/src newtmitch/sonar-scanner \
+      -Dproject.settings=/usr/src/sonarqube/${NAME}.properties
 }
