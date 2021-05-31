ee849a61d9cf8aecf338c984203001b90c47f1e5
Romain Weeger <romain.weeger@gmail.com>
2021-05-31 08:42:29 +0200

    N8n auth and domain

--- project/extend/v2/services/n8n/config.sh
+++ project/extend/v2/services/n8n/config.sh
@@ -5,4 +5,6 @@ n8nConfig() {
   # Override default container.
   echo "\nSITE_CONTAINER=n8n"
   echo "\nN8N_VERSION="${N8N_VERSION}
+  echo "\nN8N_BASIC_AUTH_USER="${N8N_BASIC_AUTH_USER}
+  echo "\nN8N_GENERIC_TIMEZONE="${N8N_GENERIC_TIMEZONE}
 }
