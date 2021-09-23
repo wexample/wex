585cf28cf6caf308ad43e8d9bc26810ea3776cc7
weeger <romain.weeger@gmail.com>
2021-09-23 11:22:07 +0200

    Prepare php fpm in web service

--- project/extend/v2/services/web/started.sh
+++ project/extend/v2/services/web/started.sh
@@ -7,5 +7,13 @@ webStarted() {
   echo "Waiting for apache restart..."
   sleep 5
   # Then reload apache (SSL certs)
-  wex apache/restart
+  sudo wex site/exec -c="service apache2 restart"
+
+  # TODO for PHP FPM we should launch service.
+  #  sudo wex site/exec -c="a2enmod proxy_fcgi setenvif"
+  #  sudo wex site/exec -c="a2dismod php8.0"
+  #  sudo wex site/exec -c="a2enconf php8.0-fpm"
+  #
+  #  sudo wex site/exec -c="service php8.0-fpm start"
+  #  sudo wex site/exec -c="service apache2 restart"
 }
