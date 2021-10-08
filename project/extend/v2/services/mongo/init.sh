13c5ff11ece304f0a20613c61a3c2e531270732f
weeger <romain.weeger@gmail.com>
2021-10-08 09:11:26 +0200

    Mongo version

--- project/extend/v2/services/mongo/init.sh
+++ project/extend/v2/services/mongo/init.sh
@@ -3,6 +3,9 @@
 mongoInit() {
   wex site/start -c=mongo
 
+  # Override default container.
+  echo "MONGO_VERSION=5.0.3-focal" >> .wex
+
   echo "Wait 20 seconds for database fill.."
 
   wex site/stop
