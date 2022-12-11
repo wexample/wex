28c91b813211ef536d0a457bdce04b97cb1c9306
weeger <romain.weeger@gmail.com>
2022-12-11 17:03:06 +0100

    Unit testing

--- project/tests/bash/default/config/changeValue.sh
+++ project/tests/bash/default/config/changeValue.sh
@@ -8,45 +8,57 @@ configChangeValueTest() {
   RESULT=$(wex config/processSeparator -s=",")
   _wexTestAssertEqual "${RESULT}" "\(,\)\{1,\}"
 
-  # No separator
+  # No SEPARATOR
   RESULT=$(wex config/getValue -f="${FILEPATH}" -k="ConfigTestOption")
   _wexTestAssertEqual "${RESULT}" "two"
 
-  # Space separator
+  # Space SEPARATOR
   configChangeValueTestItem "${FILEPATH}" "ConfigTestOption" " "
 
-  # Strict equal separator
+  # Revert file.
+  FILEPATH=$(_wexTestSampleInit configSample)
+
+  # Strict equal SEPARATOR
   configChangeValueTestItem "${FILEPATH}" "ConfigTestOptionEqual" "="
 
   # Revert file.
   FILEPATH=$(_wexTestSampleInit configSample)
 
-  configChangeValueTestItem "${FILEPATH}" "ChallengeResponseAuthentication"
+  configChangeValueTestItem "${FILEPATH}" "ChallengeResponseAuthentication" " "
 
   FILEPATH=$(_wexTestSampleInit sshd_config)
   configChangeValueTestItem "${FILEPATH}" Port " "
 }
 
 configChangeValueTestItem() {
-  FILEPATH=${1}
-  variableName=${2}
-  separator=${3}
-  expected="tested"
+  local FILEPATH="${1}"
+  local NAME="${2}"
+  local SEPARATOR="${3}"
+  local EXPECTED="tested"
+  local ORIGINAL
+  local CHANGED
 
   # Backup
-  original=$(wex config/getValue -f="${FILEPATH}" -k="${variableName}" -s="${separator}")
+  ORIGINAL=$(wex config/getValue -f="${FILEPATH}" -k="${NAME}" -s="${SEPARATOR}")
 
   # Set value.
-  wex config/changeValue -f=${FILEPATH} -k=${variableName} -v="${expected}" -s="${separator}"
-  # Get value
-  changed=$(wex config/getValue -f="${FILEPATH}" -k="${variableName}" -s="${separator}")
-  # Check
-  _wexTestAssertEqual "${changed}" "${expected}"
+  wex config/changeValue -f="${FILEPATH}" -k="${NAME}" -v="${EXPECTED}" -s="${SEPARATOR}"
+  configChangeValueTestItemCheck "${EXPECTED}"
 
   # Revert
-  wex config/changeValue -f=${FILEPATH} -k=${variableName} -v="${original}" -s="${separator}"
-  # Get value
-  changed=$(wex config/getValue -f="${FILEPATH}" -k="${variableName}" -s="${separator}")
+  wex config/changeValue -f="${FILEPATH}" -k="${NAME}" -v="${ORIGINAL}" -s="${SEPARATOR}"
+  configChangeValueTestItemCheck "${ORIGINAL}"
+
+  # Remove
+  wex config/removeKey -f="${FILEPATH}" -k="${NAME}" -s="${SEPARATOR}"
+  configChangeValueTestItemCheck ""
+
+  # Reset
+  wex config/setValue -f="${FILEPATH}" -k="${NAME}" -v="${ORIGINAL}" -s="${SEPARATOR}"
+  configChangeValueTestItemCheck "${ORIGINAL}"
+}
+
+configChangeValueTestItemCheck() {
   # Check
-  _wexTestAssertEqual "${changed}" "${original}"
+  _wexTestAssertEqual "$(wex config/getValue -f="${FILEPATH}" -k="${NAME}" -s="${SEPARATOR}")" "${1}"
 }
