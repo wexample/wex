0653d04fe35409be9020e983d08c5fe83ccef7fb
weeger <romain.weeger@gmail.com>
2022-12-12 16:36:03 +0100

    Config manipulation

---
+++ project/bash/default/config/setValue.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+configSetValueArgs() {
+ _ARGUMENTS=(
+   'key k "Target key to change" true'
+   'separator s "Separator like space or equal sign, default space" false " "'
+   'file f "File" true'
+   'ignore_duplicates i "Do not check if variable exists or is commented" false false'
+   'value v "New value" true'
+ )
+}
+
+configSetValue() {
+  # Empty separator
+  if [ "${SEPARATOR}" = "" ];then
+    # Default space separator
+    SEPARATOR=" "
+  fi;
+
+  if [ "${IGNORE_DUPLICATES}" = "true" ];then
+    # Add value without checks.
+    echo -e "${KEY}${SEPARATOR}${VALUE}" >> "${FILE}"
+    return
+  fi
+
+  local HAS_VALUE;
+  local HAS_VALUE_COMMENTED
+
+  # Set Value
+  # Search for a commented version
+  HAS_VALUE_COMMENTED=$(wex config/keyExists -f="${FILE}" -k="${KEY}" -s="${SEPARATOR}" -co)
+
+  if [ "${HAS_VALUE_COMMENTED}" = "true" ];then
+    # Uncomment line(s)
+    wex config/uncomment -f="${FILE}" -k="${KEY}" -s="${SEPARATOR}"
+    HAS_VALUE=true
+  else
+    # Search for an existing version
+    HAS_VALUE=$(wex config/keyExists -f="${FILE}" -k="${KEY}" -s="${SEPARATOR}")
+  fi;
+
+  if [ "${HAS_VALUE}" = "true" ];then
+    # Change value(s)
+    wex config/changeValue -f="${FILE}" -k="${KEY}" -v="${VALUE}" -s="${SEPARATOR}"
+  else
+    # Add a new line
+    wex file/textAppendOnce -f="${FILE}" -l="${KEY}${SEPARATOR}${VALUE}"
+  fi
+}
