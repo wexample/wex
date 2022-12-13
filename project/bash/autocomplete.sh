46c5a59dd53ab962ae442429375514b7b31f6551
weeger <romain.weeger@gmail.com>
2022-12-13 22:00:44 +0100

    Create cache for autocompletion

--- project/bash/autocomplete.sh
+++ project/bash/autocomplete.sh
@@ -1,43 +1,60 @@
 #!/usr/bin/env bash
 
 autocomplete() {
-  . /opt/wex/project/includes/globals.sh
-
+  local CHECKSUM
   local CUR=${COMP_WORDS[${COMP_CWORD}]}
-  local LOCATIONS
+  local WEX_DIR_ROOT
+  local WEX_DIR_TMP_AUTOCOMPLETE
   local SUGGESTIONS=''
-  local ARGS_INDEX=2
 
-  LOCATIONS=$(_wexFindScriptsLocations)
+  CHECKSUM=$(echo "${COMP_WORDS[@]}" | md5sum | grep -o '^\S\+')
+  WEX_DIR_ROOT="$(dirname "$(dirname "${BASH_SOURCE[0]}")")/"
 
-  # We are on the "group/name" section.
-  if (( COMP_CWORD < ARGS_INDEX ));then
-    # Search into extend directories.
-    for LOCATION in ${LOCATIONS[@]}
-    do
-      local SCRIPTS=$(wex scripts/list -d="${LOCATION}")
+  WEX_DIR_TMP_AUTOCOMPLETE="${WEX_DIR_ROOT}tmp/cache/autocomplete"
+  WEX_FILE_CACHE="${WEX_DIR_TMP_AUTOCOMPLETE}/${CHECKSUM}"
 
-      for SCRIPT in ${SCRIPTS[@]}
-      do
-        if [[ "${SCRIPT}" == ${CUR}* ]];then
-          SUGGESTIONS+=" ${SCRIPT}"
-        fi
-      done;
-    done;
-  # Autocomplete args.
+  if [ -f "${WEX_FILE_CACHE}" ];then
+    SUGGESTIONS=$(cat "${WEX_FILE_CACHE}")
   else
-    local WEX_CALLING_ARGUMENTS=()
+    . "${WEX_DIR_ROOT}includes/globals.sh"
 
-    _wexGetArguments "${WEX_SCRIPT_CALL_NAME}"
+    local LOCATIONS
 
-    for ((i=0; i < ${#WEX_CALLING_ARGUMENTS[@]}; i++));
-    do
-      eval "PARAMS=(${WEX_CALLING_ARGUMENTS[${i}]})"
-      local ARG_EXPECTED_LONG=${PARAMS[0]}
+    local ARGS_INDEX=2
 
-      SUGGESTIONS+=" --${ARG_EXPECTED_LONG}"
-    done;
+    LOCATIONS=$(_wexFindScriptsLocations)
 
+    # We are on the "group/name" section.
+    if (( COMP_CWORD < ARGS_INDEX ));then
+      # Search into extend directories.
+      for LOCATION in ${LOCATIONS[@]}
+      do
+        local SCRIPTS=$(wex scripts/list -d="${LOCATION}")
+
+        for SCRIPT in ${SCRIPTS[@]}
+        do
+          if [[ "${SCRIPT}" == ${CUR}* ]];then
+            SUGGESTIONS+=" ${SCRIPT}"
+          fi
+        done;
+      done;
+    # Autocomplete args.
+    else
+      local WEX_CALLING_ARGUMENTS=()
+
+      _wexGetArguments "${WEX_SCRIPT_CALL_NAME}"
+
+      for ((i=0; i < ${#WEX_CALLING_ARGUMENTS[@]}; i++));
+      do
+        eval "PARAMS=(${WEX_CALLING_ARGUMENTS[${i}]})"
+        local ARG_EXPECTED_LONG=${PARAMS[0]}
+
+        SUGGESTIONS+=" --${ARG_EXPECTED_LONG}"
+      done;
+    fi
+
+    mkdir -p "${WEX_DIR_TMP_AUTOCOMPLETE}"
+    echo "${SUGGESTIONS}" >> "${WEX_FILE_CACHE}"
   fi
 
   COMPREPLY=( $(compgen -W "${SUGGESTIONS}" -- ${CUR}) )
