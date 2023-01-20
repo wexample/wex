c25a6b732e6f99ebd27195cdb8fc3c1ebfc84956
weeger <romain.weeger@gmail.com>
2023-01-20 14:29:34 +0100

    Starting minor changes

--- project/includes/function/messages-progress.sh
+++ project/includes/function/messages-progress.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-_wexLog() {
-  wex prompt/progress -p="${WEX_PROGRESS_CURRENT_PERCENTAGE}" -s="${1}" -nl
-}
-
 _wexMessage() {
   wex prompt/progress -p="${WEX_PROGRESS_CURRENT_PERCENTAGE}" -s="${1}" -nl
 }
