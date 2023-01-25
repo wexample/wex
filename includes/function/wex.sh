6258adfca612e341702640aa70798231ae6c3774
weeger <romain.weeger@gmail.com>
2023-01-25 14:22:52 +0100

    Code format

---
+++ includes/function/wex.sh
@@ -0,0 +1,241 @@
+#!/usr/bin/env bash
+
+wex() {
+  # Using false as argument allow to load file and init wex
+  # without to execute any script.
+  if [ -z "${1+x}" ] || [ "${1}" = '' ] || [ ${1} = false ]; then
+    return
+  fi
+
+  # Running unit test.
+  if [ "${1}" = "test" ]; then
+    bash "${WEX_DIR_BASH}test.sh" "${2}"
+    return
+  fi
+
+  local WEX_SCRIPT_FILE
+  local WEX_SCRIPT_METHOD_NAME
+  local WEX_SCRIPT_FOUND=false
+
+  WEX_SCRIPT_CALL_NAME="${1}"
+  WEX_SCRIPT_FILE=$(_wexFindScriptFile "${WEX_SCRIPT_CALL_NAME}")
+
+  # File does not exists.
+  if [ ! -f "${WEX_SCRIPT_FILE}" ]; then
+    . "${WEX_DIR_ROOT}includes/colors.sh"
+
+    # Show hi
+    if [ "${WEX_SCRIPT_CALL_NAME}" = "hi" ]; then
+      printf "hi!\n"
+    # Not found.
+    elif [ ${WEX_SCRIPT_FOUND} = false ]; then
+      _wexError "Script file not found" "${WEX_SCRIPT_CALL_NAME}"
+    fi
+
+    return
+  fi
+
+  # Load init.sh file once, if exists.
+  local INIT_FILE
+  INIT_FILE="$(realpath "$(dirname "${WEX_SCRIPT_FILE}")/../")/init.sh"
+  if [ -f "${INIT_FILE}" ]; then
+    WEX_INIT_REGISTRY[${INIT_FILE}]=true
+    if [ "${WEX_INIT_REGISTRY[${INIT_FILE}]}" = "true" ]; then
+      . "${INIT_FILE}"
+    fi
+  fi
+
+  # Get parameters keeping quoted strings.
+  WEX_ARGUMENTS=''
+  whitespace="[[:space:]]"
+  for i in "${@:2}"; do
+    if [[ $i =~ $whitespace ]]; then
+      ARG_NAME=$(echo ${i} | cut -d"=" -f1)
+      ARG_VALUE=${i#*\=}
+      i="-"${ARG_NAME}"=\"${ARG_VALUE}\""
+    fi
+    WEX_ARGUMENTS+=' '${i}
+  done
+
+  WEX_SCRIPT_METHOD_NAME=$(_wexMethodName "${WEX_SCRIPT_CALL_NAME}")
+
+  # Include found script file
+  . "${WEX_SCRIPT_FILE}"
+  # Disable space as array item separator
+
+  local _AS_SUDO=true
+  local _AS_NON_SUDO=true
+  local WEX_CALLING_ARGUMENTS=()
+  _wexGetArguments "${WEX_SCRIPT_CALL_NAME}"
+
+  # Then start in negative value (length of previous table).
+  local _NEGATIVE_ARGS_LENGTH="${#WEX_ARGUMENT_DEFAULTS[@]}"
+  # We iterate first on system extra parameters
+  # Using negative values allow to use clean push method on array.
+  for ((i = -_NEGATIVE_ARGS_LENGTH; i < ${#WEX_CALLING_ARGUMENTS[@]} - _NEGATIVE_ARGS_LENGTH; i++)); do
+    local WEX_ARG_FOUND=false
+    eval "PARAMS=(${WEX_CALLING_ARGUMENTS[${i}]})"
+    local ARG_EXPECTED_LONG=${PARAMS[0]}
+    local ARG_EXPECTED_SHORT=${PARAMS[1]}
+    # Mark variable as empty
+    local ${ARG_EXPECTED_LONG^^}=
+    # Set it as null.
+    eval 'unset '${ARG_EXPECTED_LONG^^}
+
+    # Get args given,
+    # ignore first one which is always method name.
+    local ARG_SEARCH=0
+    local ORIGINAL_ARGS=("${@:2}")
+
+    for ARG_GIVEN in "${ORIGINAL_ARGS[@]}"; do
+      ARG_GIVEN_NAME=$(sed -e 's/-\{1,2\}\([^\=]\{0,\}\)\=.\{0,\}/\1/' <<<${ARG_GIVEN})
+      ARG_GIVEN_VALUE=${ARG_GIVEN#*\=}
+
+      if [ "${ARG_GIVEN_NAME}" = "${ARG_EXPECTED_LONG}" ] || [ "${ARG_GIVEN_NAME}" = "${ARG_EXPECTED_SHORT}" ]; then
+        WEX_ARG_FOUND=true
+        local ${ARG_EXPECTED_LONG^^}="${ARG_GIVEN_VALUE}"
+      # Support --noEqualSign -nes
+      # Support also space separator.
+      elif [ "--${ARG_EXPECTED_LONG}" = "${ARG_GIVEN}" ] || [ "-${ARG_EXPECTED_SHORT}" = "${ARG_GIVEN}" ]; then
+        WEX_ARG_FOUND=true
+        ARG_SEARCH_NEXT=$((${ARG_SEARCH} + 1))
+        ARG_NEXT_VALUE=${ORIGINAL_ARGS[${ARG_SEARCH_NEXT}]}
+
+        # Use next arguments as value if exits
+        if [ "${ARG_NEXT_VALUE}" != "" ] && [ "$(echo "${ARG_NEXT_VALUE}" | head -c 1)" != "-" ]; then
+          local ${ARG_EXPECTED_LONG^^}="${ARG_NEXT_VALUE}"
+          # Ignore next parsing.
+          ARG_SEARCH=ARG_SEARCH_NEXT
+        else
+          local ${ARG_EXPECTED_LONG^^}=true
+        fi
+      fi
+
+      ((ARG_SEARCH++))
+    done
+
+    if [ ! -z ${QUIET+x} ]; then
+      # Override all messages and errors functions
+      WEX_FILE_MESSAGE_FUNCTION="${WEX_DIR_ROOT}includes/function/messages-quiet.sh"
+      . "${WEX_FILE_MESSAGE_FUNCTION}"
+    fi
+
+    # If an argument is not found
+    # And we are not on help page.
+    if [ "${WEX_ARG_FOUND}" = "false" ] && [ -z ${HELP+x} ] && [ -z ${SOURCE+x} ]; then
+      # A default value is set in arguments list.
+      if [ "${PARAMS[4]}" != "" ]; then
+        local ${ARG_EXPECTED_LONG^^}="${PARAMS[4]}"
+      # Expected
+      elif [ "${PARAMS[3]}" = "true" ]; then
+        # Interactive mode allowed.
+        if [ -z ${NON_INTERACTIVE+x} ] || [ "${NON_INTERACTIVE}" = "false" ]; then
+          printf "${WEX_COLOR_CYAN}${PARAMS[2]}:${WEX_COLOR_RESET} "
+          read ${ARG_EXPECTED_LONG^^}
+        else
+          _wexError "Expected argument not found" "${WEX_SCRIPT_METHOD_NAME} : "${ARG_EXPECTED_LONG}
+          # Raise an error.
+          # Unable to fetch expected variable
+          exit 0
+        fi
+      fi
+    fi
+  done
+
+  # Show help manual
+  if [ ! -z ${HELP+x} ]; then
+    . "${WEX_DIR_ROOT}includes/colors.sh"
+
+    echo ""
+    printf '.%.0s' {1..60}
+    printf "\n"
+
+    printf "${WEX_COLOR_CYAN}Name${WEX_COLOR_RESET}\t\t${WEX_SCRIPT_CALL_NAME}\n"
+    printf "${WEX_COLOR_CYAN}Function${WEX_COLOR_RESET}\t${WEX_SCRIPT_METHOD_NAME}\n"
+    printf "${WEX_COLOR_CYAN}File${WEX_COLOR_RESET}\t\t${WEX_SCRIPT_FILE}\n"
+    printf "${WEX_COLOR_CYAN}Requirements${WEX_COLOR_RESET}\t${_REQUIREMENTS[*]}\n"
+
+    if [ "${_DESCRIPTION}" != "false" ]; then
+      echo ""
+      printf "${WEX_COLOR_CYAN}Description${WEX_COLOR_RESET}\n"
+      echo "${_DESCRIPTION}" | fold -w 60
+    fi
+
+    echo ""
+    printf "${WEX_COLOR_CYAN}Arguments${WEX_COLOR_RESET}\n"
+
+    for ((i = -${_NEGATIVE_ARGS_LENGTH}; i < ${#WEX_CALLING_ARGUMENTS[@]} - ${_NEGATIVE_ARGS_LENGTH}; i++)); do
+      eval "PARAMS=(${WEX_CALLING_ARGUMENTS[${i}]})"
+      ARG_EXPECTED_LONG=${PARAMS[0]}
+
+      TEXT=$(wex text/color -c=lightblue -t="--${PARAMS[0]} -${PARAMS[1]}")
+
+      local PROPS=''
+
+      if [ "${PARAMS[3]}" = "true" ]; then
+        PROPS+=[required]
+      fi
+
+      if [ "${PARAMS[4]}" != "" ]; then
+        PROPS+=[default=\"${PARAMS[4]}\"]
+      fi
+
+      if [ "${PROPS}" != "" ]; then
+        TEXT+="\n\t$(wex text/color -c=brown -t=${PROPS})"
+      fi
+
+      TEXT+="\n\t\t"${PARAMS[2]}
+      TEXT+="\n"
+
+      echo -e ${TEXT}
+    done
+
+    printf '.%.0s' {1..60}
+    echo ""
+    echo ""
+
+    return
+  # Display script file source.
+  elif [ ! -z ${SOURCE+x} ]; then
+    . "${WEX_DIR_ROOT}includes/colors.sh"
+    printf ${WEX_COLOR_LIGHT_CYAN}
+    cat ${WEX_SCRIPT_FILE}
+    printf ${WEX_COLOR_RESET}"\n"
+
+    return
+  fi
+
+  # User is not sudo.
+  if [ "${EUID}" -ne 0 ]; then
+    if [ "${_AS_NON_SUDO}" = "false" ]; then
+      # User can switch to sudo without password typing
+      # Or interactive mode is allowed.
+      if [ "$(sudo -n true 2>/dev/null)" = "" ] && [ "${NON_INTERACTIVE}" != "true" ]; then
+        # Enforce using sudo
+        ${WEX_SWITCH_SUDO_COMMAND} bash -c "wex ${@}"
+      else
+        _wexError "${WEX_SCRIPT_CALL_NAME} should be executed as sudo" "You are \"$(whoami)\", retry with : " "sudo wex ${WEX_SCRIPT_CALL_NAME} ... "
+      fi
+
+      return
+    fi
+  # User is sudo.
+  else
+    if [ "${_AS_SUDO}" = "false" ]; then
+      _wexError "${WEX_SCRIPT_CALL_NAME} should not be executed as sudo" "You are \"$(whoami)\", try to \"exit\" sudo mode, then retry this command"
+      return
+    fi
+  fi
+
+  # Some scripts can't be executed as sudo (using sudo prefix),
+  # as we need a username for execution, but sudo is still required
+  # for script body execution.
+  if [ "${_AS_SUDO_RUN}" = "true" ] && [ -z "${WEX_RUNNER_USERNAME}" ]; then
+    # Enforce using sudo
+    ${WEX_SWITCH_SUDO_COMMAND} bash -c "wex ${@}"
+    return
+  fi
+
+  # Execute script with all parameters.
+  ${WEX_SCRIPT_METHOD_NAME} "${@:2}"
+}
