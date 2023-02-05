732ad2de05ed96f048087886ccd059b8a73c1c76
weeger <romain.weeger@gmail.com>
2023-02-05 16:47:07 +0100

    Description

--- bash/core/build.sh
+++ bash/core/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+coreBuildArgs() {
+  _DESCRIPTION="Update core data. Should be executed before deploying."
+}
+
+
 coreBuild() {
   _wexLog "Updating variables in globals.sh"
   wex default::config/setValue \
