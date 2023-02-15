596e4a91a7ec10a1eead9c2c07ee0f97e49c37bf
weeger <romain.weeger@gmail.com>
2023-02-15 10:45:17 +0100

    addons/list

---
+++ tests/bash/addons/list.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+addonsListTest() {
+  local ADDONS=($(wex addons/list))
+  local IS_ARRAY=false
+  if [[ "$(declare -p ADDONS)" =~ "declare -a" && -n "${ADDONS[0]}" ]]; then
+    IS_ARRAY=true
+  fi
+
+  _wexTestAssertEqual "${IS_ARRAY}" "true"
+}
+
