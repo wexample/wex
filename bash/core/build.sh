b1cc18d6bb40a55e71f154ed03f54abf7af92074
weeger <romain.weeger@gmail.com>
2023-02-20 17:44:32 +0100

    Increment wex core version

--- bash/core/build.sh
+++ bash/core/build.sh
@@ -7,10 +7,31 @@ coreBuildArgs() {
 coreBuild() {
   _wexLog "Updating variables in globals.sh"
 
-  wex default::config/setValue \
+  wex-exec default::config/setValue \
     -f="${WEX_DIR_ROOT}includes/globals.sh" \
     -s="=" \
     -k="WEX_ADDONS" \
-    -v="($(wex addons/list))" \
+    -v="($(wex-exec addons/list))" \
+    -vv
+
+  # Increment core version
+
+  local VERSION_ACTUAL
+  local VERSION
+  local BUILD
+  local NEW_BUILD
+
+  VERSION_ACTUAL=$(wex-exec core/version)
+  VERSION=$(echo "${VERSION_ACTUAL}" | cut -d '.' -f 1-2)
+  BUILD=$(echo "${VERSION_ACTUAL}" | cut -d '.' -f 3)
+
+  # Increment.
+  NEW_BUILD=$((BUILD+1))
+
+  wex-exec default::config/setValue \
+    -f="${WEX_DIR_ROOT}includes/globals.sh" \
+    -s="=" \
+    -k="export WEX_CORE_VERSION" \
+    -v="${VERSION}.${NEW_BUILD}" \
     -vv
 }
