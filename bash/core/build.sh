dad857996a5715a0564dc42fcc69fb89ccb18684
weeger <romain.weeger@gmail.com>
2023-03-03 17:04:33 +0100

    core/build

--- bash/core/build.sh
+++ bash/core/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 coreBuildArgs() {
-  _DESCRIPTION="Prepare core for new version"
   # shellcheck disable=SC2034
   _DESCRIPTION="Update core data. Should be executed before deploying."
 }
@@ -18,18 +17,8 @@ coreBuild() {
 
   # Increment core version
 
-  local VERSION_ACTUAL
-  local VERSION
-  local BUILD
-  local NEW_BUILD
-
-  VERSION_ACTUAL=$(wex-exec core/version)
-  VERSION=$(echo "${VERSION_ACTUAL}" | cut -d '.' -f 1-2)
-  BUILD=$(echo "${VERSION_ACTUAL}" | cut -d '.' -f 3)
-
-  # Increment.
-  NEW_BUILD=$((BUILD + 1))
-  NEW_VERSION="${VERSION}.${NEW_BUILD}"
+  local NEW_VERSION
+  NEW_VERSION=$(wex default::version/increment -t=minor -v="${WEX_CORE_VERSION}")
 
   wex-exec default::config/setValue \
     -f="${WEX_DIR_ROOT}includes/globals.sh" \
