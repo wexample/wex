2946697a985d09b3e5627f3511e144b9bc9cf962
weeger <romain.weeger@gmail.com>
2023-03-10 17:53:12 +0100

    External group / arg

--- index.py
+++ index.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
-import sys
+import argparse
 
-if len(sys.argv) > 1:
-    print("Hi, " + sys.argv[1] + " ! Welcome in wex world.")
+parser = argparse.ArgumentParser(description='Description de votre programme.')
+parser.add_argument('addon_group_name', metavar='addon_group_name', type=str,
+                    help='Nom de l\'addon, groupe et nom du service.')
+parser.add_argument('-a', '--first-arg', dest='first_arg', type=str,
+                    help='Premier argument avec le nom court.')
+
+args = parser.parse_args()
+
+print('Hi! Welcome in wex world.')
+print('Addon/group/name: ' + args.addon_group_name)
+print('First arg: ' + str(args.first_arg))
