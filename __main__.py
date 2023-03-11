cab306e5cad17981b0d6e5b12c9111c726f8c449
weeger <romain.weeger@gmail.com>
2023-03-11 15:14:02 +0100

    Variable args

--- __main__.py
+++ __main__.py
@@ -2,16 +2,26 @@
 
 import click
 
-@click.command()
+
+@click.group()
 @click.argument('command_name')
-@click.option('-a', '--first-arg', type=str, help='Your first arg')
-def main(command_name, first_arg):
+def main(command_name):
     """
     A bash scripts manager.
     """
     click.echo('Addon/group/name: ' + command_name)
-    if first_arg:
-        click.echo('First arg: ' + first_arg)
+
+
+options = [
+    {'name': '--first-arg', 'short': '-f', 'type': str, 'help': 'Your first arg'},
+    {'name': '--second-arg', 'short': '-s', 'type': str, 'help': 'Your second arg'},
+]
+
+for option in options:
+    name = option['name']
+    type_ = option['type']
+    help_ = option['help']
+    main = click.option(name, type=type_, help=help_)(main)
 
 if __name__ == '__main__':
     main()
