92e8260112fb01f928046f641223194c35a46d9a
weeger <romain.weeger@gmail.com>
2023-03-11 07:51:26 +0100

    Reformat

--- index.py
+++ index.py
@@ -2,14 +2,26 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Description de votre programme.')
-parser.add_argument('addon_group_name', metavar='addon_group_name', type=str,
-                    help='Nom de l\'addon, groupe et nom du service.')
-parser.add_argument('-a', '--first-arg', dest='first_arg', type=str,
-                    help='Premier argument avec le nom court.')
+parser = argparse.ArgumentParser(
+    description='A bash scripts manager.'
+)
+
+parser.add_argument(
+    'command_name',
+    metavar='command_name',
+    type=str,
+    help='Command name'
+)
+
+parser.add_argument(
+    '-a',
+    '--first-arg',
+    dest='first_arg',
+    type=str,
+    help='Your first arg'
+)
 
 args = parser.parse_args()
 
-print('Hi! Welcome in wex world.')
 print('Addon/group/name: ' + args.addon_group_name)
 print('First arg: ' + str(args.first_arg))
