20f1f2195e4a433d4d6e132f6fab3234f64eca1f
weeger <romain.weeger@gmail.com>
2023-03-11 09:50:55 +0100

    Autocode

---
+++ .wex/script/autoCode.sh
@@ -0,0 +1,14 @@
+
+_wexMessage "No auto code here. ${BASH_SOURCE[0]}" "Use this file to paste custom code, then rollback it before commit."
+
+# Code is executed from project's root directory.
+
+#_wexLog "Updating addons..."
+#for ADDON in "${WEX_ADDONS[@]}"; do
+#  if [ -d "${WEX_DIR_ADDONS}${ADDON}" ]; then
+#    _wexLog "Updating ${ADDON}..."
+#
+#    cd ${WEX_DIR_ADDONS}${ADDON}
+#
+#  fi
+#done
