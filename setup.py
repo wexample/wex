9642aa4e2b27201852e27d92812553b6afefcd54
weeger <romain.weeger@gmail.com>
2023-03-11 10:11:17 +0100

    Setup

--- setup.py
+++ setup.py
@@ -7,22 +7,9 @@ setup(
     author='Wexample',
     author_email='contact@wexample.com',
     packages=find_packages(),
-    install_requires=[
-
-    ],
     entry_points={
-                         'console_scripts': [
-                             'wex = myapp.index:main'
-                         ]
-                     },,
-    classifiers=[
-        'Development Status :: 5 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
+        'console_scripts': [
+            'wex = myapp.index:main'
+        ]
+    },
 )
