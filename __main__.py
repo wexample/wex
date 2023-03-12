93af1635f727958dd207deab4f22620e733b1da6
weeger <romain.weeger@gmail.com>
2023-03-12 12:05:10 +0100

    Code reformat

--- __main__.py
+++ __main__.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 
+import click
 from src.core.Kernel import Kernel
+
+kernel = Kernel()
+
+
+@click.command()
+@click.argument('command', type=str, callback=kernel.command_validate)
+def main(command):
+    # options = [
+    #     {'name': '--first-arg', 'short': '-f', 'type': str, 'help': 'Your first arg'},
+    #     {'name': '--second-arg', 'short': '-s', 'type': str, 'help': 'Your second arg'},
+    # ]
+    #
+    # for option in options:
+    #     name = option['name']
+    #     type_ = option['type']
+    #     help_ = option['help']
+    #     short = option['short']
+    #     click.option(
+    #         name,
+    #         short,
+    #         type=type_,
+    #         help=help_,
+    #     )(self.command)
+
+    click.echo(click.style('command : ' + command, fg='cyan', bold=True))
 
 
 if __name__ == '__main__':
-    Kernel().setup()
+    main()
--- src/core/Kernel.py
+++ src/core/Kernel.py
@@ -1,8 +1,8 @@
 import click
-import re
 import json
-from ..const.error import ERR_ARGUMENT_COMMAND_MALFORMED
 import os
+import re
+from ..const.error import ERR_ARGUMENT_COMMAND_MALFORMED
 
 
 class Kernel:
@@ -23,34 +23,3 @@ class Kernel:
         if not re.match(r"^(?:\w+::)?[\w-]+/[\w-]+$", value):
             raise self.error(ERR_ARGUMENT_COMMAND_MALFORMED)
         return value
-
-    @click.command()
-    def command(self, command):
-        click.echo(click.style('command : ' + command, fg='cyan', bold=True))
-
-    def setup(self):
-        options = [
-            {'name': '--first-arg', 'short': '-f', 'type': str, 'help': 'Your first arg'},
-            {'name': '--second-arg', 'short': '-s', 'type': str, 'help': 'Your second arg'},
-        ]
-
-        for option in options:
-            name = option['name']
-            type_ = option['type']
-            help_ = option['help']
-            short = option['short']
-            click.option(
-                name,
-                short,
-                type=type_,
-                help=help_,
-            )(self.command)
-
-        # Binding validation in non-static context (keeping reference to self).
-        self.command = click.argument(
-            'command',
-            type=str,
-            callback=self.command_validate
-        )(self.command)
-
-        self.command()
