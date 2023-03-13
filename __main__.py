5013902d315ea2c859d20d86d42dc634cf1bb99b
weeger <romain.weeger@gmail.com>
2023-03-13 13:52:59 +0100

    Fix kernel

--- __main__.py
+++ __main__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import click
-import sys
 from src.core.Kernel import Kernel
-import importlib
 
 
 @click.group()
@@ -10,26 +8,7 @@ import importlib
 def cli(ctx):
     ctx.obj = {'kernel': kernel}
 
-    pass
-
 
 if __name__ == '__main__':
     kernel = Kernel()
-
-    if kernel.validate_argv(sys.argv):
-        command = sys.argv[1]
-        kernel.validate_command(command)
-
-        module_name = 'addons.core.registry.build'
-        command_name = 'core::registry/build'
-        function_name = 'core_registry_build'
-
-        module = importlib.import_module(module_name)
-        function = getattr(module, function_name)
-
-        cli.add_command(
-            function,
-            name=command_name
-        )
-
-        cli()
+    kernel.command_prepare(cli)
--- src/core/Kernel.py
+++ src/core/Kernel.py
@@ -2,8 +2,10 @@ import click
 import json
 import os
 import re
+import sys
 from ..const.globals import WEX_VERSION
 from ..const.error import ERR_ARGUMENT_COMMAND_MALFORMED
+import importlib
 
 
 class Kernel:
@@ -31,3 +33,23 @@ class Kernel:
         if not re.match(r"^(?:\w+::)?[\w-]+/[\w-]+$", value):
             self.error(ERR_ARGUMENT_COMMAND_MALFORMED)
         return value
+
+    def command_prepare(self, cli):
+        if not self.validate_argv(sys.argv):
+            return
+
+        command = sys.argv[1]
+        self.validate_command(command)
+
+        module_name = 'addons.core.registry.build'
+        command_name = 'core::registry/build'
+        function_name = 'core_registry_build'
+
+        module = importlib.import_module(module_name)
+        function = getattr(module, function_name)
+
+        cli.add_command(
+            function,
+            name=command_name
+        )
+        cli()
