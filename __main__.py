ff279b2f43bb369a53d664c2aed379dd542bcfa1
weeger <romain.weeger@gmail.com>
2023-03-13 10:30:32 +0100

    Rewrite

--- __main__.py
+++ __main__.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python3
-import sys
-
 import click
+import sys
 from src.core.Kernel import Kernel
 
 kernel = Kernel()
 
 
 @click.command()
-@click.argument('command', type=str)
-@click.argument('--first-arg', type=str)
-def exec(command, __first_arg):
-    print('Command : ' + command)
-    print('First : ' + __first_arg)
+@click.option('--myarg', type=str)
+@click.option('-d', is_flag=True, type=bool)
+@click.option('-f', is_flag=True, type=bool)
+def first_command(myarg, d, f):
+    print('EXEC')
+    print(myarg)
+    print(d)
+    print(f)
 
 
-def main():
-    if not kernel.validate_argv(sys.argv):
-        return
-
-    command = sys.argv[1]
-    kernel.validate_command(command)
-
-    # TODO Remove command name
-    # TODO convert command to commandName
-    # TODO load file and execute
-
-    exec()
+@click.group()
+def cli():
+    pass
 
 
 if __name__ == '__main__':
-    main()
+    if kernel.validate_argv(sys.argv):
+        command = sys.argv[1]
+        kernel.validate_command(command)
+
+        # TODO Remove command name
+        # TODO convert command to commandName
+        # TODO load file and execute
+
+        cli.add_command(
+            first_command,
+            name='toto::tata/truc'
+        )
+
+        cli()
