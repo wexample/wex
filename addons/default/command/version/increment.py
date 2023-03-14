260cbac5f2d9e09b22c0728f373d27b0c459dc7b
weeger <romain.weeger@gmail.com>
2023-03-14 11:51:33 +0100

    Support pre builds and timestamps

--- addons/default/command/version/increment.py
+++ addons/default/command/version/increment.py
@@ -1,12 +1,36 @@
 import click
+import datetime
 
 
 @click.command()
 @click.option('--version', '-v', type=str, required=True)
 @click.option('--type', '-t', type=str, default='minor', help="Upgrade type (major, intermediate or minor)")
 @click.option('--increment', '-i', type=int, default=1, help="Amount of version to increment")
-def default_version_increment(version, type, increment) -> str:
-    major, intermediate, minor = version.split('.')
+def default_version_increment(version: str, type: str, increment: str) -> str:
+    pre_build_number = 1
+
+    # Handle 1.0.0-beta.1+build.1234
+    if '-' in version:
+        base_version, pre_build = version.split('-')
+
+        if '.' in pre_build:
+            pre_build_parts = pre_build.split('.')
+            if len(pre_build_parts) == 2:
+                pre_build, pre_build_number = pre_build_parts
+            else:
+                pre_build, pre_build_number, _ = pre_build_parts
+
+            # pre_build_number can be : 1+build.1234
+            if '+' in pre_build_number:
+                # Ignore last part which is a timestamp.
+                pre_build_number: int = int(pre_build_number.split('+')[0])
+            else:
+                pre_build_number: int = int(pre_build_number)
+    else:
+        base_version, pre_build = version, ''
+
+    major, intermediate, minor = base_version.split('.')
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
     # Increment according type.
     if type == 'major':
@@ -15,6 +39,10 @@ def default_version_increment(version, type, increment) -> str:
     elif type == 'intermediate':
         intermediate = str(int(intermediate) + increment)
         minor = '0'
+    # Any of pre-build version.
+    elif type == 'alpha' or type == 'beta' or type == 'dev' or type == 'rc' or type == 'nightly' or type == 'snapshot':
+        pre_build = type
+        pre_build_number += increment
     else:  # type == 'minor'
         minor = str(int(minor) + increment)
 
@@ -26,4 +54,8 @@ def default_version_increment(version, type, increment) -> str:
     elif int(minor) < 0:
         minor = '0'
 
-    return f"{major}.{intermediate}.{minor}"
+    build_info = ''
+    if pre_build:
+        build_info = f'-{pre_build}.{pre_build_number}'
+
+    print(f"{major}.{intermediate}.{minor}{build_info}+build.{timestamp}")
