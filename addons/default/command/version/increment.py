1c16725050d5bc32ae5334d0bed2670cafa994b6
weeger <romain.weeger@gmail.com>
2023-03-14 10:55:26 +0100

    Refactor

--- addons/default/command/version/increment.py
+++ addons/default/command/version/increment.py
@@ -6,24 +6,24 @@ import click
 @click.option('--type', '-t', type=str, default='minor', help="Upgrade type (major, intermediate or minor)")
 @click.option('--increment', '-i', type=int, default=1, help="Amount of version to increment")
 def default_version_increment(version, type, increment) -> str:
-    major, minor, build = version.split('.')
+    major, intermediate, minor = version.split('.')
 
     # Increment according type.
     if type == 'major':
         major = str(int(major) + increment)
-        minor, build = '0', '0'
+        intermediate, minor = '0', '0'
     elif type == 'intermediate':
-        minor = str(int(minor) + increment)
-        build = '0'
+        intermediate = str(int(intermediate) + increment)
+        minor = '0'
     else:  # type == 'minor'
-        build = str(int(build) + increment)
+        minor = str(int(minor) + increment)
 
     # Set to zero in result is negative.
     if int(major) < 0:
-        major, minor, build = '0', '0', '0'
+        major, intermediate, minor = '0', '0', '0'
+    elif int(intermediate) < 0:
+        intermediate, minor = '0', '0'
     elif int(minor) < 0:
-        minor, build = '0', '0'
-    elif int(build) < 0:
-        build = '0'
+        minor = '0'
 
-    return f"{major}.{minor}.{build}"
+    return f"{major}.{intermediate}.{minor}"
