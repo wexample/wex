a3b3939b2e403f63417248322b07f61c16935819
weeger <romain.weeger@gmail.com>
2023-03-14 09:33:40 +0100

    Execute changelog

--- addons/core/command/version/build.py
+++ addons/core/command/version/build.py
@@ -1,4 +1,8 @@
+import os
+
 import click
+from src.const.globals import GITHUB_GROUP, GITHUB_PROJECT
+from src.const.error import ERR_ENV_VAR_MISSING
 
 
 @click.command()
@@ -6,3 +10,17 @@ import click
 def core_version_build(kernel) -> None:
     print('Building new version...')
     print(kernel.version)
+
+    print(kernel.exec('default::version/increment', ['--version', '5.0.0']))
+
+    env_core_github_token: str = os.getenv('CORE_GITHUB_TOKEN')
+
+    if env_core_github_token:
+        print('Building CHANGELOG.md...')
+
+        kernel.shell_exec(
+            f'github_changelog_generator -u {GITHUB_GROUP} -p {GITHUB_PROJECT} -t {env_core_github_token}'
+        )
+    else:
+        kernel.error(ERR_ENV_VAR_MISSING)
+
