c633673854f5db6a881857a5370153a26fe9b5b2
weeger <romain.weeger@gmail.com>
2023-03-14 15:34:23 +0100

    Check git changes before build

--- addons/core/command/version/build.py
+++ addons/core/command/version/build.py
@@ -1,11 +1,18 @@
 import os
-
+import git
 import click
 from src.const.globals import GITHUB_GROUP, GITHUB_PROJECT
-from src.const.error import ERR_ENV_VAR_MISSING
-
+from src.const.error import ERR_CORE_REPO_DIRTY, ERR_ENV_VAR_MISSING
+import subprocess
 from addons.default.command.version.increment import default_version_increment
+
 from addons.default.command.config.change import default_config_change
+
+
+def has_uncommitted_changes(directory):
+    command = 'git status --porcelain'
+    output = subprocess.check_output(command.split(), cwd=directory).decode()
+    return bool(output.strip())
 
 
 @click.command()
@@ -13,10 +20,17 @@ from addons.default.command.config.change import default_config_change
 def core_version_build(kernel) -> None:
     print(f'Building new version from {kernel.version}...')
 
-    # Check requirements : GitHub token for changelog build
+    # Check requirements
+    # GitHub token for changelog build
     env_core_github_token: str = os.getenv('CORE_GITHUB_TOKEN')
     if not env_core_github_token:
         kernel.error(ERR_ENV_VAR_MISSING)
+
+    # There is no uncommitted change
+    repo = git.Repo(kernel.path['root'])
+    if repo.is_dirty(untracked_files=True):
+        kernel.shell_exec('git diff')
+        kernel.error(ERR_CORE_REPO_DIRTY)
 
     new_version = default_version_increment.callback(
         kernel.version,
@@ -38,4 +52,3 @@ def core_version_build(kernel) -> None:
     kernel.shell_exec(
         f'github_changelog_generator -u {GITHUB_GROUP} -p {GITHUB_PROJECT} -t {env_core_github_token}'
     )
-
