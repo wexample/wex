7c7402e6d44f4b6b464c953857fd59f072604871
weeger <romain.weeger@gmail.com>
2023-03-15 12:01:06 +0100

    Improve autocode

--- .wex/script/autoCode.sh
+++ .wex/script/autoCode.sh
@@ -1,14 +1,18 @@
 
-_wexMessage "No auto code here. ${BASH_SOURCE[0]}" "Use this file to paste custom code, then rollback it before commit."
+_wexMessage "Use this file to paste custom code, then rollback it before commit."
+
+CURRENT_WEX_ROOT="$(realpath "$(dirname "$(realpath ${BASH_SOURCE[0]})")/../../")/"
 
 # Code is executed from project's root directory.
 
-#_wexLog "Updating addons..."
-#for ADDON in "${WEX_ADDONS[@]}"; do
-#  if [ -d "${WEX_DIR_ADDONS}${ADDON}" ]; then
-#    _wexLog "Updating ${ADDON}..."
-#
-#    cd ${WEX_DIR_ADDONS}${ADDON}
-#
-#  fi
-#done
+_wexLog "Updating addons..."
+for ADDON in "${WEX_ADDONS[@]}"; do
+  ADDON_PATH=$(echo ${CURRENT_WEX_ROOT}addons/${ADDON})
+  if [ -d "${ADDON_PATH}" ]; then
+    _wexLog "Updating ${ADDON}..."
+
+    cd ${CURRENT_WEX_ROOT}addons/${ADDON}
+
+    # git commit ...
+  fi
+done
