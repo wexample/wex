5749c4cb367312e3627660bbb38b725b312edd48
weeger <romain.weeger@gmail.com>
2023-03-17 14:55:22 +0100

    Relative parsing in suggestion

--- addons/core/command/autocomplete/suggest.py
+++ addons/core/command/autocomplete/suggest.py
@@ -7,9 +7,10 @@ from src.const.globals import COMMAND_SEPARATOR_ADDON, COMMAND_SEPARATOR_GROUP
 
 @click.command
 @click.pass_obj
-@click.option('--cursor', '-c', type=int, required=True)
-@click.option('--search', '-s', type=str, required=True, help="Separated arguments, i.e. : wex app :: config/write")
-def core_autocomplete_suggest(kernel, cursor, search: str) -> str:
+@click.option('--cursor', '-c', type=int, required=True, help="Indicates which part of search string is focused")
+@click.option('--search', '-s', type=str, required=True, help="Separated arguments, without first command, i.e. : "
+                                                              "app :: config/write")
+def core_autocomplete_suggest(kernel, cursor: int, search: str) -> str:
     """
     Works and interact with cli/autocomplete bash command to provide a smart autocomplete intergration.
     Returns a string with suggestions to transmit to bash compgen.
@@ -24,18 +25,18 @@ def core_autocomplete_suggest(kernel, cursor, search: str) -> str:
 
     search_part = search_split[cursor]
 
-    if cursor == 1:
+    if cursor == 0:
         # User typed ony "wex ", we suggest all addons names.
         suggestion = ' '.join(kernel.registry['addons'].keys())
-    elif cursor == 2:
+    elif cursor == 1:
         # User typed "app::", we suggest all addon groups.
-        if search_split[2] == COMMAND_SEPARATOR_ADDON:
-            suggestion = ' '.join(kernel.get_group_names(search_split[1]))
+        if search_split[1] == COMMAND_SEPARATOR_ADDON:
+            suggestion = ' '.join(kernel.get_group_names(search_split[0]))
         else:
             # User types "app:", we add a second ":"
             suggestion = ':'
-    elif cursor == 3:
-        addon = search_split[1]
+    elif cursor == 2:
+        addon = search_split[0]
 
         if addon in kernel.registry['addons']:
             # User typed "wex app::conf", we suggest full addon commands list.
@@ -44,7 +45,7 @@ def core_autocomplete_suggest(kernel, cursor, search: str) -> str:
                 command_split = command_parts[1].split(COMMAND_SEPARATOR_GROUP)
 
                 if COMMAND_SEPARATOR_GROUP in search_part:
-                    if command_split[0] == search_split[3].split(COMMAND_SEPARATOR_GROUP)[0]:
+                    if command_split[0] == search_split[2].split(COMMAND_SEPARATOR_GROUP)[0]:
                         suggestion += f' {" ".join(command_parts)}'
                 else:
                     suggestion += f' {command_split[0]}'
@@ -52,9 +53,9 @@ def core_autocomplete_suggest(kernel, cursor, search: str) -> str:
             # Reduce unique values
             suggestion = " ".join(set(suggestion.split()))
     # Complete arguments.
-    elif cursor >= 4:
-        command = search_split[1:4]
-        params_current = [val for val in search_split[4:] if val.startswith("-")]
+    elif cursor >= 3:
+        command = search_split[0:3]
+        params_current = [val for val in search_split[3:] if val.startswith("-")]
 
         match = re.match(
             COMMAND_PATTERN,
