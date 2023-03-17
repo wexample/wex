e21730334ec2005e3db409cdba98b12ed13ea562
weeger <romain.weeger@gmail.com>
2023-03-17 08:12:37 +0100

    Fix registry

--- addons/core/command/registry/build.py
+++ addons/core/command/registry/build.py
@@ -14,15 +14,13 @@ def core_registry_build(kernel) -> None:
                 'name': addon,
                 'commands': {
                     f"{addon}::{group}/{os.path.splitext(command_name)[0]}": {
-                        'file': os.path.join(addon_dir, group, command),
+                        'file': os.path.join(addon_command_dir, group, command),
                         'test': test_file if os.path.exists(test_file) else None
                     }
                     for group in kernel.list_subdirectories(addon_command_dir)
                     for command in os.listdir(os.path.join(addon_command_dir, group))
                     if command.endswith('.py')
-                    for command_name, ext in [
-                        os.path.splitext(command)
-                    ]
+                    for command_name, ext in [os.path.splitext(command)]
                     for test_file in [os.path.join(addon_dir, 'tests', 'command', group, command)]
                 }
             }
