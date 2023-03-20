8a876c759d081624cd975732672cd8dae0fba469
weeger <romain.weeger@gmail.com>
2023-03-20 12:10:10 +0100

    git management

--- .wex/script/appStarted.sh
+++ .wex/script/appStarted.sh
@@ -2,5 +2,15 @@
 
 _wexLog "Running custom app scripts"
 
-wex app::app/exec -c=". /var/www/html/cli/install"
+# Share current user identity
+GIT_USER_NAME=$(git config --global user.name)
+GIT_USER_EMAIL=$(git config --global user.email)
+wex app::app/exec -vv -c="git config --global user.name ${GIT_USER_NAME}"
+wex app::app/exec -vv -c="git config --global user.email ${GIT_USER_EMAIL}"
+
+_wexLog "Copying .git inside container"
+docker cp .git $(wex app/container -c=dev):/var/www/html
+
+# Install wex.
+wex app::app/exec -vv -c=". /var/www/html/cli/install"
 
