039dc65b0cbe19073ea9dd9b9702663af8e7d503
owner <owner@c2789dffeefc>
2023-03-20 14:19:20 +0000

    New version v5.0.0-dev.3+build.20230320141652

--- src/const/globals.py
+++ src/const/globals.py
@@ -11,4 +11,4 @@ GITHUB_PROJECT = 'wex'
 LOG_FILENAME = 'app.log'
 LOG_MAX_DAYS = 90
 PATH_GLOBALS = 'src/const/globals.py'
-WEX_VERSION = '5.0.0-dev.2+build.20230314180944'
+WEX_VERSION = '5.0.0-dev.3+build.20230320141652'
