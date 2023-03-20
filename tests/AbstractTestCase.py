ef1d5b417a0cd6299580d09cf7c98c0df2082f61
weeger <romain.weeger@gmail.com>
2023-03-20 01:08:39 +0100

    Build

--- tests/AbstractTestCase.py
+++ tests/AbstractTestCase.py
@@ -11,3 +11,13 @@ class AbstractTestCase(unittest.TestCase):
         cls.kernel.setup_test_manager(
             TestCoreAction(cls.kernel)
         )
+
+    def assertFileExists(self, file_path, exists=True):
+        """
+        Assert that the specified file exists.
+        """
+        self.assertEqual(
+            os.path.exists(file_path),
+            exists,
+            f'The file does not exists : {file_path}'
+        )
