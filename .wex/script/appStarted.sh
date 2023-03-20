e57db02e19f5b29f3effe527ffad23dc91f17cf7
weeger <romain.weeger@gmail.com>
2023-03-20 14:32:33 +0100

    Install

--- .wex/script/appStarted.sh
+++ .wex/script/appStarted.sh
@@ -7,18 +7,12 @@ CONTAINER_USER=owner
 # Share current user identity
 GIT_USER_NAME=$(git config --global user.name)
 GIT_USER_EMAIL=$(git config --global user.email)
-wex app::app/exec -vv -u=${CONTAINER_USER} -c="git config --global user.name '${GIT_USER_NAME}'"
-wex app::app/exec -vv -u=${CONTAINER_USER} -c="git config --global user.email '${GIT_USER_EMAIL}'"
+wex app::app/exec -vv -u="${CONTAINER_USER}" -c="git config --global user.name '${GIT_USER_NAME}'"
+wex app::app/exec -vv -u="${CONTAINER_USER}" -c="git config --global user.email '${GIT_USER_EMAIL}'"
 
-_wexLog "Copying .git inside container"
-docker cp .git $(wex app/container -c=dev):/var/www/html
-
-_wexLog "Copying ssh credentials inside container"
-docker cp ~/.ssh/ $(wex app/container -c=dev):/home/${CONTAINER_USER}/
-
-# Enforce asking passphrase for ssh key.
-wex app::app/exec -vv -u="cd /var/www/html && git status"
+# Get rid of old branches
+wex app::app/exec -vv -u="${CONTAINER_USER}" -c="cd /var/www/html && git prune"
 
 # Install wex.
-wex app::app/exec -vv -c=". /var/www/html/cli/install"
+wex app::app/exec -vv -u="${CONTAINER_USER}" -c=". /var/www/html/cli/install"
 
