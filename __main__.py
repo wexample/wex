57c3ae426b6d089c56512ecb8e582ab35b2d66d0
weeger <romain.weeger@gmail.com>
2023-03-24 12:02:51 +0100

    First call method

---
+++ __main__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Main entry point for the application."""
+
+from src.core.Kernel import Kernel
+
+if __name__ == '__main__':
+    Kernel(__file__).call()
---
+++ src/core/Kernel.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from typing import Optional
+
+
+class Kernel:
+    path: dict[str, Optional[str]] = {
+        'root': None,
+        'addons': None
+    }
+
+    def __init__(self, path_root):
+        # Initialize global variables.
+        self.path['root'] = os.path.dirname(os.path.realpath(path_root)) + '/'
+
+    def call(self):
+        if not len(sys.argv) > 1:
+            return
