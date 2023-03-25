b3f5a0809c6801cc23625a75ddb9bb760df9b0e6
weeger <romain.weeger@gmail.com>
2023-03-25 11:07:17 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ setup(
     name='wex',
     version=WEX_VERSION,
     description='A bash command manager',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     author='Wexample',
     author_email='contact@wexample.com',
+    url='https://github.com/wexample/wex',
     packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
     entry_points={
         'console_scripts': [
-            'wex = wex.__main__:main'
-        ]
+            'wex = wex.__main__:main',
+        ],
     },
-    long_description="""Wex is a powerful command-line tool that allows you to manage your bash commands with ease. With Wex, you can easily create, organize, and execute bash commands from anywhere in your terminal.
-
-    Features:
-    - Manage your bash commands with ease
-    - Create, organize, and execute bash commands from anywhere in your terminal
-    - Simple and intuitive command-line interface
-
-    Get started with Wex today and take control of your bash commands!"""
 )
