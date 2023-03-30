11c9091a4b7ffa620214a04567a4202aeeda95c7
weeger <romain.weeger@gmail.com>
2023-03-30 13:35:52 +0200

    Split master and build

--- .wex/script/appStarted.sh
+++ .wex/script/appStarted.sh
@@ -10,15 +10,7 @@ GIT_USER_EMAIL=$(sudo -u "${SUDO_USER}" git config --global user.email)
 wex app::app/exec -vv -u="${CONTAINER_USER}" -c="git config --global user.name '${GIT_USER_NAME}'"
 wex app::app/exec -vv -u="${CONTAINER_USER}" -c="git config --global user.email '${GIT_USER_EMAIL}'"
 
-# Give root permission.
-wex app::app/exec -vv -c="chown -R root:root /root/.gnupg"
-wex app::app/exec -vv -c="chmod 700 /root/.gnupg"
-
-# For debian package
-wex app::app/exec -vv -c="export DEBEMAIL='${GIT_USER_EMAIL}'"
-
 # Get rid of old branches
-
 wex app::app/exec -vv -c="touch /home/owner/.gitconfig"
 wex app::app/exec -vv -c="chown -R ${CONTAINER_USER}:${CONTAINER_USER} /home/owner/"
 wex app::app/exec -vv -c="chown -R ${CONTAINER_USER}:${CONTAINER_USER} /opt/wex"
