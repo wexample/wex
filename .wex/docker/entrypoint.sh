16f1fe5d7db36e9905ecc0dedae40272e13bb98e
weeger <romain.weeger@gmail.com>
2023-03-30 13:46:31 +0200

    Fix folder

--- .wex/docker/entrypoint.sh
+++ .wex/docker/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd /opt/wex && pip install -r requirements.txt
-
 exec "$@"
--- .wex/script/appInstall.sh
+++ .wex/script/appInstall.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 _wexMessage "Install pip requirements"
-
-wex app::app/exec -c="cd /opt/wex && sudo pip-compile requirements.in && sudo pip install -r requirements.txt"
