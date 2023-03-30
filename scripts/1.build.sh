6ee059c640b03c79c823d78475d714a6dd786c95
weeger <romain.weeger@gmail.com>
2023-03-30 14:08:17 +0200

    Build

--- scripts/1.build.sh
+++ scripts/1.build.sh
@@ -1,20 +1,29 @@
-# 1
-# Le répertoire est monté dans le container wex
+#!/usr/bin/env bash
 
-cd /var/www/build/ || return
-rm -rf workdir/wex_5.0.0~beta.2/wex
-rm workdir/wex_5.0.0~beta.2.orig.tar.gz
+VERSION="wex_5.0.0~beta.3"
+PATH_ROOT="$(realpath "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")/"
+PATH_BUILD="${PATH_ROOT}builds/"
+PATH_BUILD_SOURCE="${PATH_BUILD}${VERSION}/wex"
+cd "${PATH_ROOT}" || return
 
-mkdir /var/www/build/workdir/wex_5.0.0~beta.2/wex
-cd /var/www/build/workdir/wex_5.0.0~beta.2/wex || return
-git clone /opt/wex .
+echo "Cleanup build dir for ${VERSION}"
+rm -rf "${PATH_BUILD_SOURCE}"
+rm -f "${PATH_BUILD}${VERSION}.orig.tar.gz"
+
+mkdir -p "${PATH_BUILD_SOURCE}"
+cd "${PATH_BUILD_SOURCE}" || return
+
+echo "Cloning la sources to build folder"
+git clone "${PATH_ROOT}source/" .
+
+echo "Remove useless files"
 rm -rf .git
+rm -rf .wex
 find . -name ".gitignore" -type f -delete
 
-cd /var/www/build/workdir/ || return
-tar -czvf wex_5.0.0~beta.2.orig.tar.gz wex_5.0.0~beta.2/wex
 
-cd /var/www || return
-chown -R owner:owner build
+cd "${PATH_BUILD}" || return
+tar -czvf "${VERSION}.orig.tar.gz" "${VERSION}/wex"
+chown -R owner:owner .
 
-# --> pack.sh
+echo "Done."
