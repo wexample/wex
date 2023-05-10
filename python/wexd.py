7308264e93463cd900df39202659ba53457ea79a
weeger <romain.weeger@gmail.com>
2023-05-10 15:31:17 +0200

    Improve deamon

--- python/wexd.py
+++ python/wexd.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import re
+import subprocess
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -8,11 +11,25 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers()
 
-        message = "WEX"
-        self.wfile.write(bytes(message, "utf8"))
+        url = urlparse(self.path)
+        self.parse_url(url)
+
+    def parse_url(self, parsed_url):
+        path = parsed_url.path
+
+        pattern = r'^\/webhook/([a-zA-Z]+)/([a-zA-Z]+)$'
+        match = re.match(pattern, path)
+
+        if match:
+            part1, part2 = match.groups()
+
+            query_string_data = parse_qs(parsed_url.query)
+
+            output = subprocess.check_output(["wex", "hi"])
+            self.wfile.write(output)
 
 def serve(port):
-    with HTTPServer(('', 4242), handler) as server:
+    with HTTPServer(('', port), handler) as server:
         server.serve_forever()
 
 if __name__ == "__main__":
