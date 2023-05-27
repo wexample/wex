6b3bf63b6517ce244531335ca3e0d206cdac54a3
weeger <romain.weeger@gmail.com>
2023-05-27 07:47:09 +0200

    Append once

---
+++ addons/default/tests/command/file/append_once.py
@@ -0,0 +1,38 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestDefaultCommandFileAppend_once(AbstractTestCase):
+    def test_append_once(self):
+        file = self.build_test_file_path('config_bash')
+
+        with open(file, 'r', newline='') as f:
+            file_original = f.read()
+            first_line = f.readline()
+
+        self.kernel.exec('default::file/append_once', {
+            'file': file,
+            'line': first_line
+        })
+
+        with open(file, 'r', newline='') as f:
+            file_modified = f.read()
+
+        self.assertEqual(
+            file_original,
+            file_modified,
+            'The file should not be modified'
+        )
+
+        self.kernel.exec('default::file/append_once', {
+            'file': file,
+            'line': 'NEW_LINE=yes'
+        })
+
+        with open(file, 'r', newline='') as f:
+            file_modified = f.read()
+
+        self.assertNotEqual(
+            file_original,
+            file_modified,
+            'The file should be modified'
+        )
