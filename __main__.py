b93c27941f678998ef1f0540f06e8d95d26b5b12
weeger <romain.weeger@gmail.com>
2023-05-27 05:31:48 +0200

    Helper

--- __main__.py
+++ __main__.py
@@ -5,7 +5,12 @@ from src.core.Kernel import Kernel
 import sys
 
 if __name__ == '__main__':
+    arg = sys.argv[1] if len(sys.argv) > 1 else None
+    if arg is None:
+        print('Please use the "bash ./cli/wex" file to run wex script.')
+        sys.exit(1)
+
     Kernel(
         __file__,
-        sys.argv[1]
+        arg
     ).call()
