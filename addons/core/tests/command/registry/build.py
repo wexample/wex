26f1fa738cff4f53137c94c4f820a48aec8f5251
weeger <romain.weeger@gmail.com>
2023-05-27 07:42:52 +0200

    Fix registry first build

--- addons/core/tests/command/registry/build.py
+++ addons/core/tests/command/registry/build.py
@@ -2,13 +2,15 @@ import os
 
 from tests.AbstractTestCase import AbstractTestCase
 from src.const.globals import FILE_REGISTRY
+from src.helper.file import remove_file_if_exists
 
 
 class TestCoreCommandRegistryBuild(AbstractTestCase):
     def test_build(self):
         registry_path = f'{self.kernel.path["tmp"]}{FILE_REGISTRY}'
 
-        os.remove(registry_path)
+        remove_file_if_exists(registry_path)
+
         self.kernel.exec('core::registry/build')
 
         self.assertFileExists(registry_path)
