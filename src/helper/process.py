548c91da316bb84713bf623315163ac4519d3d47
weeger <romain.weeger@gmail.com>
2023-05-31 08:54:09 +0200

    Post process func

--- src/helper/process.py
+++ src/helper/process.py
@@ -1,3 +1,8 @@
+from src.const.globals import CORE_COMMAND_FILE
+from src.helper.args import convert_dict_to_args
+from src.helper.command import build_command_from_function
+
+
 def process_post_exec(kernel, args):
     # Print joined command in a post process file.
     with open(kernel.path['tmp'] + '/process/' + str(kernel.process_id) + '.post-exec', 'a') as f:
@@ -5,3 +10,20 @@ def process_post_exec(kernel, args):
             ' '.join(['"' + arg + '"' if ' ' in arg else arg for arg in args])
             + '\n'
         )
+
+
+def process_post_exec_wex(kernel, function: callable, args={}, is_async=False):
+    command = [
+        'bash',
+        CORE_COMMAND_FILE,
+        build_command_from_function(function),
+    ] + convert_dict_to_args(function, args)
+
+    if is_async:
+        command.insert(0, 'nohup')
+        command += ['>', '/dev/null', '2>&1', '&']
+
+    process_post_exec(
+        kernel,
+        command
+    )
