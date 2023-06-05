e966a9f5fae123d718e6fec618c5d8fd5976f76d
weeger <romain.weeger@gmail.com>
2023-06-05 13:21:42 +0200

    Command create

---
+++ addons/core/command/command/create.py
@@ -0,0 +1,40 @@
+import click
+import os
+
+from addons.core.command.test.create import core__test__create
+from src.helper.file import create_from_template
+from src.helper.command import build_function_name_from_match
+
+
+@click.command()
+@click.pass_obj
+@click.option('--command', '-c', type=str, required=True, help="Full name of the command, i.e. addon::some/thing")
+def core__command__create(kernel, command: str) -> {}:
+    kernel.log('Creating command file...')
+
+    match = kernel.build_match_or_fail(command)
+
+    command_path: str = kernel.build_command_path_from_match(match)
+    dir = os.path.dirname(command_path)
+    function_name = build_function_name_from_match(match)
+
+    os.makedirs(dir, exist_ok=True)
+
+    create_from_template(
+        kernel.path['templates'] + 'command.py.tpl',
+        command_path,
+        {
+            'function_name': function_name,
+        }
+    )
+
+    kernel.log_notice(f'Created command file : {command_path}')
+
+    test_file = kernel.exec_function(core__test__create, {
+        'command': command
+    })
+
+    return {
+        'command': command_path,
+        'test': test_file
+    }
---
+++ addons/core/tests/command/command/create.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+
+from addons.core.command.command.create import core__command__create
+from src.helper.file import remove_file_if_exists
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestCoreCommandCommandCreate(AbstractTestCase):
+    def test_create(self):
+        test_file_path_command = f'command/lorem/ipsum.py'
+        test_file_path = f"{self.kernel.path['addons']}core/{test_file_path_command}"
+        test_file_path_test = f"{self.kernel.path['addons']}core/tests/{test_file_path_command}"
+
+        remove_file_if_exists(test_file_path)
+        remove_file_if_exists(test_file_path_test)
+
+        self.kernel.exec_function(
+            core__command__create,
+            {
+                'command': 'core::lorem/ipsum'
+            }
+        )
+
+        self.assertFileExists(
+            test_file_path
+        )
+
+        self.assertFileExists(
+            test_file_path_test
+        )
+
+        # Cleanup created file.
+        shutil.rmtree(
+            os.path.dirname(
+                test_file_path
+            )
+        )
+
+        shutil.rmtree(
+            os.path.dirname(
+                test_file_path_test
+            )
+        )
+
+        self.assertTrue(True)
