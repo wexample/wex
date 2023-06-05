7fb30c11d1bdc4d2316c9cc4566a6a9456de78ee
weeger <romain.weeger@gmail.com>
2023-06-05 09:49:06 +0200

    Make webhook handler works

---
+++ addons/core/tests/command/webhook/exec.py
@@ -0,0 +1,10 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestCoreCommandWebhookExec(AbstractTestCase):
+    def test_exec(self):
+        # TODO
+        # self.kernel.exec('core::webhook/exec', {
+        #     'name': 'test'
+        # })
+        self.assertTrue(True)
