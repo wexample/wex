ef0520127b7e2d74aa277573173c360123171c7e
weeger <romain.weeger@gmail.com>
2023-06-05 09:47:38 +0200

    Prepare tests scripts

---
+++ .wex-5-dev/webhook/test-wraped.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wex app/exec -c="bash /opt/wex/python/tests/Resources/test-wraped.sh ${*}" --non_interactive
---
+++ .wex-5-dev/webhook/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "OK"
+echo "0 : ${0}"
+echo "1 : ${1}"
+echo "2 : ${2}"
+echo "3 : ${3}"
