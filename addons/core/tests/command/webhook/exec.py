06309b755b85737c89c2038395c1dd5f2284226d
weeger <romain.weeger@gmail.com>
2023-06-06 13:44:40 +0200

    Code style

--- addons/core/tests/command/webhook/exec.py
+++ addons/core/tests/command/webhook/exec.py
@@ -1,10 +1,33 @@
 from tests.AbstractTestCase import AbstractTestCase
 
+from addons.core.command.webhook.exec import core__webhook__exec
+
 
 class TestCoreCommandWebhookExec(AbstractTestCase):
     def test_exec(self):
-        # TODO
-        # self.kernel.exec('core::webhook/exec', {
-        #     'name': 'test'
-        # })
-        self.assertTrue(True)
+        success = self.kernel.exec_function(
+            core__webhook__exec,
+            {
+                'url': 'http://localhost:4242/webhook/wex/test'
+            }
+        )
+
+        self.assertTrue(success)
+
+        success = self.kernel.exec_function(
+            core__webhook__exec,
+            {
+                'url': 'http://localhost:4242/webhook/wex/test?lorem=ipsum'
+            }
+        )
+
+        self.assertTrue(success)
+
+        success = self.kernel.exec_function(
+            core__webhook__exec,
+            {
+                'url': 'http://localhost:4242/webhook/wex/test-wraped?p=155&v=wex_5.0.0-beta.6+build.20230321054915'
+            }
+        )
+
+        self.assertTrue(success)
