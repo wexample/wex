ead40e7cd3a31c406a4f87f5bc588285098e6c8e
weeger <romain.weeger@gmail.com>
2023-06-08 09:51:20 +0200

    core/uninstall

---
+++ addons/core/command/core/uninstall.py
@@ -0,0 +1,10 @@
+import click
+
+from src.decorator.as_sudo import as_sudo
+
+
+@click.command()
+@click.pass_obj
+@as_sudo
+def core__core__uninstall(kernel):
+    pass
---
+++ addons/core/tests/command/core/uninstall.py
@@ -0,0 +1,10 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestCoreCommandCoreUninstall(AbstractTestCase):
+    def test_uninstall(self):
+        # TODO
+        # self.kernel.exec_function(test_uninstall, {
+        #     'name': 'test'
+        # })
+        self.assertTrue(True)
