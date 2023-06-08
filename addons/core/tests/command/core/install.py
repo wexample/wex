8fedab3cb14d8c0c765d88143a802774be7249be
weeger <romain.weeger@gmail.com>
2023-06-08 09:44:44 +0200

    core/install

---
+++ addons/core/tests/command/core/install.py
@@ -0,0 +1,10 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestCoreCommandCoreInstall(AbstractTestCase):
+    def test_install(self):
+        # TODO
+        # self.kernel.exec_function(test_install, {
+        #     'name': 'test'
+        # })
+        self.assertTrue(True)
