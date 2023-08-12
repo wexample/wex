b591f64a9e03ea1c6f898c3c8530e632ec5a4b5d
weeger <romain.weeger@gmail.com>
2023-08-12 21:03:00 +0200

    Fix pipeline

--- .wex/script/gpgExport.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-gpg --armor --export contact@wexample.com > .wex/source/gpg/public.key
-gpg --export-secret-keys -a contact@wexample.com > .wex/source/gpg/private.key.asc
-
-# In container
-#gpg --import /root/.gnupg/private.key.asc
-#gpg --import /root/.gnupg/public.key
