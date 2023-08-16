f46cf4ca922bff9831206b2b54a8f116a4dc670a
weeger <romain.weeger@gmail.com>
2023-08-16 16:11:18 +0200

    Fix install

--- addons/core/command/core/install.py
+++ addons/core/command/core/install.py
@@ -1,6 +1,8 @@
 import click
 import os
 
+from addons.app.const.app import APP_ENV_LOCAL
+from addons.app.helpers.app import create_env
 from addons.core.command.logo.show import core__logo__show
 from addons.core.command.webhook.serve import core__webhook__serve
 from addons.default.command.file.append_once import default__file__append_once
@@ -15,11 +17,19 @@ from src.decorator.as_sudo import as_sudo
 @click.pass_obj
 @as_sudo
 def core__core__install(kernel):
+    __core__core__install_env(kernel)
     __core__core__install_terminal(kernel)
     __core__core__install_autocomplete(kernel)
     __core__core__install_symlink(kernel)
     __core__core__install_webhook_server(kernel)
     return kernel.exec_function(core__logo__show)
+
+
+def __core__core__install_env(kernel):
+    create_env(
+        APP_ENV_LOCAL,
+        kernel.path['root']
+    )
 
 
 def __core__core__install_terminal(kernel):
