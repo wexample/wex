dda280675ae891fe9721e1cea6da7ddac7782c32
weeger <romain.weeger@gmail.com>
2023-08-16 15:41:04 +0200

    Fix uninstall

--- addons/core/command/core/uninstall.py
+++ addons/core/command/core/uninstall.py
@@ -1,8 +1,10 @@
 import click
+import os
 
 from addons.core.command.webhook.stop import core__webhook__stop
 from addons.default.command.file.remove_line import default__file__remove_line
-from src.helper.core import get_bashrc_handler_path, get_bashrc_handler_command
+from src.helper.system import get_sudo_username, get_sudo_user_home_path
+from addons.system.command.system.is_docker import system__system__is_docker
 from src.const.globals import CORE_BIN_FILE
 from src.decorator.as_sudo import as_sudo
 from src.helper.file import remove_file_if_exists
@@ -14,7 +16,8 @@ from src.helper.file import remove_file_if_exists
 def core__core__uninstall(kernel):
     __core__core__uninstall_webhook_server(kernel)
     __core__core__uninstall_symlink()
-    __core__core__uninstall_bashrc(kernel)
+    __core__core__uninstall_autocomplete(kernel)
+    __core__core__uninstall_terminal(kernel)
     kernel.log('Bye!')
 
 
@@ -28,17 +31,47 @@ def __core__core__uninstall_symlink():
     remove_file_if_exists(CORE_BIN_FILE)
 
 
-def __core__core__uninstall_bashrc(kernel):
-    bashrc_handler_path = get_bashrc_handler_path(kernel)
-    bashrc_handler_command = get_bashrc_handler_command(kernel)
-    kernel.log(f'Removing autocompletion script to {bashrc_handler_path}...')
+def __core__core__uninstall_autocomplete(kernel):
+    script_path = '/etc/bash_completion.d/wex'
+
+    remove_file_if_exists(script_path)
+
+    __remove_source_file_for_docker(kernel, script_path)
+
+
+def __core__core__uninstall_terminal(kernel):
+    script_path = '/etc/profile.d/wex'
+
+    remove_file_if_exists(script_path)
+
+    __remove_source_file_for_docker(kernel, script_path)
+
+
+def __remove_source_file_for_docker(kernel, file_path):
+    if not kernel.exec_function(system__system__is_docker):
+        return
+
+    # If sudo has a parent user.
+    sudo_user = get_sudo_username()
+    if sudo_user:
+        __remove_source_file_in_bashrc(kernel, file_path, f'{get_sudo_user_home_path()}.bashrc')
+
+    __remove_source_file_in_bashrc(kernel, file_path, os.path.expanduser('~/.bashrc'))
+
+
+def __remove_source_file_in_bashrc(kernel, file_path, bashrc_path):
+    if not os.path.exists(bashrc_path):
+        with open(bashrc_path, 'w') as f:
+            pass
+
+    kernel.log(f'Removing autocompletion script to {bashrc_path}...')
 
     kernel.exec_function(
         default__file__remove_line,
         {
-            'file-path': bashrc_handler_path,
-            'line': bashrc_handler_command
+            'file-path': bashrc_path,
+            'line': f'. {file_path}'
         }
     )
 
-    kernel.message(f'Updated bashrc {bashrc_handler_path}')
+    kernel.message(f'Updated bashrc {bashrc_path}')
