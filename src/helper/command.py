e025040f3befaccd52aa748d3875c322770af5cd
weeger <romain.weeger@gmail.com>
2023-08-17 15:18:38 +0200

    Find use commands

--- src/helper/command.py
+++ src/helper/command.py
@@ -40,13 +40,9 @@ def build_command_match(command: str):
     return None, None
 
 
-def build_command_path_from_match(kernel, match, command_type: str, subdir=None):
+def build_command_path_from_match(kernel, match, command_type: str):
     if command_type == COMMAND_TYPE_ADDON:
         base_path = f"{kernel.path['addons']}{match.group(1)}/"
-
-        # if subdir:
-        #     base_path += f"{subdir}/"
-
         return f"{base_path}command/{match.group(2)}/{match.group(3)}.py"
     elif command_type == COMMAND_TYPE_APP:
         return os.path.join(
@@ -63,9 +59,13 @@ def build_command_path_from_match(kernel, match, command_type: str, subdir=None)
             match[2] + '.py'
         )
     elif command_type == COMMAND_TYPE_USER:
-        return command_type
-    elif command_type == COMMAND_TYPE_CORE:
-        return command_type
+        return os.path.join(
+            os.path.expanduser('~'),
+            APP_DIR_APP_DATA,
+            'command',
+            match[1],
+            match[2] + '.py'
+        )
 
     return None
 
