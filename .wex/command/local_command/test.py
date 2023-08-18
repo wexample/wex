fa0e8260a8ee8e0cb7b18bc54526de24e3e830d5
weeger <romain.weeger@gmail.com>
2023-08-18 18:38:40 +0200

    Better test command

--- .wex/command/local_command/test.py
+++ .wex/command/local_command/test.py
@@ -1,3 +1,7 @@
+import click
 
-def app__local_command__test():
-    print('OK')
+
+@click.command
+@click.option('--arg_one', '-a', type=str, required=True)
+def app__local_command__test(arg_one: str):
+    return f'OK:{arg_one}'
