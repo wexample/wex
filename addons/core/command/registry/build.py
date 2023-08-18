6cc55ac985dc5081e4b36990471b213b5be285a3
weeger <romain.weeger@gmail.com>
2023-08-18 13:47:56 +0200

    registry build improve

--- addons/core/command/registry/build.py
+++ addons/core/command/registry/build.py
@@ -2,11 +2,10 @@ import click
 import os
 import json
 
-from addons.app.const.app import APP_FILE_APP_SERVICE_CONFIG
-from helper.registry import scan_commands
+from src.helper.registry import build_registry_addons, build_registry_services
 from src.decorator.as_sudo import as_sudo
-from src.const.globals import FILE_REGISTRY, COMMAND_CHAR_SERVICE
-from src.helper.file import list_subdirectories, set_sudo_user_owner
+from src.const.globals import FILE_REGISTRY
+from src.helper.file import set_sudo_user_owner
 
 
 @click.command
@@ -15,44 +14,10 @@ from src.helper.file import list_subdirectories, set_sudo_user_owner
 def core__registry__build(kernel):
     kernel.log('Building registry...')
     addons = kernel.addons
-    addons_dict = {}
-
-    for addon in addons:
-        if os.path.exists(os.path.join(kernel.path['addons'], addon, 'command')):
-            command_dict = {}
-            for group in list_subdirectories(os.path.join(kernel.path['addons'], addon, 'command')):
-                command_dict.update(scan_commands(
-                    os.path.join(kernel.path['addons'], addon, 'command', group),
-                    group,
-                    f"{addon}::"
-                ))
-            addons_dict[addon] = {
-                'name': addon,
-                'commands': command_dict
-            }
 
     registry = {
-        'addons': addons_dict,
-        'services': {
-            service: {
-                'name': service,
-                'commands': scan_commands(
-                    os.path.join(services_dir, service, 'command'),
-                    service,
-                    f"{COMMAND_CHAR_SERVICE}"
-                ),
-                'addon': addon,
-                'dir': os.path.join(services_dir, service) + '/',
-                "config": json.load(
-                    open(os.path.join(services_dir, service, APP_FILE_APP_SERVICE_CONFIG))) if os.path.exists(
-                    os.path.join(services_dir, service, APP_FILE_APP_SERVICE_CONFIG)) else {}
-            }
-            for addon in addons
-            for addon_dir in [os.path.join(kernel.path['addons'], addon)]
-            for services_dir in [os.path.join(addon_dir, 'services')]
-            if os.path.exists(services_dir)
-            for service in os.listdir(os.path.join(kernel.path['addons'], addon, 'services'))
-        }
+        'addons': build_registry_addons(addons, kernel),
+        'services': build_registry_services(addons, kernel)
     }
 
     registry_path = os.path.join(kernel.path["tmp"], FILE_REGISTRY)
--- src/helper/registry.py
+++ src/helper/registry.py
@@ -1,3 +1,11 @@
+import json
+import os
+
+from addons.app.const.app import APP_FILE_APP_SERVICE_CONFIG
+from src.const.globals import COMMAND_CHAR_SERVICE
+from src.helper.file import list_subdirectories
+
+
 def get_commands_groups_names(kernel, addon):
     group_names = set()
 
@@ -25,3 +33,52 @@ def get_all_services_names(kernel):
         output.append(kernel.registry['services'][service]['name'])
 
     return output
+
+
+def scan_commands(directory, group, prefix, endswith='.py'):
+    """Scans the given directory for command files and returns a dictionary of found commands."""
+    commands = {}
+    for command in os.listdir(directory):
+        if command.endswith(endswith):
+            command_name, ext = os.path.splitext(command)
+            test_file = os.path.realpath(os.path.join(directory, '../../tests/command', group, command))
+            commands[f"{prefix}{group}/{command_name}"] = {
+                'file': os.path.join(directory, command),
+                'test': test_file if os.path.exists(test_file) else None
+            }
+    return commands
+
+
+def build_registry_addons(addons, kernel):
+    addons_dict = {}
+
+    for addon in addons:
+        addon_path = os.path.join(kernel.path['addons'], addon, 'command')
+        if os.path.exists(addon_path):
+            command_dict = {}
+            for group in list_subdirectories(addon_path):
+                group_path = os.path.join(addon_path, group)
+                command_dict.update(scan_commands(group_path, group, f"{addon}::"))
+            addons_dict[addon] = {'name': addon, 'commands': command_dict}
+
+    return addons_dict
+
+
+def build_registry_services(addons, kernel):
+    services_dict = {}
+
+    for addon in addons:
+        services_dir = os.path.join(kernel.path['addons'], addon, 'services')
+        if os.path.exists(services_dir):
+            for service in os.listdir(services_dir):
+                service_path = os.path.join(services_dir, service)
+                config_file_path = os.path.join(service_path, APP_FILE_APP_SERVICE_CONFIG)
+                services_dict[service] = {
+                    'name': service,
+                    'commands': scan_commands(os.path.join(service_path, 'command'), service, f"{COMMAND_CHAR_SERVICE}"),
+                    'addon': addon,
+                    'dir': service_path + '/',
+                    "config": json.load(open(config_file_path)) if os.path.exists(config_file_path) else {}
+                }
+
+    return services_dict
