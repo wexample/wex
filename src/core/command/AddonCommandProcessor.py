257703883bcef8de6d4bc02d64a6ff5152fb919e
weeger <romain.weeger@gmail.com>
2023-08-22 00:27:39 +0200

    Cleanup

--- src/core/command/AddonCommandProcessor.py
+++ src/core/command/AddonCommandProcessor.py
@@ -70,35 +70,7 @@ class AddonCommandProcessor(AbstractCommandProcessor):
         from src.const.globals import COMMAND_SEPARATOR_ADDON, COMMAND_CHAR_SERVICE, COMMAND_CHAR_USER, COMMAND_CHAR_APP
 
         if cursor == 0:
-            # User typed "wex ~"
-            # if search_split[0] == COMMAND_CHAR_USER:
-            #     pass
-            #
-            # #         import os
-            # #         from src.helper.suggest import suggest_from_path
-            # #         from addons.app.const.app import APP_DIR_APP_DATA
-            # #
-            # #         suggestion = suggest_from_path(
-            # #             f'{os.path.expanduser("~")}/',
-            # #             search_split[0]
-            # #         )
-            # #     # User typed "wex ."
-            # #     elif search_split[0].startswith(COMMAND_CHAR_APP):
-            # #         from addons.app.command.location.find import app__location__find
-            # #         from src.helper.suggest import suggest_from_path
-            # #
-            # #         app_path = kernel.exec_function(app__location__find)
-            # #         # We are in an app dir or subdir
-            # #         if app_path:
-            # #             suggestion = suggest_from_path(app_path, search_split[0])
-            # #
-            # #     # User typed "wex @"
-            # #     elif search_split[0] == COMMAND_CHAR_SERVICE:
-            # #         from src.helper.suggest import get_all_services_names_suggestions
-            # #
-            # #         suggestion = get_all_services_names_suggestions(kernel)
-
-            # User typed "wex co"
+            # User typed "co"
             if search_split[0] != '':
                 suggestion = ' '.join([addon + COMMAND_SEPARATOR_ADDON for addon in self.kernel.registry['addons'].keys() if
                                        addon.startswith(search_split[0])])
@@ -111,22 +83,6 @@ class AddonCommandProcessor(AbstractCommandProcessor):
                 suggestion = ' '.join(addon + COMMAND_SEPARATOR_ADDON for addon in self.kernel.registry['addons'].keys())
                 # Adds also all core actions.
                 suggestion += ' ' + ' '.join(self.kernel.get_core_actions().keys())
-                # Suggest to execute service command
-                suggestion += f' \\{COMMAND_CHAR_SERVICE}'
-
-                import os
-                from addons.app.const.app import APP_DIR_APP_DATA
-                # User local command path exists
-                base_path = f'{os.path.expanduser("~")}/{APP_DIR_APP_DATA}command'
-                if os.path.exists(base_path):
-                    # Suggest to execute local user command
-                    suggestion += f' \\{COMMAND_CHAR_USER}'
-
-                from addons.app.command.location.find import app__location__find
-                # We are in an app dir or subdir
-                if self.kernel.exec_function(app__location__find):
-                    # Suggest to execute local app command
-                    suggestion += f' \\{COMMAND_CHAR_APP}'
 
                 return suggestion
 
