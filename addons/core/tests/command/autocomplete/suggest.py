c877ec14099ed41d7d42e54e431096d6c7434aca
weeger <romain.weeger@gmail.com>
2023-08-22 09:47:21 +0200

    Add tests

--- addons/core/tests/command/autocomplete/suggest.py
+++ addons/core/tests/command/autocomplete/suggest.py
@@ -1,4 +1,8 @@
+import os.path
+import time
+
 from addons.core.command.autocomplete.suggest import core__autocomplete__suggest
+from addons.core.command.command.create import core__command__create
 from src.const.globals import COMMAND_CHAR_USER, COMMAND_CHAR_SERVICE, COMMAND_CHAR_APP, COMMAND_SEPARATOR_ADDON, \
     COMMAND_SEPARATOR_GROUP
 from tests.AbstractTestCase import AbstractTestCase
@@ -8,6 +12,9 @@ class TestCoreCommandAutocompleteSuggest(AbstractTestCase):
     def test_suggest(self):
         self.check_suggest_addon()
         self.check_suggest_addon_args()
+        self.check_suggest_app()
+        self.check_suggest_service()
+        self.check_suggest_user()
 
     def check_suggest_addon(self):
         # User ask "", it should suggest all
@@ -23,7 +30,7 @@ class TestCoreCommandAutocompleteSuggest(AbstractTestCase):
 
         self.assertTrue(COMMAND_CHAR_APP in suggestions)
         self.assertTrue(COMMAND_CHAR_SERVICE in suggestions)
-        self.assertTrue(COMMAND_CHAR_USER in suggestions)
+        # Do not test user char as it may not exist at this point
 
         # User ask "co", it should suggest "core::",
         # with addon separator as there is no more
@@ -123,6 +130,7 @@ class TestCoreCommandAutocompleteSuggest(AbstractTestCase):
         )
 
     def check_suggest_addon_args(self):
+        # Search full addon command name with a final space
         suggestions = self.kernel.exec_function(
             core__autocomplete__suggest,
             {
@@ -131,7 +139,7 @@ class TestCoreCommandAutocompleteSuggest(AbstractTestCase):
                     'core',
                     COMMAND_SEPARATOR_ADDON,
                     'autocomplete' + COMMAND_SEPARATOR_GROUP + 'suggest'
-                    ' '
+                                                               ' '
                 ])
             }
         )
@@ -139,3 +147,92 @@ class TestCoreCommandAutocompleteSuggest(AbstractTestCase):
         self.assertTrue(
             len(suggestions.split(' ')) >= 2
         )
+
+    def check_suggest_service(self):
+        # Search only "@", should return all service commands
+        suggestions: str = self.kernel.exec_function(
+            core__autocomplete__suggest,
+            {
+                'cursor': 0,
+                'search': COMMAND_CHAR_SERVICE
+            }
+        )
+
+        self.assertTrue(
+            len(suggestions.split(' ')) >= 2
+        )
+
+        # Search "@te", to find created command
+        self.create_and_test_created_command(
+            COMMAND_CHAR_SERVICE,
+            '@ te'
+        )
+
+    def create_and_test_created_command(self, prefix:str, search:str = None):
+        command = prefix + 'test/autocomplete_command'
+        if not search:
+            search = prefix + 'te'
+
+        self.kernel.log(f'Testing command {command}')
+
+        # First create command
+        info = self.kernel.exec_function(
+            core__command__create,
+            {
+                'command': command
+            }
+        )
+
+        suggestions: str = self.kernel.exec_function(
+            core__autocomplete__suggest,
+            {
+                'cursor': 0,
+                'search': search
+            }
+        )
+
+        print(prefix)
+        print(suggestions)
+
+        self.assertTrue(
+            command in suggestions
+        )
+
+        self.assertTrue(
+            os.path.isfile(
+                info['command']
+            )
+        )
+
+        os.remove(info['command'])
+
+        # Test is optional at this point
+        if info['test']:
+            os.remove(info['test'])
+
+    def check_suggest_app(self):
+        # Search only ".", should return all app commands
+        suggestions: str = self.kernel.exec_function(
+            core__autocomplete__suggest,
+            {
+                'cursor': 0,
+                'search': COMMAND_CHAR_APP
+            }
+        )
+
+        # There is at least on custom app command in wex
+        self.assertTrue(
+            len(suggestions.split(' ')) >= 1
+        )
+
+        # Search ".te", to find created command
+        self.create_and_test_created_command(
+            COMMAND_CHAR_APP,
+        )
+
+    def check_suggest_user(self):
+        # Search "~te", to find created command
+        self.create_and_test_created_command(
+            COMMAND_CHAR_USER,
+        )
+
