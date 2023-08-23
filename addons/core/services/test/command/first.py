7aa54a0288197654d63182e44cb487f2d7fece3b
weeger <romain.weeger@gmail.com>
2023-08-23 13:05:06 +0200

    Help

--- addons/core/services/test/command/first.py
+++ addons/core/services/test/command/first.py
@@ -2,7 +2,9 @@ import click
 
 
 @click.command()
-@click.option('--option', '-o', is_flag=True, required=False)
-@click.option('--another-option', '-ao', is_flag=True, required=False)
+@click.option('--option', '-o', is_flag=True, required=False,
+              help="A first option as flag")
+@click.option('--another-option', '-ao', is_flag=True, required=False,
+              help="Another option")
 def service__test__first(option=None, another_option=None):
     return 'FIRST'
