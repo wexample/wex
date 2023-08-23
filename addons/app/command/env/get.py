9c7428ce3d3638a2eeef25de552961a4fdac24a2
weeger <romain.weeger@gmail.com>
2023-08-23 12:46:13 +0200

    Help

--- addons/app/command/env/get.py
+++ addons/app/command/env/get.py
@@ -5,8 +5,10 @@ from addons.app.const.app import APP_FILEPATH_REL_ENV
 
 
 @click.command
-@click.option('--key', '-k', type=str, required=False, default='APP_ENV')
-@click.option('--app-dir', '-a', type=str, required=False)
+@click.option('--key', '-k', type=str, required=False, default='APP_ENV',
+              help="Key in env file")
+@click.option('--app-dir', '-a', type=str, required=False,
+              help="App directory")
 def app__env__get(key: str = 'APP_ENV', app_dir: str = './') -> str:
     config = dotenv_values(app_dir + APP_FILEPATH_REL_ENV)
 
