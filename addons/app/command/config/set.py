ac6cec9576f91f9acad2aaeb8f88e48dfc39612b
weeger <romain.weeger@gmail.com>
2023-08-23 12:44:17 +0200

    Help

--- addons/app/command/config/set.py
+++ addons/app/command/config/set.py
@@ -4,10 +4,14 @@ from src.helper.file import set_json_file_item
 
 
 @click.command
-@click.option('--key', '-k', type=str, required=True)
-@click.option('--value', '-v', required=True)
-@click.option('--build', '-b', is_flag=True, required=False, default=False)
-@click.option('--app-dir', '-a', type=str, required=False)
+@click.option('--key', '-k', type=str, required=True,
+              help="Key in config file")
+@click.option('--value', '-v', required=True,
+              help="Value to set")
+@click.option('--build', '-b', is_flag=True, required=False, default=False,
+              help="Edit auto generated config or source config (default)")
+@click.option('--app-dir', '-a', type=str, required=False,
+              help="App directory")
 def app__config__set(key, value, build: bool = False, app_dir: str = None):
     if build:
         config_file = APP_FILEPATH_REL_CONFIG_BUILD
