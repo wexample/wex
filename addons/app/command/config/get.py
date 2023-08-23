b4307629f32476ba6419ac372e71d83ad8c1e488
weeger <romain.weeger@gmail.com>
2023-08-23 12:45:33 +0200

    Help

--- addons/app/command/config/get.py
+++ addons/app/command/config/get.py
@@ -4,10 +4,14 @@ from src.helper.file import get_json_file_item
 
 
 @click.command
-@click.option('--key', '-k', type=str, required=True)
-@click.option('--default', '-d', required=False)
-@click.option('--build', '-b', is_flag=True, required=False, default=False)
-@click.option('--app-dir', '-a', type=str, required=False)
+@click.option('--key', '-k', type=str, required=True,
+              help="Key in config file")
+@click.option('--default', '-d', required=False,
+              help="Default returned value if not found in config file")
+@click.option('--build', '-b', is_flag=True, required=False, default=False,
+              help="Edit auto generated config or source config (default)")
+@click.option('--app-dir', '-a', type=str, required=False,
+              help="App directory")
 def app__config__get(key: str, default: str = None, build: bool = False, app_dir: str = None):
     if build:
         config_file = APP_FILEPATH_REL_CONFIG_BUILD
