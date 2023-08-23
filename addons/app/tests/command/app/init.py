e892c36aedf160daec962e4b2954c3c340dcd0fe
weeger <romain.weeger@gmail.com>
2023-08-23 13:47:30 +0200

    Test app/init

---
+++ addons/app/tests/command/app/init.py
@@ -0,0 +1,8 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+from addons.app.helpers.test import create_test_app
+
+
+class TestAppCommandAppInit(AbstractTestCase):
+    def test_init(self):
+        create_test_app(self.kernel, services=['php-8'])
