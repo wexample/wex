099c596b3ffb28f9e8bf078574809a662c15b0e8
weeger <romain.weeger@gmail.com>
2023-08-24 17:53:18 +0200

    service/used

--- addons/app/command/service/used.py
+++ addons/app/command/service/used.py
@@ -5,9 +5,11 @@ from addons.app.command.config.get import app__config__get
 
 @click.command()
 @click.pass_obj
-@click.option('--app-dir', '-a', type=str, required=False)
-@click.option('--service', '-s', type=str, required=True)
-def app_service_used(kernel, service, app_dir):
+@click.option('--app-dir', '-a', type=str, required=True,
+              help="App directory")
+@click.option('--service', '-s', type=str, required=True,
+              help="Service name to check in app configuration")
+def app__service__used(kernel, service, app_dir):
     return service in kernel.exec_function(
         app__config__get,
         {
