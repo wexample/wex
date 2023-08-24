b30c9e46280d6c2c3e51ed89cf074dab6970fb20
weeger <romain.weeger@gmail.com>
2023-08-24 13:58:18 +0200

    Install proxy

--- addons/app/services/proxy/command/install.py
+++
@@ -1,6 +0,0 @@
-import click
-
-@click.command()
-def service__proxy__install():
-    # TODO
-    print('OK')
---
+++ addons/app/services/proxy/command/service/install.py
@@ -0,0 +1,31 @@
+import click
+import platform
+
+from addons.app.const.app import PROXY_APP_NAME
+from addons.app.helpers.app import config_save
+
+
+@click.command()
+@click.pass_obj
+def proxy__service__install(kernel):
+    port = 80
+    port_secure = 443
+
+    # MacOS changes (not tested inherited code)
+    if platform.system() == 'Darwin':
+        proxy_dir = '/Users/.wex/server/'
+        port = 7780
+        port_secure = 7743
+    else:
+        proxy_dir = '/opt/{}/'.format(PROXY_APP_NAME)
+
+    kernel.addons['app']['config']['global'].update({
+        'default_port': port,
+        'default_port_secure': port_secure,
+    })
+
+    kernel.addons['app']['config']['path'].update({
+        'proxy': proxy_dir
+    })
+
+    config_save(kernel)
