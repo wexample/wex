fd274fcbcad0907eaf851df657ff344d0d716233
weeger <romain.weeger@gmail.com>
2023-08-27 00:04:41 +0200

    install maria

---
+++ addons/services-db/services/maria-10/command/service/install.py
@@ -0,0 +1,22 @@
+import click
+
+from addons.app.helpers.app import config_save
+from src.const.globals import PASSWORD_INSECURE
+
+
+@click.command()
+@click.pass_obj
+def maria_10__service__install(kernel):
+    name = kernel.addons['app']['config']['global']['name']
+
+    kernel.addons['app']['config']['maria-10'] = {
+        'db': {
+            'host': f'{name}_maria_10',
+            'name': f'{name}',
+            'password': PASSWORD_INSECURE,
+            'port': 3306,
+            'user': 'root',
+        }
+    }
+
+    config_save(kernel)
