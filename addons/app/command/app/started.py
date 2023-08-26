5966f34e105e0da0a05dafc9127574e2c0210c46
weeger <romain.weeger@gmail.com>
2023-08-26 10:51:34 +0200

    app/started performance

--- addons/app/command/app/started.py
+++ addons/app/command/app/started.py
@@ -1,24 +1,14 @@
 import click
 
-from addons.app.command.config.get import app__config__get
-
 APP_STARTED_CHECK_MODE_CONFIG = 'config'
+APP_STARTED_CHECK_MODE_FULL = 'full'
 
 
 @click.command()
 @click.pass_obj
 @click.option('--app-dir', '-a', type=str, required=False)
-@click.option('--check-mode', '-cm', type=str, required=False, default=APP_STARTED_CHECK_MODE_CONFIG)
-def app__app__started(kernel, app_dir: str, check_mode: str = APP_STARTED_CHECK_MODE_CONFIG):
-    config_started = kernel.exec_function(
-        app__config__get,
-        {
-            'app-dir': app_dir,
-            'default': False,
-            'build': True,
-            'key': 'context.started',
-        }
-    )
-
-    if check_mode == APP_STARTED_CHECK_MODE_CONFIG:
-        return config_started
+@click.option('--check-mode', '-cm', type=str, required=False, default=APP_STARTED_CHECK_MODE_FULL)
+def app__app__started(kernel, app_dir: str, check_mode: str = APP_STARTED_CHECK_MODE_FULL):
+    if check_mode == APP_STARTED_CHECK_MODE_FULL or check_mode == APP_STARTED_CHECK_MODE_CONFIG:
+        if not kernel.addons['app']['config_build']['context']['started']:
+            return kernel.addons['app']['config_build']['context']['started']
