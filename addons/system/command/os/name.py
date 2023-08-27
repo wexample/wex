0ac3e7e44a1bc8d0584d8814edce7fa4d1202e24
weeger <romain.weeger@gmail.com>
2023-08-27 19:08:24 +0200

    Os name

--- addons/system/command/os/name.py
+++ addons/system/command/os/name.py
@@ -1,16 +1,21 @@
 import platform
 import click
 
+OS_NAME_LINUX = 'linux'
+OS_NAME_MAC = 'mac'
+OS_NAME_UNDEFINED = 'undefined'
+OS_NAME_WINDOWS = 'windows'
+
 
 @click.command(help="Return the local OS name.")
 def system__os__name():
     os_name = platform.system()
 
     if os_name == "Darwin":
-        return "mac"
+        return OS_NAME_MAC
     elif os_name == "Linux":
-        return "linux"
+        return OS_NAME_LINUX
     elif os_name in ["CYGWIN", "MINGW32", "MINGW64", "MSYS"]:
-        return "windows"
+        return OS_NAME_WINDOWS
     else:
-        return "undefined"
+        return OS_NAME_UNDEFINED
