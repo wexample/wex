16431e601c6df8972d542bdf2d35018f6cdf3287
weeger <romain.weeger@gmail.com>
2023-08-28 12:32:45 +0200

    service used

---
+++ addons/app/tests/command/service/used.py
@@ -0,0 +1,25 @@
+from addons.app.command.service.used import app__service__used
+from addons.app.helpers.test import create_test_app
+from app.command.service.install import app__service__install
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestAppCommandServiceUsedPy(AbstractTestCase):
+    def test_used(self):
+        app_dir = create_test_app(self.kernel)
+
+        self.kernel.exec_function(
+            app__service__install, {
+                'app-dir': app_dir,
+                'service': 'nextcloud'
+            }
+        )
+
+        self.assertTrue(
+            self.kernel.exec_function(
+                app__service__used, {
+                    'app-dir': app_dir,
+                    'service': 'nextcloud'
+                }
+            )
+        )
