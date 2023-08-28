732f589889e0ce76df0f38b509712f55e26668c4
weeger <romain.weeger@gmail.com>
2023-08-28 16:09:30 +0200

    Fix test

--- addons/app/tests/command/hook/exec.py
+++ addons/app/tests/command/hook/exec.py
@@ -1,13 +1,14 @@
 from addons.app.helpers.test import create_test_app
 from tests.AbstractTestCase import AbstractTestCase
 from addons.app.command.hook.exec import app__hook__exec
+from src.const.globals import COMMAND_CHAR_APP
 
 
 class TestAppCommandHookExec(AbstractTestCase):
     def test_exec(self):
         app_dir = create_test_app(self.kernel, services=['php-8'])
 
-        service_results, app_result = self.kernel.exec_function(
+        results = self.kernel.exec_function(
             app__hook__exec,
             {
                 'app-dir': app_dir,
@@ -16,11 +17,11 @@ class TestAppCommandHookExec(AbstractTestCase):
         )
 
         self.assertEqual(
-            service_results['php-8'],
+            results['php-8'],
             None
         )
 
         self.assertEqual(
-            app_result,
+            results[COMMAND_CHAR_APP],
             None
         )
