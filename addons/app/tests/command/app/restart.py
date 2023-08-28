9036aeb9d3aeccc858fa43456cadceb096070a74
weeger <romain.weeger@gmail.com>
2023-08-28 12:24:19 +0200

    app restart

--- addons/app/tests/command/app/restart.py
+++ addons/app/tests/command/app/restart.py
@@ -1,15 +1,28 @@
-from addons.app.helpers.test import create_test_app
+from addons.app.command.app.restart import app__app__restart
 from addons.app.command.app.start import app__app__start
 from addons.app.command.app.stop import app__app__stop
 from tests.AbstractTestCase import AbstractTestCase
+from addons.app.helpers.test import create_test_app
 
 
-class TestAppCommandAppStart(AbstractTestCase):
-    def test_start(self):
+class TestAppCommandAppRestartPy(AbstractTestCase):
+    def test_restart(self):
         app_dir = create_test_app(self.kernel, services=['php-8'])
 
         self.kernel.exec_function(
+            app__app__restart, {
+                'app-dir': app_dir
+            }
+        )
+
+        self.kernel.exec_function(
             app__app__start, {
+                'app-dir': app_dir
+            }
+        )
+
+        self.kernel.exec_function(
+            app__app__restart, {
                 'app-dir': app_dir
             }
         )
