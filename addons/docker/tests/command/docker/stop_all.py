e818cdadcc9043b5787d034afab5164db63c3820
weeger <romain.weeger@gmail.com>
2023-08-28 09:10:14 +0200

    docker/stop-all

---
+++ addons/docker/tests/command/docker/stop_all.py
@@ -0,0 +1,10 @@
+from addons.docker.command.docker.stop_all import docker__docker__stop_all
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestDockerCommandDockerStopAl(AbstractTestCase):
+    def test_stop_all(self):
+        # We can't execute as it may stop current container to run.
+        self.assertIsNotNone(
+            docker__docker__stop_all
+        )
