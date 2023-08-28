f362f3d0473c30a8d6cdeecb1991d7a605be4ebc
weeger <romain.weeger@gmail.com>
2023-08-28 12:26:14 +0200

    app serve

---
+++ addons/app/tests/command/app/serve.py
@@ -0,0 +1,34 @@
+from addons.app.command.app.serve import app__app__serve
+from tests.AbstractTestCase import AbstractTestCase
+from addons.app.command.app.start import app__app__start
+from addons.app.command.app.stop import app__app__stop
+from addons.app.helpers.test import create_test_app
+
+
+class TestAppCommandAppServePy(AbstractTestCase):
+    def test_serve(self):
+        app_dir = create_test_app(self.kernel, services=['php-8'])
+
+        self.kernel.exec_function(
+            app__app__serve, {
+                'app-dir': app_dir
+            }
+        )
+
+        self.kernel.exec_function(
+            app__app__start, {
+                'app-dir': app_dir
+            }
+        )
+
+        self.kernel.exec_function(
+            app__app__serve, {
+                'app-dir': app_dir
+            }
+        )
+
+        self.kernel.exec_function(
+            app__app__stop, {
+                'app-dir': app_dir
+            }
+        )
