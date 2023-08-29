97f686c9ffe2d0c77e08329e5fb50f4a0bc6a1b2
weeger <romain.weeger@gmail.com>
2023-08-29 17:06:05 +0200

    Quite working patch

--- .wex/command/assistant/AppAssistant.py
+++ .wex/command/assistant/AppAssistant.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from crawler.WexAppCrawler import WexAppCrawler
 import os
 from dotenv import load_dotenv
@@ -13,26 +15,83 @@ from langchain.chains import LLMChain
 
 class AppAssistant:
     def __init__(self):
-        self.root = os.getcwd()
+        self.root = os.getcwd() + '/'
 
         # Update tree info
         self.crawler = WexAppCrawler(self.root, '.wex/ai/data/tree.yml')
 
         # Load .env file to get API token
-        load_dotenv(dotenv_path=self.root + '/.env')
+        load_dotenv(dotenv_path=self.root + '.env')
 
+        self.llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+    def create_chain(self, prompt_template):
+        return LLMChain(
+            llm=self.llm,
+            prompt=ChatPromptTemplate.from_messages(
+                prompt_template
+            ),
+        )
+
+    def assist(self, question: str):
+        human_message_prompt = HumanMessagePromptTemplate.from_template(
+            '{text}'
+        )
+
+        chain = self.create_chain(
+            ChatPromptTemplate.from_messages([
+                human_message_prompt
+            ])
+        )
+
+        print(
+            chain.run(question)
+        )
+
+    def test_patch(self):
+        self.patch('I want this program return the text in uppercase.')
+
+    def patch(self, question: str):
         system_message_prompt = SystemMessagePromptTemplate.from_template(
             """
             You are a world class computer programming assistant.
-            User will pass in question with and code of an application.
+            Below base information about the program named __PROGRAM__.
+            At the end user will pass in question with and code of an application.
+            """
+        )
+
+        git_message_prompt = SystemMessagePromptTemplate.from_template(
+            """
             You should generate a git patch to apply on the code to change the program behaviour.
             ONLY return a git patch without any other text.
             """
         )
 
+        files_structure_message_prompt = HumanMessagePromptTemplate.from_template(
+            """
+            File structure of the program __PROGRAM__ :
+            .git
+              type: folder
+            .wex
+              type: folder
+              children:
+                command:
+                  type:folder
+                  children:
+                    samples:
+                    children:
+                      main.py:
+                        type: file
+                        description: The main entrypoint of the program.
+            """
+        )
+
         code_message_prompt = HumanMessagePromptTemplate.from_template(
             """
-            print('Hello World!')
+            Full code of the program __PROGRAM__,
+            after this line there is NO extra empty line at the beginning of the file :
+            def my_program_main_function():
+                return 'Hello World!'
             """
         )
 
@@ -40,18 +99,28 @@ class AppAssistant:
             '{text}'
         )
 
-        chat_prompt = ChatPromptTemplate.from_messages([
-            system_message_prompt,
-            code_message_prompt,
-            human_message_prompt
-        ])
+        chain = self.create_chain(
+            ChatPromptTemplate.from_messages([
+                system_message_prompt,
+                git_message_prompt,
+                files_structure_message_prompt,
+                code_message_prompt,
+                human_message_prompt
+            ]))
 
-        chain = LLMChain(
-            llm=ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY")),
-            prompt=chat_prompt,
-        )
+        # TODO : validate patch structure
 
-        print(
-            chain.run('I want this code this code to format text in uppercase.')
-        )
+        patch_content = chain.run(question)
+        patch_path = self.root + 'autocode.patch'
 
+        # Save the patch content to a file
+        with open(patch_path, 'w') as file:
+            file.write(patch_content + '\n')
+
+        # Apply patch
+        subprocess.run(['git', 'apply', '--ignore-whitespace', patch_path], cwd=self.root)
+
+        # Delete patch
+        # os.remove(patch_path)
+
+        return patch_content
--- .wex/command/autocode.py
+++ .wex/command/autocode.py
@@ -5,3 +5,13 @@ from assistant.AppAssistant import AppAssistant
 
 assistant = AppAssistant()
 
+print(
+    assistant.patch('I want this program return the text in uppercase.')
+)
+
+# assistant.assist(
+#     """
+#     Ask here when OpenAI website is Down...
+#     """
+# )
+
