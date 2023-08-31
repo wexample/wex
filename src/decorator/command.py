677c06537a274cf60486eca8e7f89814c3c958bf
weeger <romain.weeger@gmail.com>
2023-08-31 10:25:02 +0200

    Syntax

--- src/decorator/command.py
+++ src/decorator/command.py
@@ -5,6 +5,8 @@ import click
 def command(*args, **kwargs):
     def decorator(f):
         if callable(f):
+            # Apply the click.pass_obj decorator
+            f = click.pass_obj(f)
             # Apply the original click.command decorator
             f = click.command(*args, **kwargs)(f)
         return f
