b4d2d35b51aba35bf029c597ad7a43d44ff47b8f
weeger <romain.weeger@gmail.com>
2023-08-31 10:13:37 +0200

    service/used

--- addons/app/command/service/used.py
+++ addons/app/command/service/used.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
 import click
 
-from addons.app.command.config.get import app__config__get
 from addons.app.decorator.app_dir_option import app_dir_option
+from addons.app.AppAddonManager import AppAddonManager
 
 
 @click.command()
@@ -10,10 +12,6 @@ from addons.app.decorator.app_dir_option import app_dir_option
 @click.option('--service', '-s', type=str, required=True,
               help="Service name to check in app configuration")
 def app__service__used(kernel, service, app_dir):
-    return service in kernel.exec_function(
-        app__config__get,
-        {
-            'key': 'global.services',
-            'app-dir': app_dir
-        }
-    )
+    manager: 'AppAddonManager' = kernel.addons['app']
+
+    return manager.get_config('global.services')
