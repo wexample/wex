8feba03d21ef7e1361b271051895b17c6ddcfe63
weeger <romain.weeger@gmail.com>
2023-09-03 17:26:41 +0200

    Fix tests

--- addons/system/tests/command/os/name.py
+++ addons/system/tests/command/os/name.py
@@ -1,12 +1,10 @@
+import re
 from addons.system.command.os.name import system__os__name
 from tests.AbstractTestCase import AbstractTestCase
 
 
 class TestSystemCommandOsName(AbstractTestCase):
     def test_name(self):
-        self.assertEqual(
-            self.kernel.run_function(system__os__name, {
-                'name': 'test'
-            }),
-            'linux'
-        )
+        os_name = self.kernel.run_function(system__os__name)
+
+        self.assertTrue(re.match('^[a-z]+$', os_name))
