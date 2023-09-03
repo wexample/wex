ef76abddf99a040697c2ffcc06c3498ace1e040d
weeger <romain.weeger@gmail.com>
2023-09-03 17:23:57 +0200

    Fix tests

--- addons/system/tests/command/disk/spaces.py
+++ addons/system/tests/command/disk/spaces.py
@@ -4,8 +4,8 @@ from tests.AbstractTestCase import AbstractTestCase
 
 class TestSystemCommandDiskSpaces(AbstractTestCase):
     def test_spaces(self):
-        # TODO
-        # self.kernel.run_function(system__disk__spaces, {
-        #     'name': 'test'
-        # })
-        self.assertTrue(False)
+        text = self.kernel.run_function(system__disk__spaces)
+
+        self.assertIsNotNone(
+            text
+        )
