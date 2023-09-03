254382e08330d01c0230037e13cdb8a06daa151e
weeger <romain.weeger@gmail.com>
2023-09-03 17:21:28 +0200

    Fix tests

--- addons/core/tests/command/webhook/serve.py
+++ addons/core/tests/command/webhook/serve.py
@@ -5,7 +5,8 @@ from tests.AbstractTestCase import AbstractTestCase
 class TestCoreCommandWebhookServe(AbstractTestCase):
     def test_serve(self):
         self.kernel.run_function(core__webhook__serve, {
-            'dry-run': True
+            'dry-run': True,
+            'port': 6543
         })
 
         # No easy way to define if server ran.
