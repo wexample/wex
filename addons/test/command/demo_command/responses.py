797c96344cbee098818943440dac03de95f11ae7
weeger <romain.weeger@gmail.com>
2023-09-04 12:03:03 +0200

    Test responses

--- addons/test/command/demo_command/responses.py
+++ addons/test/command/demo_command/responses.py
@@ -1,11 +1,23 @@
 from src.core.response.ShellCommandResponse import ShellCommandResponse
 from src.core.response.ResponseCollectionResponse import ResponseCollectionResponse
 from src.decorator.test_command import test_command
+from src.decorator.option import option
 from src.core import Kernel
+
+RESPONSES_DEFAULT_VALUES = {
+    'string': 'STRING',
+    'integer': 'INTEGER',
+    'boolean': 'BOOLEAN',
+}
 
 
 @test_command()
-def test__demo_command__responses(kernel: Kernel):
+@option('--type', '-y', required=True,
+        help="Response type to test")
+def test__demo_command__responses(kernel: Kernel, type: str):
+    if type in RESPONSES_DEFAULT_VALUES:
+        return RESPONSES_DEFAULT_VALUES[type]
+
     return ResponseCollectionResponse(kernel, [
         # Will be converted to PythonFunctionResponse
         _test__demo_command__responses_one,
--- addons/test/tests/command/demo_command/responses.py
+++ addons/test/tests/command/demo_command/responses.py
@@ -1,14 +1,18 @@
-from addons.test.command.demo_command.responses import test__demo_command__responses
+from addons.test.command.demo_command.responses import test__demo_command__responses, RESPONSES_DEFAULT_VALUES
 from tests.AbstractTestCase import AbstractTestCase
 
 
 class TestTestCommandDemoCommandResponses(AbstractTestCase):
     def test_responses(self):
-        response = self.kernel.run_function(
-            test__demo_command__responses
-        )
+        for type in RESPONSES_DEFAULT_VALUES:
+            response = self.kernel.run_function(
+                test__demo_command__responses,
+                {
+                    'type': type
+                }
+            )
 
-        self.assertEqual(
-            response,
-            'COLLECTION'
-        )
+            self.assertEqual(
+                response,
+                RESPONSES_DEFAULT_VALUES[type]
+            )
