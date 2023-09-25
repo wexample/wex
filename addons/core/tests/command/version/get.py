456237d91dafea0fec54c2758e0306ea9c0985b0
weeger <romain.weeger@gmail.com>
2023-09-25 17:42:17 +0200

    version/get

---
+++ addons/core/tests/command/version/get.py
@@ -0,0 +1,11 @@
+from addons.core.command.version.get import core__version__get
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestCoreCommandVersionGet(AbstractTestCase):
+    def test_get(self):
+        self.assertIsNotNone(
+            self.kernel.run_function(
+                core__version__get
+            )
+        )
