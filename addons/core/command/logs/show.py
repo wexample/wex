508a3845a308fb6d7366ca7d71e68f487e09108a
weeger <romain.weeger@gmail.com>
2023-09-25 15:54:49 +0200

    show logs

---
+++ addons/core/command/logs/show.py
@@ -0,0 +1,29 @@
+from src.decorator.alias import alias
+from src.decorator.command import command
+from src.core import Kernel
+import os
+import json
+
+
+@command(help="Description")
+@alias(name='logs')
+def core__logs__show(kernel: Kernel, max: int = 10) -> str:
+    output = []
+    directory = kernel.path['log']
+
+    all_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    json_files = [f for f in all_files if f.endswith('.json')]
+    sorted_files = sorted(json_files)
+    last_files = sorted_files[-max:]
+
+    for filename in last_files:
+        with open(os.path.join(directory, filename), 'r') as file:
+            data = json.load(file)
+
+            for command_data in data['commands']:
+                output.append(' | '.join([
+                    data['dateStart'],
+                    command_data['command']
+                ]))
+
+    return "\n".join(output)
---
+++ addons/core/tests/command/logs/show.py
@@ -0,0 +1,12 @@
+from addons.core.command.logs.show import core__logs__show
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestCoreCommandLogsShow(AbstractTestCase):
+    def test_show(self):
+        output = self.kernel.run_function(core__logs__show)
+
+        self.assertTrue(
+            len(output.splitlines()) > 0,
+            "Output is empty or doesn't contain any lines."
+        )
