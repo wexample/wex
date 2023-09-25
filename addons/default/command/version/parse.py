aeafb5be62b116585cf290760793c020a2e17b44
weeger <romain.weeger@gmail.com>
2023-09-25 18:42:05 +0200

    Testing parse

--- addons/default/command/version/parse.py
+++ addons/default/command/version/parse.py
@@ -1,3 +1,5 @@
+import re
+
 from src.decorator.command import command
 from src.decorator.option import option
 from src.core import Kernel
@@ -24,17 +26,20 @@ def default__version__parse(kernel: Kernel, version: str) -> Dict[str, Optional[
             # pre_build_number can be : 1+build.1234
             if '+' in pre_build_number:
                 pre_build_number, build_metadata = pre_build_number.split('+')
-            pre_build_number = int(pre_build_number)
-    else:
-        base_version, pre_build = version, ''
 
-    major, intermediate, minor = base_version.split('.')
+            pre_build_number = int(pre_build_number) if pre_build_number else None
+
+    match = re.match(r'(\d+)?\.?(\d+)?\.?(\d+)?([-.+].*)?', version)
+    major = intermediate = minor = None
+
+    if match:
+        major, intermediate, minor, _ = match.groups()
 
     # Create a dictionary to store the elements
     version_dict = {
-        'major': major,
-        'intermediate': intermediate,
-        'minor': minor,
+        'major': int(major) if major else None,
+        'intermediate': int(intermediate) if intermediate else None,
+        'minor': int(minor) if minor else None,
         'pre_build_type': pre_build_type,
         'pre_build_number': pre_build_number,
     }
