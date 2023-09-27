586bb24f6d5197693097920b0eade0229a5263b3
weeger <romain.weeger@gmail.com>
2023-09-27 17:14:59 +0200

    Code style

--- src/core/response/ResponseCollectionResponse.py
+++ src/core/response/ResponseCollectionResponse.py
@@ -8,20 +8,18 @@ from src.core.response.AbstractResponse import AbstractResponse
 class ResponseCollectionResponse(AbstractResponse):
     def __init__(self, kernel, collection: list):
         super().__init__(kernel)
-
         self.collection = collection
 
     def render(self, render_mode: str = KERNEL_RENDER_MODE_CLI, args={}) -> str | int | bool | None:
         request = self.kernel.current_request
 
         # Convert step to a list of integers
-        step_split = list(map(int, request.step.split('.'))) if request.step is not None else [None]
+        step_split = list(map(int, request.step.split('.'))) if request.step else [None]
 
         return self._render(step_split, 0)
 
     def _render(self, step_list, step_position: int | None, output_bag: list = None):
         output_bag = output_bag if output_bag is not None else []
-
         request = self.kernel.current_request
         current_step = step_list[step_position]
 
@@ -31,50 +29,27 @@ class ResponseCollectionResponse(AbstractResponse):
             return None if self.kernel.allow_post_exec else output_bag
 
         # Wrap responses
-        collection = []
-        for item in self.collection:
-            collection.append(request.resolver.wrap_response(item))
+        collection = [request.resolver.wrap_response(item) for item in self.collection]
 
         render_args = {}
         if current_step > 0:
-            render_args = {
-                'previous': parse_arg(
-                    self.kernel.task_file_load(
-                        'response',
-                    )
-                )
-            }
+            render_args = {'previous': parse_arg(self.kernel.task_file_load('response'))}
+            remove_file_if_exists(self.kernel.task_file_path('response'))
 
-            remove_file_if_exists(
-                self.kernel.task_file_path('response')
-            )
-
-        output = collection[current_step].render(
-            args=render_args
-        )
-
+        output = collection[current_step].render(args=render_args)
         output_bag.append(output)
 
-        # If the output is a sub-collection, append a new level to step
+        # Handle nested collection response
         if isinstance(output, ResponseCollectionResponse):
             step_position += 1
-
-            # Set value at step_position, extending the list with None values if necessary
-            step_list.extend([None] * (step_position - len(step_list) + 1)) if step_position >= len(step_list) else None
-
-            sub_render = output._render(step_list, step_position, output_bag)
-
-            return sub_render if self.kernel.allow_post_exec else output_bag
+            step_list += [None] * (step_position + 1 - len(step_list))
+            return output._render(step_list, step_position, output_bag) if self.kernel.allow_post_exec else output_bag
 
         step_next = current_step + 1
 
         if step_next < len(collection):
             # Store response in a file to allow next step to access it.
-            self.kernel.task_file_write(
-                'response',
-                str(output)
-            )
-
+            self.kernel.task_file_write('response', str(output))
             self.enqueue_next_step(step_list, step_position, step_next, output_bag)
 
         return output if self.kernel.allow_post_exec else output_bag
@@ -83,11 +58,9 @@ class ResponseCollectionResponse(AbstractResponse):
         step_list[step_position] = step_next
 
         if self.kernel.allow_post_exec:
-            request = self.kernel.current_request
-            args_dict = request.args_dict.copy()
-
+            args_dict = self.kernel.current_request.args_dict.copy()
             args_dict['command-request-step'] = '.'.join(map(str, step_list))
-            process_post_exec_wex(self.kernel, request.function, args_dict)
+            process_post_exec_wex(self.kernel, self.kernel.current_request.function, args_dict)
         else:
             # Run now.
             self._render(step_list, step_position, output_bag)
