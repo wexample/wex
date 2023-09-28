8797645a509466773f42416c2c124226f8533cb5
weeger <romain.weeger@gmail.com>
2023-09-28 09:11:46 +0200

    Parent

--- src/core/response/AbstractResponse.py
+++ src/core/response/AbstractResponse.py
@@ -7,6 +7,12 @@ class AbstractResponse:
         from src.core.Kernel import Kernel
         self.request = kernel.current_request
         self.kernel: Kernel = kernel
+        self.parent = None
+
+    def get_root_parent(self):
+        if self.parent:
+            return self.parent.get_root_parent()
+        return self
 
     @abstractmethod
     def render(self, render_mode: str = KERNEL_RENDER_MODE_CLI, args: dict = None) -> str | int | bool | None:
