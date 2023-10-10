c7e4dd92d3b4a25a712b01c849972c3bf78427ad
weeger <romain.weeger@gmail.com>
2023-10-10 10:22:41 +0200

    app go test pass

---
+++ addons/app/tests/command/app/go.py
@@ -0,0 +1,9 @@
+from addons.app.tests.AbstractAppTestCase import AbstractAppTestCase
+
+
+class TestAppCommandAppGo(AbstractAppTestCase):
+
+    def test_go(self):
+        app_dir = self.create_and_start_text_app(services=['php_8'])
+
+        # We don't test app__app__go as it is an interactive command.
