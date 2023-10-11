e54491f197a16abe6276aa2cc870aa00f28537ad
weeger <romain.weeger@gmail.com>
2023-10-11 16:04:57 +0200

    own/this

--- addons/system/command/own/this.py
+++ addons/system/command/own/this.py
@@ -3,9 +3,11 @@ import os
 from src.helper.system import set_owner_recursively, get_user_or_sudo_user
 from src.decorator.command import command
 from src.decorator.option import option
+from src.decorator.alias_without_addon import alias_without_addon
 
 
 @command(help="Make current user owner of this directory and every files or subdirectories")
+@alias_without_addon()
 @option('--path', '-p', type=str, required=False, default=None, help="Argument")
 def system__own__this(kernel, path: str = None):
     if path is None:
