e69c32a3a3a6eb80138fa7a9832920523ae936a7
weeger <romain.weeger@gmail.com>
2023-10-17 08:03:37 +0200

    sql server ready

---
+++ addons/services_db/services/sqlserver/command/service/ready.py
@@ -0,0 +1,37 @@
+from src.core.Kernel import Kernel
+from addons.app.decorator.app_dir_option import app_dir_option
+from addons.app.decorator.service_option import service_option
+from src.decorator.command import command
+from addons.services_db.services.sqlserver.command.db.exec import sqlserver__db__exec
+from src.const.globals import COMMAND_TYPE_SERVICE
+from addons.app.command.app.exec import app__app__exec
+
+
+@command(help="Return true if database runs")
+@app_dir_option()
+@service_option()
+def sqlserver__service__ready(kernel: Kernel, app_dir: str, service: str):
+    exec_command = kernel.run_function(
+        sqlserver__db__exec, {
+            'app-dir': app_dir,
+            'service': service,
+            'command': 'SELECT 1'
+        }, COMMAND_TYPE_SERVICE
+    ).print()
+
+    response = kernel.run_function(
+        app__app__exec,
+        {
+            'app-dir': app_dir,
+            'container-name': service,
+            # Ask to execute bash
+            'command': exec_command,
+            'sync': True
+        }
+    )
+
+    first = response.first()
+    if isinstance(first, list) and first[0] == '1':
+        return True
+
+    return False
