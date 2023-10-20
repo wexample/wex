12296f2275bc9d18b67ef4b7874044560ad5511f
weeger <romain.weeger@gmail.com>
2023-10-20 11:20:24 +0200

    Simplify tests

--- addons/app/tests/command/app/go.py
+++ addons/app/tests/command/app/go.py
@@ -4,8 +4,5 @@ from addons.app.tests.AbstractAppTestCase import AbstractAppTestCase
 class TestAppCommandAppGo(AbstractAppTestCase):
 
     def test_go(self):
-        app_dir = self.create_and_start_test_app(services=['php'])
-
         # We don't test app__app__go as it is an interactive command.
-
-        self.stop_test_app()
+        self.assertTrue(True)
--- addons/core/tests/command/service/install.py
+++ addons/core/tests/command/service/install.py
@@ -3,6 +3,5 @@ from addons.app.tests.AbstractAppTestCase import AbstractAppTestCase
 
 class TestCoreCommandServiceInstall(AbstractAppTestCase):
     def test_install(self):
-        self.create_test_app()
-
-        self.stop_test_app()
+        # Creating app is intensively used in all tests.
+        self.assertTrue(True)
