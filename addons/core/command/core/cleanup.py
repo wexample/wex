9f6bf9042c04a2818916064147baf49b3c2cd526
weeger <romain.weeger@gmail.com>
2023-10-22 20:13:10 +0200

    Cleanup

--- addons/core/command/core/cleanup.py
+++ addons/core/command/core/cleanup.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 
 from addons.core.command.logs.rotate import core__logs__rotate
 from src.decorator.alias import alias
@@ -8,12 +10,9 @@ from src.decorator.command import command
 @command(help="Uninstall core")
 @alias('cleanup')
 def core__core__cleanup(kernel: Kernel):
-    kernel.run_function(
-        core__logs__rotate,
-        {
-            'max-days': False,
-            'max-count': False
-        }
-    )
+    shutil.rmtree(kernel.path['tmp'])
+
+    os.makedirs(os.path.dirname(kernel.path['tmp']), exist_ok=True)
+    gitkeep_path = os.path.join(kernel.path['tmp'], '.gitkeep')
 
     kernel.rebuild()
