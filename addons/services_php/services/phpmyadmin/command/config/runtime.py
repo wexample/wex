c16afa195639607154af7d60687b20c370c2dafa
weeger <romain.weeger@gmail.com>
2023-10-26 13:07:02 +0200

    Allow disabling pma

--- addons/services_php/services/phpmyadmin/command/config/runtime.py
+++ addons/services_php/services/phpmyadmin/command/config/runtime.py
@@ -9,34 +9,37 @@ from src.const.globals import COMMAND_TYPE_SERVICE
 def phpmyadmin__config__runtime(kernel: Kernel, app_dir: str, service: str):
     # Save config
     manager: AppAddonManager = kernel.addons['app']
-    domain_pma = manager.get_runtime_config('domain_pma')
+    domain_pma = manager.get_runtime_config('domain_pma', None)
 
-    if not domain_pma:
-        env = manager.get_runtime_config('env')
+    # Setting false to domain will disable domain setting,
+    # used to disable phpmyadmin in some environment.
+    if domain_pma is not False:
+        if domain_pma is not None:
+            env = manager.get_runtime_config('env')
 
-        domain_pma = ['pma']
-        if env != APP_ENV_LOCAL and env != APP_ENV_PROD:
-            domain_pma.append(env)
+            domain_pma = ['pma']
+            if env != APP_ENV_LOCAL and env != APP_ENV_PROD:
+                domain_pma.append(env)
 
-        domain_pma.append(
-            manager.get_runtime_config('domain_tld')
+            domain_pma.append(
+                manager.get_runtime_config('domain_tld')
+            )
+
+            domain_pma = '.'.join(domain_pma)
+
+            manager.set_runtime_config(
+                'domain_pma',
+                domain_pma
+            )
+
+        domains = manager.get_runtime_config(
+            'domains',
+            []
         )
 
-        domain_pma = '.'.join(domain_pma)
+        domains.append(domain_pma)
 
         manager.set_runtime_config(
-            'domain_pma',
-            domain_pma
+            'domains',
+            domains
         )
-
-    domains = manager.get_runtime_config(
-        'domains',
-        []
-    )
-
-    domains.append(domain_pma)
-
-    manager.set_runtime_config(
-        'domains',
-        domains
-    )
