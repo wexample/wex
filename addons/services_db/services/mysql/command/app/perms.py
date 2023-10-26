9d00e7ca1f07b8030701cce2092ecb2aa41cb4ca
weeger <romain.weeger@gmail.com>
2023-10-26 17:20:22 +0200

    No mysql cnf file

--- addons/services_db/services/mysql/command/app/perms.py
+++ addons/services_db/services/mysql/command/app/perms.py
@@ -1,7 +1,4 @@
-import os
-
 from addons.app.AppAddonManager import AppAddonManager
-from addons.services_db.services.mysql.command.config.write_post import mysql_get_my_cnf_path
 from src.core.Kernel import Kernel
 from addons.app.decorator.app_command import app_command
 from src.const.globals import COMMAND_TYPE_SERVICE
@@ -11,10 +8,3 @@ from src.const.globals import COMMAND_TYPE_SERVICE
 def mysql__app__perms(kernel: Kernel, app_dir: str, service: str):
     manager: AppAddonManager = kernel.addons['app']
     manager.log(service + " : setting permissions to my.cnf")
-
-    # Create connexion file info
-    my_conf_path = mysql_get_my_cnf_path(app_dir, service)
-
-    # Setting file permissions
-    os.chmod(my_conf_path, 0o755)
-    os.chmod(my_conf_path, 0o644)
