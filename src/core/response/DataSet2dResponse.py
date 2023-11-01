790fe331225525d319f7d5873c3615d229e8ebc5
weeger <romain.weeger@gmail.com>
2023-11-01 17:23:05 +0100

    Allow several sections and title

--- src/core/response/DataSet2dResponse.py
+++ src/core/response/DataSet2dResponse.py
@@ -1,3 +1,4 @@
+import shutil
 from src.core.CommandRequest import CommandRequest
 from src.core.response.AbstractResponse import AbstractResponse
 from src.const.globals import KERNEL_RENDER_MODE_CLI
@@ -7,20 +8,35 @@ class DataSet2dResponse(AbstractResponse):
     def __init__(self, kernel):
         super().__init__(kernel)
 
-        self.header: list | None = None
-        self.body: list | None = None
+        self.sections: list = []
+        self.current_section = None
+        self.new_section()
+
+    def set_title(self, title):
+        self.current_section['title'] = title
 
     def set_header(self, header):
-        self.header = header
+        self.current_section['header'] = header
 
     def get_header(self):
-        return self.header
+        return self.current_section['header']
 
     def set_body(self, body):
-        self.body = body
+        self.current_section['body'] = body
 
     def get_body(self):
-        return self.body
+        return self.current_section['body']
+
+    def new_section(self):
+        self.current_section = {
+            'title': None,
+            'header': None,
+            'body': None,
+        }
+
+        self.sections.append(
+            self.current_section
+        )
 
     def render_content(
             self,
@@ -29,15 +45,28 @@ class DataSet2dResponse(AbstractResponse):
             args: dict = None) -> AbstractResponse:
         # Function to adjust the render mode with kernel call method info
 
+        for section in self.sections:
+            self.render_content_section(
+                section,
+                render_mode
+            )
+
+        return self
+
+    def render_content_section(
+            self,
+            section,
+            render_mode: str = KERNEL_RENDER_MODE_CLI):
+
+        terminal_size = shutil.get_terminal_size()
+
         if render_mode == KERNEL_RENDER_MODE_CLI:
-            header = self.header if self.header else []  # check for None or empty header
-            array = self.body
+            header = section['header'] if section['header'] else []  # check for None or empty header
+            array = section['body']
+            title = section.get('title', '')
 
             # Initialize max widths with zeros
-            if header:
-                num_columns = len(header)
-            else:
-                num_columns = len(array[0]) if array else 0  # infer the number of columns from the first row of array
+            num_columns = len(header) if header else len(array[0]) if array else 0  # infer the number of columns
             max_widths = [0] * num_columns
 
             # Calculate the maximum widths for each column
@@ -56,7 +85,21 @@ class DataSet2dResponse(AbstractResponse):
             # Generate the horizontal separator line
             separator_line = "+" + "-" * total_line_length + "+\n"
 
-            bash_array = separator_line
+            bash_array = ""
+
+            # Add title if exists, aligned to the left and fill with underscores
+            if title:
+                # Calculate how much padding is needed on each side of the title
+                title_length = len(title)
+                padding_each_side = (total_line_length - title_length) // 2
+
+                # Check if we need an extra '=' at the end (for odd width)
+                extra_equal = "=" if (total_line_length - title_length) % 2 == 1 else ""
+
+                # Construct the title line
+                bash_array += f"{'=' * padding_each_side} {title} {'=' * padding_each_side}{extra_equal}\n"
+
+            bash_array += separator_line
 
             # Add header only if exists
             if header:
@@ -76,4 +119,3 @@ class DataSet2dResponse(AbstractResponse):
             bash_array += separator_line
 
             self.output_bag.append(bash_array)
-        return self
