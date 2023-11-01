33c15b71cee3c6d0431bd0818a2ec929a931fee8
weeger <romain.weeger@gmail.com>
2023-11-01 23:40:13 +0100

    Exec fix

--- addons/app/command/webhook/exec.py
+++ addons/app/command/webhook/exec.py
@@ -7,12 +7,15 @@ from src.core.Kernel import Kernel
 from src.decorator.command import command
 from src.decorator.option import option
 from addons.app.AppAddonManager import AppAddonManager
+from src.core.response.ResponseCollectionResponse import ResponseCollectionResponse
+from src.core.response.ResponseCollectionStopResponse import ResponseCollectionStopResponse
+from src.core.response.ResponseCollectionHiddenResponse import ResponseCollectionHiddenResponse
 
 
 @command(help="Execute a webhook")
 @option('--url', '-u', type=str, required=True, help="Argument")
 @option('--env', '-e', type=str, required=False, help="Env directory")
-def app__webhook__exec(kernel: Kernel, url: str, env: None | str = None) -> bool:
+def app__webhook__exec(kernel: Kernel, url: str, env: None | str = None):
     source_data = {}
     parsed_url = urlparse(url)
     path = parsed_url.path
@@ -23,6 +26,8 @@ def app__webhook__exec(kernel: Kernel, url: str, env: None | str = None) -> bool
 
     if match:
         app_name, webhook = match.groups()
+
+    def _check():
         query_string = parsed_url.query.replace('+', '%2B')
         query_string_data = parse_qs(query_string)
         has_error = False
@@ -50,29 +55,38 @@ def app__webhook__exec(kernel: Kernel, url: str, env: None | str = None) -> bool
 
             # App exists somewhere.
             if app_name in apps:
-                manager.set_app_workdir(apps[app_name])
-                source_data['app_dir'] = manager.app_dir
+                return ResponseCollectionHiddenResponse(kernel, apps[app_name])
 
-                response_collection = kernel.run_function(
-                    app__script__exec,
-                    {
-                        'name': webhook,
-                        'app-dir': manager.app_dir,
-                    }
-                ).first()
+        kernel.logger.append_event({
+            'url': url,
+            'source_data': source_data,
+            'success': False
+        })
 
-                kernel.logger.append_event({
-                    'url': url,
-                    'source_data': source_data,
-                    'success': response_collection is not None
-                })
+        return ResponseCollectionStopResponse(kernel)
 
-                return True
+    def _execute(previous: str):
+        manager = AppAddonManager(kernel)
+        manager.set_app_workdir(previous)
+        source_data['app_dir'] = manager.app_dir
 
-    kernel.logger.append_event({
-        'url': url,
-        'source_data': source_data,
-        'success': False
-    })
+        response = kernel.run_function(
+            app__script__exec,
+            {
+                'name': webhook,
+                'app-dir': manager.app_dir,
+            }
+        ).first()
 
-    return False
+        kernel.logger.append_event({
+            'url': url,
+            'source_data': source_data,
+            'success': True
+        })
+
+        return response
+
+    return ResponseCollectionResponse(kernel, [
+        _check,
+        _execute
+    ])
