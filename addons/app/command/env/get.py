63467286bd14495db94865e283655fefc7d22891
weeger <romain.weeger@gmail.com>
2023-11-02 09:21:55 +0100

    Minor

--- addons/app/command/env/get.py
+++ addons/app/command/env/get.py
@@ -10,6 +10,4 @@ from addons.app.decorator.app_command import app_command
 @option('--key', '-k', type=str, required=False, default='APP_ENV',
         help="Key in env file")
 def app__env__get(kernel: Kernel, app_dir: str, key: str = 'APP_ENV') -> str:
-    config = dotenv_values(app_dir + APP_FILEPATH_REL_ENV)
-
-    return config.get(key)
+    return dotenv_values(app_dir + APP_FILEPATH_REL_ENV).get(key)
