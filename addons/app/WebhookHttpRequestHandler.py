dcb31d1d48a5162cc91618c2bc99f344f4330831
weeger <romain.weeger@gmail.com>
2023-11-02 17:22:50 +0100

    Improve handler

--- addons/app/WebhookHttpRequestHandler.py
+++ addons/app/WebhookHttpRequestHandler.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler
 from addons.app.command.webhook.exec import app__webhook__exec
 from src.const.globals import KERNEL_RENDER_MODE_HTTP
@@ -7,7 +8,12 @@ class WebhookHttpRequestHandler(BaseHTTPRequestHandler):
     kernel = None
 
     def do_GET(self):
+        self.send_header('Content-type', 'application/json')
+
         try:
+            self.send_response(200)
+            self.end_headers()
+
             success = self.kernel.run_function(
                 app__webhook__exec,
                 {
@@ -16,18 +22,21 @@ class WebhookHttpRequestHandler(BaseHTTPRequestHandler):
                 render_mode=KERNEL_RENDER_MODE_HTTP
             ).first()
 
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-
             if success:
                 self.wfile.write(success.encode())
             else:
-                self.wfile.write(b'{"error":true}')
+                self.wfile.write(b'{"error":"empty"}')
 
         except Exception as e:
-            self.send_response(500)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
+            import traceback
 
-            self.wfile.write("ERROR : {}".format(e).encode())
+            self.kernel.logger.append_event({
+                "error": 'Error during server execution: ' + str(e),
+                'trace': traceback.format_exc()
+            })
+
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(b'{"error":"Error during server execution: ' + str(e) + '"}')
+
+            raise
