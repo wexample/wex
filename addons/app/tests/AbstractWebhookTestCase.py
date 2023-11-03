219a0f678b20b72f02c432374f47ef5ba2369d19
weeger <romain.weeger@gmail.com>
2023-11-03 13:35:48 +0100

    New test type

---
+++ addons/app/tests/AbstractWebhookTestCase.py
@@ -0,0 +1,31 @@
+import time
+
+from addons.app.tests.AbstractAppTestCase import AbstractAppTestCase
+from addons.app.command.webhook.listen import app__webhook__listen
+from http.client import HTTPConnection
+
+
+class AbstractWebhookTestCase(AbstractAppTestCase):
+    def start_webhook_listener(self, port: int = 6543):
+        self.kernel.run_function(app__webhook__listen, {
+            'port': port,
+            'asynchronous': True,
+            'force': True
+        })
+
+        time.sleep(2)
+
+        return port
+
+    def request_listener(self, path: str, port: int = 6543, check_code: None | int = 200):
+        conn = HTTPConnection(f'localhost:{port}')
+        conn.request("GET", path)
+        response = conn.getresponse()
+
+        if check_code:
+            self.assertEqual(
+                response.status,
+                200
+            )
+
+        return response
