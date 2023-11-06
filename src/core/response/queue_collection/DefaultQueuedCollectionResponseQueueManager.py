767647bee8f59bf3845fc5e15f84ec811ccc8476
weeger <romain.weeger@gmail.com>
2023-11-06 17:02:41 +0100

    Do not remove stored data until end of collection

--- src/core/response/queue_collection/DefaultQueuedCollectionResponseQueueManager.py
+++ src/core/response/queue_collection/DefaultQueuedCollectionResponseQueueManager.py
@@ -12,14 +12,25 @@ class DefaultQueuedCollectionResponseQueueManager(AbstractQueuedCollectionRespon
     def __init__(self, response):
         super().__init__(response)
 
-    def get_previous_value(self):
+    def get_previous_storage_path(self) -> str | None:
         path = self.get_previous_response_path()
 
         if path is None:
             return None
 
-        path = '-'.join(map(str, path))
-        previous_data = self.response.kernel.task_file_load(path + '.response')
+        return self.build_storage_path(path)
+
+    def get_previous_value(self):
+        storage_path = self.get_previous_storage_path()
+
+        if not storage_path:
+            return None
+
+        previous_data = self.response.kernel.task_file_load(
+            storage_path,
+            delete_after_read=False
+        )
+
         return yaml.safe_load(previous_data)['body'] if previous_data else None
 
     def enqueue_next_step_by_index(self, next_step_index):
@@ -35,24 +46,32 @@ class DefaultQueuedCollectionResponseQueueManager(AbstractQueuedCollectionRespon
             args
         )
 
+    def build_storage_path(self, path: list) -> str:
+        return '-'.join(map(str, path)) + '.response'
+
     def enqueue_next_step_if_exists(self, step_index, response) -> bool:
         # Array is modified in super call
         steps_current = list(self.response.path_manager.steps)
         exists = super().enqueue_next_step_if_exists(step_index, response)
+        storage_path = steps_current[:self.response.step_position + 1]
 
         if exists:
             store_data = response.store_data()
             if store_data is not None:
-                path = steps_current[:self.response.step_position + 1]
-                path = '-'.join(map(str, path))
-
                 # Store response in a file to allow next step to access it.
                 self.response.kernel.task_file_write(
-                    path + '.response',
+                    self.build_storage_path(storage_path),
                     yaml.dump({
                         # Save raw value to keep type
                         'body': store_data
                     })
+                )
+        else:
+            storage_path = self.get_previous_storage_path()
+
+            if storage_path:
+                self.response.kernel.task_file_load(
+                    storage_path
                 )
 
         return exists
