e1b574aeaee719fc53582ae687f8c0b462463592
weeger <romain.weeger@gmail.com>
2023-11-07 08:32:00 +0100

    Webhook tests

--- addons/app/tests/command/webhook/exec.py
+++ addons/app/tests/command/webhook/exec.py
@@ -1,49 +1,38 @@
-import os
 
 from addons.app.tests.AbstractWebhookTestCase import AbstractWebhookTestCase
-from tests.AbstractTestCase import AbstractTestCase
-from addons.app.command.webhook.exec import app__webhook__exec
+from src.const.globals import CORE_COMMAND_NAME
+from addons.app.AppAddonManager import AppAddonManager
 
 
 class TestAppCommandWebhookExec(AbstractWebhookTestCase):
     def test_exec(self):
-        test_webhook_app_path = '/var/www/test/wex-test-webhook'
-        if not os.path.islink(test_webhook_app_path):
-            os.makedirs(
-                os.path.dirname(test_webhook_app_path),
-                exist_ok=True
-            )
-            os.symlink(
-                self.kernel.get_path('root'),
-                test_webhook_app_path
-            )
+        manager = AppAddonManager(
+            self.kernel,
+            app_dir=self.kernel.path['root']
+        )
 
-        success = self.kernel.run_function(
-            app__webhook__exec,
-            {
-                'url': 'http://localhost:4242/webhook/wex-test-webhook/test',
-                'env': 'test'
-            }
-        ).first()
+        # Add application as a local app
+        manager.add_proxy_app(
+            CORE_COMMAND_NAME,
+            app_dir=self.kernel.path['root']
+        )
 
-        self.assertTrue(success)
+        self.start_webhook_listener()
 
-        success = self.kernel.run_function(
-            app__webhook__exec,
-            {
-                'url': 'http://localhost:4242/webhook/wex-test-webhook/test?lorem=ipsum',
-                'env': 'test'
-            }
-        ).first()
+        # Missing hook
+        response = self.request_listener(
+            '/webhook/wex/missing-hook',
+        )
 
-        self.assertTrue(success)
+        json = self.parse_response(response)
 
-        success = self.kernel.run_function(
-            app__webhook__exec,
-            {
-                'url': 'http://localhost:4242/webhook/wex-test-webhook/test-wrapped?p=155&v=wex_5.0.0-beta.6+build.20230321054915',
-                'env': 'test'
-            }
-        ).first()
+        # Even missing, returns okay as it is an async response.
+        self.assertTrue(
+            json['status'] == 'started'
+        )
 
-        self.assertTrue(success)
+        response = self.request_listener(
+            f'/status/process/{json["task_id"]}',
+        )
+
+        json = self.parse_response(response)
