b065b034dc85b05020085f6bcb9f2def59b2d17d
weeger <romain.weeger@gmail.com>
2023-11-07 08:40:02 +0100

    Handle fatal errors

--- addons/app/WebhookHttpRequestHandler.py
+++ addons/app/WebhookHttpRequestHandler.py
@@ -1,12 +1,9 @@
 from http.server import BaseHTTPRequestHandler
 from src.helper.routing import is_allowed_route, get_route_info
-from src.helper.array import array_replace_value
-import subprocess
 from logging.handlers import RotatingFileHandler
 import traceback
 import logging
 import json
-from urllib.parse import urlparse
 
 WEBHOOK_COMMAND_URL_PLACEHOLDER = '__URL__'
 
@@ -31,33 +28,40 @@ class WebhookHttpRequestHandler(BaseHTTPRequestHandler):
         super().__init__(*args, **kwargs)
 
     def do_GET(self):
-        error = False
+        try:
+            error = False
 
-        if not is_allowed_route(self.path, self.routes):
-            error = 'NOT_FOUND'
+            if not is_allowed_route(self.path, self.routes):
+                error = 'NOT_FOUND'
 
-        if error:
+            if error:
+                self.send_response(500)
+                output = {
+                    'error': error,
+                }
+            else:
+                self.send_response(200)
+                output = {
+                    'status': 'started'
+                }
+
+            output['task_id'] = self.task_id
+            output['path'] = self.path
+            output['info'] = get_route_info(self.path, self.routes)
+
+        except Exception as e:
+            # Log the exception with traceback
+            self.logger.error("Exception occurred", exc_info=True)
             self.send_response(500)
+
             output = {
-                'error': error,
-            }
-        else:
-            self.send_response(200)
-            output = {
-                'status': 'started'
+                'error': 'WEBHOOK_HANDLER_ERROR',
+                'details': str(e),
+                'traceback': traceback.format_exc()
             }
 
-        output['task_id'] = self.task_id
-        output['path'] = self.path
-        output['info'] = get_route_info(self.path, self.routes)
+        # Serialize the output and send the response
         output = json.dumps(output)
-
-        if error:
-            self.logger.error(output)
-        else:
-            self.logger.info(output)
-
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-
         self.wfile.write(output.encode())
