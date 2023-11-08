3228035bd933f0a704200186445bef9dd7a9e93f
weeger <romain.weeger@gmail.com>
2023-11-08 13:46:34 +0100

    Fix tests

--- addons/test/tests/command/logging/event.py
+++ addons/test/tests/command/logging/event.py
@@ -5,7 +5,19 @@ from tests.AbstractTestCase import AbstractTestCase
 class TestTestCommandLoggingEvent(AbstractTestCase):
     def test_event(self):
         self.kernel.run_function(test__logging__event)
+        has_empty_event = False
+        has_event_with_data = False
+
+        for event in self.kernel.logger.log_data['events']:
+            if event['name'] == 'TEST_EVENT_EMPTY':
+                has_empty_event = True
+            if event['name'] == 'TEST_EVENT_DATA':
+                has_event_with_data = True
 
         self.assertTrue(
-            'events' in self.kernel.logger.log_data['commands'][0]
+            has_empty_event
+        )
+
+        self.assertTrue(
+            has_event_with_data
         )
