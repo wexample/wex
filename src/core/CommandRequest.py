13fb4c77ff1d409ec3b2fdca087b171c38d04f9d
weeger <romain.weeger@gmail.com>
2023-11-09 09:56:17 +0100

    Init

--- src/core/CommandRequest.py
+++ src/core/CommandRequest.py
@@ -1,26 +1,30 @@
-from src.helper.args import arg_shift, convert_dict_to_args
+import os
+
+from src.const.globals import COMMAND_EXTENSION_PYTHON
+from src.helper.args import convert_dict_to_args
 
 
 class CommandRequest:
     function = None
     localized = None
     match = None
-    path = None
 
     def __init__(self, resolver, command: str, args: dict | list = None):
         args = args or []
 
-        self.extension: str
+        self.extension: None | str = None
         self.quiet = False
         self.resolver = resolver
         self.command = resolver.resolve_alias(command)
         self.type = resolver.get_type()
         self.storage = {}  # Useful to store data about the current command execution
         self.args = []
-        self.resolver.locate_function(self)
         self.parent = self.resolver.kernel.current_request
+        self.path: None | str = None
 
-        if not self.function:
+        self.resolver.locate_function(self)
+
+        if not self.path:
             # Do not return any error if function is missing,
             # as it is managed outside.
             return
