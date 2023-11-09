871dfd04bdf53703524d969d80205e8e9bd670ed
weeger <romain.weeger@gmail.com>
2023-11-09 10:17:19 +0100

    Refactor

--- src/const/resolver.py
+++ src/const/resolver.py
@@ -1,7 +1,7 @@
-from src.core.command_resolver.AddonCommandResolver import AddonCommandResolver
-from src.core.command_resolver.AppCommandResolver import AppCommandResolver
-from src.core.command_resolver.ServiceCommandResolver import ServiceCommandResolver
-from src.core.command_resolver.UserCommandResolver import UserCommandResolver
+from src.core.command.resolver.AddonCommandResolver import AddonCommandResolver
+from src.core.command.resolver.AppCommandResolver import AppCommandResolver
+from src.core.command.resolver.ServiceCommandResolver import ServiceCommandResolver
+from src.core.command.resolver.UserCommandResolver import UserCommandResolver
 
 COMMAND_RESOLVERS_CLASSES = [
     AddonCommandResolver,
