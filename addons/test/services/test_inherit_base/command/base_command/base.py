23bc1fa536b5b8c13bc20f9b5e05b65d97913464
weeger <romain.weeger@gmail.com>
2023-11-10 10:26:29 +0100

    Service test files

---
+++ addons/test/services/test_inherit_base/command/base_command/base.py
@@ -0,0 +1,11 @@
+from src.core.Kernel import Kernel
+from src.decorator.test_command import test_command
+from src.decorator.option import option
+from src.const.globals import COMMAND_TYPE_SERVICE
+
+
+@test_command(command_type=COMMAND_TYPE_SERVICE)
+@option('--option', '-o', type=str, required=False,
+        help="An option as flag")
+def test_inherit_base__base_command__base(kernel: Kernel, service: str, option: str = None):
+    return f'BASE:{option}:{service}'
