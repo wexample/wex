3baaa5ebc832937ed6df17b2a64310a4f3581b8e
weeger <romain.weeger@gmail.com>
2023-11-10 17:07:47 +0100

    Fix app restart

--- addons/app/command/app/restart.py
+++ addons/app/command/app/restart.py
@@ -2,20 +2,28 @@ from addons.app.command.app.start import app__app__start
 from addons.app.command.app.stop import app__app__stop
 from src.core.Kernel import Kernel
 from addons.app.decorator.app_command import app_command
+from src.core.response.QueuedCollectionResponse import QueuedCollectionResponse
 
 
 @app_command(help="Restarts app")
 def app__app__restart(kernel: Kernel, app_dir: str):
-    kernel.run_function(
-        app__app__stop,
-        {
-            'app-dir': app_dir
-        }
-    )
+    def _app__app__restart__stop(previous=None):
+        kernel.run_function(
+            app__app__stop,
+            {
+                'app-dir': app_dir
+            }
+        )
 
-    kernel.run_function(
-        app__app__start,
-        {
-            'app-dir': app_dir
-        }
-    )
+    def _app__app__restart__start(previous=None):
+        kernel.run_function(
+            app__app__start,
+            {
+                'app-dir': app_dir
+            }
+        )
+
+    return QueuedCollectionResponse(kernel, [
+        _app__app__restart__stop,
+        _app__app__restart__start,
+    ])
--- src/core/response/queue_collection/QueuedCollectionPathManager.py
+++ src/core/response/queue_collection/QueuedCollectionPathManager.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+from src.const.globals import VERBOSITY_LEVEL_MAXIMUM
 from src.helper.args import args_shift_one
 
 
@@ -27,9 +29,15 @@ class QueuedCollectionPathManager:
             self.response.step_position = self.response.find_parent_response_collection().step_position + 1
 
     def save_to_map(self):
-        self.response.kernel.io.log(f'Step path: {self.build_step_path()}')
-        self.response.kernel.io.log(f'Step position: {self.response.step_position}')
-        self.response.kernel.io.log(f'Step index: {self.get_step_index()}')
+        self.response.kernel.io.log(
+            f'Step path: {self.build_step_path()}',
+            verbosity=VERBOSITY_LEVEL_MAXIMUM)
+        self.response.kernel.io.log(
+            f'Step position: {self.response.step_position}',
+            verbosity=VERBOSITY_LEVEL_MAXIMUM)
+        self.response.kernel.io.log(
+            f'Step index: {self.get_step_index()}',
+            verbosity=VERBOSITY_LEVEL_MAXIMUM)
 
         self.map[self.build_step_path()] = self.response
 
