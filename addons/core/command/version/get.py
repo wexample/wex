9ed984f7091d4164518f2516350458ee7b249758
weeger <romain.weeger@gmail.com>
2023-11-15 09:06:59 +0100

    Version number

--- addons/core/command/version/get.py
+++ addons/core/command/version/get.py
@@ -1,8 +1,14 @@
 from src.helper.core import core_kernel_get_version
 from src.decorator.command import command
 from src.core import Kernel
+from src.decorator.alias import alias
 
 
+@alias('-v')
+@alias('-version')
+@alias('--v')
+@alias('--version')
+@alias('version')
 @command(help="Returns core version")
 def core__version__get(kernel: Kernel):
     return core_kernel_get_version(kernel)
--- src/decorator/alias.py
+++ src/decorator/alias.py
@@ -5,7 +5,7 @@ def alias(name: bool | str = True):
     def decorator(func):
         aliases = FunctionProperty.get_property(func, 'aliases')
         if aliases:
-            aliases.property_value.append(name)
+            aliases.append(name)
         else:
             FunctionProperty(
                 func,
