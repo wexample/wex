4c617bffc38c1794275287de53d48c078a4b5c26
weeger <romain.weeger@gmail.com>
2023-11-16 13:19:50 +0100

    Test

--- addons/core/tests/command/core/cleanup.py
+++ addons/core/tests/command/core/cleanup.py
@@ -1,3 +1,5 @@
+import os
+
 from addons.core.command.core.cleanup import core__core__cleanup
 from tests.AbstractTestCase import AbstractTestCase
 
@@ -10,4 +12,12 @@ class TestCoreCommandCoreCleanup(AbstractTestCase):
 
         self.assertIsNone(
             result,
+        )
+
+        log_dir = self.kernel.get_or_create_path('task')
+        log_files = os.listdir(log_dir)
+
+        self.assertEqual(
+            len(log_files),
+            0
         )
