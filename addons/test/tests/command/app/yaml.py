6ecd2de2fd28c7973042e6402f24596d073da26c
weeger <romain.weeger@gmail.com>
2023-11-18 18:40:01 +0100

    Fix tests

--- addons/test/tests/command/app/yaml.py
+++ addons/test/tests/command/app/yaml.py
@@ -1,18 +1,16 @@
 from src.core.FatalError import FatalError
-from addons.app.tests.AbstractAppTestCase import AbstractAppTestCase
 from addons.app.command.app.exec import app__app__exec
+from addons.app.tests.AbstractWebhookTestCase import AbstractWebhookTestCase
 
 
-class TestTestCommandAppYaml(AbstractAppTestCase):
+class TestTestCommandAppYaml(AbstractWebhookTestCase):
     def test_yaml(self):
         with self.assertRaises(FatalError):
             self.kernel.run_command('test::app/yaml', {
                 'option': 'test',
             })
 
-        app_dir = self.create_and_start_test_app(
-            services=['php']
-        )
+        app_dir, app_name = self.create_and_start_test_app_webhook()
 
         self.kernel.run_function(
             app__app__exec,
@@ -64,10 +62,20 @@ class TestTestCommandAppYaml(AbstractAppTestCase):
 
         self.assertEqual(
             response.output_bag[4].print(),
-            'IN_CONTAINER_PYTHON_SCRIPT'
+            'PYTHON_SUCCESS'
         )
 
         self.assertEqual(
             response.output_bag[5].print(),
+            'PYTHON_SUCCESS_FROM_FILE'
+        )
+
+        self.assertEqual(
+            response.output_bag[6].print(),
+            'IN_CONTAINER_PYTHON_SCRIPT'
+        )
+
+        self.assertEqual(
+            response.output_bag[7].print(),
             'TEST_PYTHON_FILE'
         )
