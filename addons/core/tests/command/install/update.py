e4fa3d72cbdc6666fca1c15e9518cccea2d1a5df
weeger <romain.weeger@gmail.com>
2023-11-18 18:24:48 +0100

    Fix tests

--- addons/core/tests/command/install/update.py
+++ addons/core/tests/command/install/update.py
@@ -1,14 +1,19 @@
 from addons.core.command.install.update import core__install__update
+from src.const.globals import COMMAND_TYPE_ADDON
 from tests.AbstractTestCase import AbstractTestCase
 
 
 class TestCoreCommandInstallUpdate(AbstractTestCase):
     def test_update(self):
-        response = self.kernel.run_function(
-            core__install__update
+        request = self.kernel.create_command_request(
+            self.kernel.get_command_resolver(
+                COMMAND_TYPE_ADDON).build_command_from_function(core__install__update),
         )
 
-        self.assertTrue(
-            # Test only update passed
-            'Hit:1 ' in response.print(),
+        self.assertIsNotNone(
+            request.command,
+        )
+
+        self.assertIsNotNone(
+            request.function,
         )
