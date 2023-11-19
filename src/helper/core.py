fe4a3ba0184cc0cf1ac57e4eba7945757c278ad0
weeger <romain.weeger@gmail.com>
2023-11-19 10:25:18 +0100

    Typing

--- src/helper/core.py
+++ src/helper/core.py
@@ -1,6 +1,8 @@
-import os
+from __future__ import annotations
 
+import os
 from src.const.globals import FILE_VERSION
+from src.core import Kernel
 
 
 def core_get_version(path: str) -> str:
@@ -8,19 +10,19 @@ def core_get_version(path: str) -> str:
         return file.read().strip()
 
 
-def core_kernel_get_version(kernel) -> str:
+def core_kernel_get_version(kernel: Kernel) -> str:
     return core_get_version(kernel.get_path('root'))
 
 
-def get_bashrc_handler_path(kernel):
+def get_bashrc_handler_path(kernel: Kernel) -> str:
     return os.path.join(kernel.get_path('root'), 'cli', "bashrc-handler")
 
 
-def get_daemon_service_path(kernel):
+def get_daemon_service_path(kernel: Kernel) -> str:
     return f'. {get_bashrc_handler_path(kernel)}'
 
 
-def get_daemon_service_resource_path(kernel):
+def get_daemon_service_resource_path(kernel: Kernel) -> str:
     return os.path.join(
         kernel.get_path('root'),
         'src',
