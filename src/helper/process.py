c660e6a87c5e6f657667ea4c03d584ed5edb4515
weeger <romain.weeger@gmail.com>
2023-11-19 11:50:16 +0100

    Typing

--- src/helper/process.py
+++ src/helper/process.py
@@ -1,13 +1,14 @@
+from __future__ import annotations
 import os
-
+from src.core import Kernel
 from src.helper.command import command_to_string, internal_command_to_shell
 from src.const.globals import VERBOSITY_LEVEL_MAXIMUM
+from typing import List, Optional
 
 
 def process_post_exec(
-        kernel,
-        command: []):
-
+        kernel: Kernel,
+        command: List[str]) -> None:
     # All command should be executed by default in the same current workdir.
     if isinstance(command, list):
         command = ['cd', os.getcwd(), '&&'] + command
@@ -21,7 +22,11 @@ def process_post_exec(
     kernel.post_exec.append(command)
 
 
-def process_post_exec_function(kernel, internal_command: str, args: None | list = None, is_async=False):
+def process_post_exec_function(
+        kernel: Kernel,
+        internal_command: str,
+        args: Optional[List[str]] = None,
+        is_async: bool = False) -> None:
     command = internal_command_to_shell(
         kernel=kernel,
         internal_command=internal_command,
