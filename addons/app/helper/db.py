45c20a05818f64281d442518ce97c736b05435c0
weeger <romain.weeger@gmail.com>
2023-11-19 16:59:25 +0100

    Typing

--- addons/app/helper/db.py
+++ addons/app/helper/db.py
@@ -1,9 +1,9 @@
 import os
 from addons.app.AppAddonManager import AppAddonManager
 from addons.app.const.app import APP_DIR_APP_DATA
+from src.core.Kernel import Kernel
 
 
-def get_db_service_dumps_path(kernel, service: str) -> str:
+def get_db_service_dumps_path(kernel: Kernel, service: str) -> str:
     manager: AppAddonManager = kernel.addons['app']
-    env_dir = f'{manager.app_dir}{APP_DIR_APP_DATA}'
-    return os.path.join(env_dir, service, 'dumps')
+    return os.path.join(manager.app_dir, APP_DIR_APP_DATA, service, 'dumps')
