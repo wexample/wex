e4c581b5fc29021957895c426e1f8a7d5c5d2a96
weeger <romain.weeger@gmail.com>
2023-11-20 10:51:00 +0100

    Typing

--- addons/docker/helper/docker.py
+++ addons/docker/helper/docker.py
@@ -20,16 +20,16 @@ def user_has_docker_permission(username: str) -> bool:
         return False
 
 
-def merge_docker_compose_files(src, dest):
+def merge_docker_compose_files(src: str, target: str) -> None:
     # Load both files as Python objects
     with open(src, 'r') as f:
         data1 = yaml.safe_load(f)
-    with open(dest, 'r') as f:
+    with open(target, 'r') as f:
         data2 = yaml.safe_load(f) or {}
 
     # Recursively merge the two objects
     merged_data = dict_merge(data1, data2)
 
     # Write the merged data to a new file
-    with open(dest, 'w') as f:
+    with open(target, 'w') as f:
         yaml.dump(merged_data, f)
