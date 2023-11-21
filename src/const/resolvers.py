064edc297b6ee7bacfe5b322670deb63e2b416dd
weeger <romain.weeger@gmail.com>
2023-11-21 11:04:17 +0100

    Typing

--- src/const/resolvers.py
+++ src/const/resolvers.py
@@ -1,9 +1,12 @@
+from typing import List
+
+from src.core.command.resolver.AbstractCommandResolver import AbstractCommandResolver
 from src.core.command.resolver.AddonCommandResolver import AddonCommandResolver
 from src.core.command.resolver.AppCommandResolver import AppCommandResolver
 from src.core.command.resolver.ServiceCommandResolver import ServiceCommandResolver
 from src.core.command.resolver.UserCommandResolver import UserCommandResolver
 
-COMMAND_RESOLVERS_CLASSES = [
+COMMAND_RESOLVERS_CLASSES: List[type[AbstractCommandResolver]] = [
     AddonCommandResolver,
     ServiceCommandResolver,
     AppCommandResolver,
