7f12e98e76b67c1f698a906ab2c0ea85b2b6648d
weeger <romain.weeger@gmail.com>
2023-12-03 10:30:38 +0100

    Fix tests

--- addons/services_php/services/phpmyadmin/command/config/runtime.py
+++ addons/services_php/services/phpmyadmin/command/config/runtime.py
@@ -1,8 +1,9 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 from addons.app.const.app import APP_ENV_LOCAL, APP_ENV_PROD
 from addons.app.decorator.app_command import app_command
 from src.const.globals import COMMAND_TYPE_SERVICE
+from src.const.types import StringsList
 
 if TYPE_CHECKING:
     from addons.app.AppAddonManager import AppAddonManager
@@ -14,24 +15,26 @@ def phpmyadmin__config__runtime(
 ) -> None:
     # Save config
     domain_pma = manager.get_runtime_config("domain_pma", None)
+    assert isinstance(domain_pma, str | bool | None)
 
     # Setting false to domain will disable domain setting,
     # used to disable phpmyadmin in some environment.
-    if domain_pma is not False:
+    if isinstance(domain_pma, str) and domain_pma:
         if domain_pma is None:
-            env = manager.get_runtime_config("env")
+            env = str(manager.get_runtime_config("env"))
 
-            domain_pma = ["pma"]
+            domain_pma_list: StringsList = ["pma"]
             if env != APP_ENV_LOCAL and env != APP_ENV_PROD:
-                domain_pma.append(env)
+                domain_pma_list.append(env)
 
-            domain_pma.append(manager.get_runtime_config("domain_tld"))
+            domain_pma_list.append(str(manager.get_runtime_config("domain_tld")))
 
-            domain_pma = ".".join(domain_pma)
+            domain_pma = ".".join(domain_pma_list)
 
             manager.set_runtime_config("domain_pma", domain_pma)
 
-        domains = manager.get_runtime_config("domains", [])
+        domains = cast(StringsList, manager.get_runtime_config("domains", []))
+        assert isinstance(domains, list)
 
         domains.append(domain_pma)
 
