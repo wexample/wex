8e55378a33b9df553b5c9c59cde53679baf5154f
weeger <romain.weeger@gmail.com>
2023-12-04 12:29:51 +0100

    Use wrapper

--- addons/app/tests/command/location/find.py
+++ addons/app/tests/command/location/find.py
@@ -1,6 +1,7 @@
 import os
 
 from addons.app.command.location.find import app__location__find
+from helper.dir import dir_execute_in_workdir
 from tests.AbstractTestCase import AbstractTestCase
 
 
@@ -10,25 +11,23 @@ class TestAppCommandLocationFind(AbstractTestCase):
         app_location = self.kernel.run_function(app__location__find).first()
         self.assertEqual(app_location, self.kernel.get_path("root"))
 
-        cwd = os.getcwd()
         tmp_dir = self.kernel.get_or_create_path("tmp")
         root_dir = self.kernel.get_path("root")
 
-        # Change to a dir inside the app and test again.
-        os.chdir(tmp_dir)
-        app_location = self.kernel.run_function(app__location__find).first()
-        self.assertEqual(app_location, root_dir)
+        def _test_find() -> None:
+            app_location = self.kernel.run_function(app__location__find).first()
+            self.assertEqual(app_location, root_dir)
 
-        # Change to root, no app dir should be found.
-        app_location = self.kernel.run_function(
-            app__location__find, {"app-dir": "/var/tmp"}
-        ).first()
-        self.assertEqual(app_location, None)
+            # Change to root, no app dir should be found.
+            app_location = self.kernel.run_function(
+                app__location__find, {"app-dir": "/var/tmp"}
+            ).first()
+            self.assertEqual(app_location, None)
 
-        # Change from root with argument to a dir inside the app and test again.
-        app_location = self.kernel.run_function(
-            app__location__find, {"app-dir": tmp_dir}
-        ).first()
-        self.assertEqual(app_location, root_dir)
+            # Change from root with argument to a dir inside the app and test again.
+            app_location = self.kernel.run_function(
+                app__location__find, {"app-dir": tmp_dir}
+            ).first()
+            self.assertEqual(app_location, root_dir)
 
-        os.chdir(cwd)
+        dir_execute_in_workdir(tmp_dir, _test_find)
