c7d3cd687c5469f69143e8ea9d8284d618d0a614
weeger <romain.weeger@gmail.com>
2023-12-06 09:47:04 +0100

    Fix pipeline

--- addons/app/tests/command/proxy/start.py
+++ addons/app/tests/command/proxy/start.py
@@ -1,6 +1,6 @@
+from addons.app.AppAddonManager import AppAddonManager
 from addons.app.command.proxy.start import app__proxy__start
 from addons.app.command.proxy.stop import app__proxy__stop
-from addons.app.AppAddonManager import AppAddonManager
 from src.helper.command import execute_command_sync
 from tests.AbstractTestCase import AbstractTestCase
 
@@ -11,13 +11,9 @@ class TestAppCommandProxyStart(AbstractTestCase):
         assert isinstance(manager, AppAddonManager)
 
         def callback() -> None:
-            success, result = execute_command_sync(
-                self.kernel, ["docker", "ps"]
-            )
+            success, result = execute_command_sync(self.kernel, ["docker", "ps"])
 
-        manager.exec_in_app_workdir(
-            manager.get_proxy_path(),
-            callback)
+        manager.exec_in_app_workdir(manager.get_proxy_path(), callback)
 
         self.kernel.run_function(app__proxy__start)
         self.kernel.run_function(app__proxy__stop)
