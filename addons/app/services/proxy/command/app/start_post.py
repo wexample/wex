2f9078eac3691c2f8cd14c3984945b6c0905f8cc
weeger <romain.weeger@gmail.com>
2023-12-06 10:25:44 +0100

    Fix pipeline

--- addons/app/services/proxy/command/app/start_post.py
+++ addons/app/services/proxy/command/app/start_post.py
@@ -19,6 +19,10 @@ def proxy__app__start_post(
     execute_command_sync(manager.kernel, ["docker", "logs", "wex_proxy_local_proxy"])
 
     print("DEBUG")
+    execute_command_sync(
+        manager.kernel,
+        ["cat", "/var/www/local/wex-proxy/.wex/tmp/docker-compose.runtime.yml"],
+    )
     execute_command_sync(manager.kernel, ["docker", "ps"])
     execute_command_sync(manager.kernel, ["docker", "ps"])
     execute_command_sync(manager.kernel, ["docker", "ps"])
