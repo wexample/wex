14148f5e3d69645aea6a89c27700905b15e2c931
weeger <romain.weeger@gmail.com>
2023-12-10 16:47:15 +0100

    Set config

--- src/core/ConfigValue.py
+++ src/core/ConfigValue.py
@@ -2,12 +2,14 @@ from typing import Any
 
 from src.core.BaseClass import BaseClass
 
-class ConfiValue(BaseClass):
-    def __init__(self, kernel: "Kernel", value:Any) -> None:
-        super().__init__(kernel)
 
-        self._value: str = value
+class ConfigValue(BaseClass):
+    def __init__(self, value: Any) -> None:
+        self.set_value(value)
 
-    def set_value(self) -> Any:
+    def get_value(self) -> Any:
         self._validate__should_not_be_none(self._value)
         return self._value
+
+    def set_value(self, value) -> None:
+        self._value = value
