8c36d642d365fb1455941a73d3134d2426ba0d2c
weeger <romain.weeger@gmail.com>
2023-12-18 09:05:03 +0100

    Version create

--- addons/app/command/version/build.py
+++ addons/app/command/version/build.py
@@ -47,6 +47,10 @@ def app__version__build(
         kernel.io.log(f"New app version : {new_version}")
 
         manager.set_config("global.version", new_version)
+
+        kernel.run_command(
+            '.version/build'
+        )
     else:
         repo = Repo(app_dir)
         new_version = manager.get_config("global.version").get_str()
