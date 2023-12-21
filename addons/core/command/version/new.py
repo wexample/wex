a430f1c90c8a219fe712bca70da50656ffd1aaf0
weeger <romain.weeger@gmail.com>
2023-12-21 15:39:54 +0100

    New version

--- addons/core/command/version/new.py
+++ addons/core/command/version/new.py
@@ -2,9 +2,14 @@ from typing import TYPE_CHECKING
 
 from git import Repo  # type: ignore
 
-from addons.core.command.version.new_write import core__version__new_write
 from addons.core.command.version.new_commit import core__version__new_commit
+from addons.core.command.version.new_write import core__version__new_write
 from addons.default.const.default import UPGRADE_TYPE_MINOR
+from src.core.response.AbstractResponse import AbstractResponse
+from src.core.response.queue_collection.AbstractQueuedCollectionResponseQueueManager import (
+    AbstractQueuedCollectionResponseQueueManager,
+)
+from src.core.response.QueuedCollectionResponse import QueuedCollectionResponse
 from src.decorator.command import command
 from src.decorator.option import option
 
@@ -14,16 +19,25 @@ if TYPE_CHECKING:
 
 @command(help="Build a new version of current core, or commit new version changes")
 @option("--type", "-t", type=str, required=False, help="Type of update")
-def core__version__new(kernel: "Kernel", type: str = UPGRADE_TYPE_MINOR) -> str:
-    new_version = kernel.run_function(
-        core__version__new_write,
-        {
-            "type": type,
-        },
-    ).first()
+def core__version__new(
+    kernel: "Kernel", type: str = UPGRADE_TYPE_MINOR
+) -> QueuedCollectionResponse:
+    def _write(
+        queue: AbstractQueuedCollectionResponseQueueManager,
+    ) -> AbstractResponse:
+        return kernel.run_function(
+            core__version__new_write,
+            {
+                "type": type,
+            },
+        )
 
-    kernel.run_function(
-        core__version__new_commit
+    def _commit(
+        queue: AbstractQueuedCollectionResponseQueueManager,
+    ) -> AbstractResponse:
+        return kernel.run_function(core__version__new_commit)
+
+    return QueuedCollectionResponse(
+        kernel,
+        [_write, _commit],
     )
-
-    return str(new_version)
