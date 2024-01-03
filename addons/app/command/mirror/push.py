24c65b6b95cc1f0efed44e5c3241f2478879e777
weeger <romain.weeger@gmail.com>
2024-01-03 17:40:03 +0100

    Transfer a test file

--- addons/app/command/mirror/push.py
+++ addons/app/command/mirror/push.py
@@ -1,14 +1,15 @@
 from src.const.globals import COMMAND_TYPE_ADDON
-from src.decorator.command import command
 from src.decorator.option import option
+from addons.app.decorator.app_command import app_command
 from typing import TYPE_CHECKING
-from addons.app.command.remote.available import app__remote__available
+from addons.app.helper.remote import remote_get_environment_ip
+from src.helper.command import execute_command_sync
 
 if TYPE_CHECKING:
-    from src.core.Kernel import Kernel
+    from addons.app.AppAddonManager import AppAddonManager
 
 
-@command(help="Description", command_type=COMMAND_TYPE_ADDON)
+@app_command(help="Description", command_type=COMMAND_TYPE_ADDON)
 @option(
     "--environment",
     "-e",
@@ -16,12 +17,40 @@ if TYPE_CHECKING:
     required=True,
     help="Remote environment (dev, prod)",
 )
-def app__mirror__push(kernel: "Kernel", environment: str) -> None:
-    response = kernel.run_function(
-        app__remote__available, {
-            "environment":
-                environment}
-    )
+def app__mirror__push(manager: "AppAddonManager", environment: str, app_dir: str) -> bool:
+    domain_or_ip = remote_get_environment_ip(manager.kernel, environment)
 
-    if response.first() is not True:
-        return
+    if not domain_or_ip:
+        return False
+
+    remote_path = f"/var/www/{environment}/{manager.get_config('global.name').get_str()}/"
+    # TODO Configure credentials
+    command = ["sshpass", "-p", "TEST_PASSWORD", "ssh", "-o", "StrictHostKeyChecking=no", f"root@{domain_or_ip}",
+               f"mkdir -p {remote_path}"]
+
+    # Creating directory if missing
+    execute_command_sync(
+        manager.kernel,
+        command)
+
+    # TODO TMP, use sync directory schema instead.
+    files = ["test.txt"]
+
+    for file_path in files:
+        command = [
+            "sshpass",
+            "-p",
+            "TEST_PASSWORD",
+            "scp",
+            "-o",
+            "StrictHostKeyChecking=no",
+            file_path,
+            f"root@{domain_or_ip}:{remote_path}test.txt"
+        ]
+
+        manager.log(f'Send file to {domain_or_ip} : {file_path}')
+        execute_command_sync(
+            manager.kernel,
+            command)
+
+    return True
--- addons/app/tests/command/mirror/push.py
+++ addons/app/tests/command/mirror/push.py
@@ -1,19 +1,19 @@
+import os
+
 from src.helper.command import execute_command_sync
-from tests.AbstractTestCase import AbstractTestCase
-from addons.app.command.remote.available import app__remote__available
+from addons.app.command.mirror.push import app__mirror__push
+from addons.app.tests.AbstractAppTestCase import AbstractAppTestCase
+from addons.app.AppAddonManager import AppAddonManager
 
 
-class TestAppCommandMirrorPush(AbstractTestCase):
+class TestAppCommandMirrorPush(AbstractAppTestCase):
     def test_push(self) -> None:
-        response = self.kernel.run_function(
-            app__remote__available, {
-                "environment": "missing"}
-        )
-
-        self.assertEqual(response.first(), False)
+        app_dir = self.create_and_start_test_app(services=["php"])
 
         self.log('Starting fake test remote server')
         command = ["docker", "compose", "-f", ".wex/docker/docker-compose.test-remote.yml", "up", "-d", "--build"]
+
+        environment = "test-remote"
 
         execute_command_sync(
             self.kernel,
@@ -32,13 +32,36 @@ class TestAppCommandMirrorPush(AbstractTestCase):
 
         ip: str = ip_list[0]
 
-        self.log(f'Send test file to {ip}')
-        command = ["sshpass", "-p", "TEST_PASSWORD", "scp", "-o", "StrictHostKeyChecking=no", f"{self.kernel.directory.path}version.txt", f"root@{ip}:/var/version.txt"]
-
+        # Reuse manager to work with
+        manager = AppAddonManager(self.kernel, app_dir=app_dir)
+        manager.set_config("env.test-remote.server.ip", ip)
 
         execute_command_sync(
+            manager.kernel,
+            ["touch", f"{app_dir}test.txt"])
+
+        response = self.kernel.run_function(
+            app__mirror__push, {
+                "environment": "test-remote",
+                "app-dir": app_dir
+            }
+        )
+
+        remote_path = f"/var/www/{environment}/{manager.get_config('global.name').get_str()}/"
+        command = ["sshpass", "-p", "TEST_PASSWORD", "ssh", "-o", "StrictHostKeyChecking=no", f"root@{ip}",
+                   f"ls -la {remote_path}test.txt"]
+
+        success, files_list = execute_command_sync(
             self.kernel,
             command)
+
+        self.assertTrue(
+            files_list[0].startswith('-rwxr-xr-x'),
+            "The local file has been created remotely"
+        )
+
+        # Rollback to working dir.
+        os.chdir(self.kernel.directory.path)
 
         self.log('Stopping fake test remote server')
         command = ["docker", "compose", "-f", ".wex/docker/docker-compose.test-remote.yml", "down", "--rmi", "all"]
