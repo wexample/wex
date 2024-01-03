001049a99ba596bb746c8ef02e02c3cccf3a94d7
weeger <romain.weeger@gmail.com>
2024-01-03 15:14:40 +0100

    Starting test mirror server

--- addons/app/tests/command/mirror/push.py
+++ addons/app/tests/command/mirror/push.py
@@ -1,3 +1,4 @@
+from src.helper.command import execute_command_sync
 from tests.AbstractTestCase import AbstractTestCase
 from addons.app.command.remote.available import app__remote__available
 
@@ -6,7 +7,22 @@ class TestAppCommandMirrorPush(AbstractTestCase):
     def test_push(self) -> None:
         response = self.kernel.run_function(
             app__remote__available, {
-                "environment": "missing"}
+                "environment": "test-mirror",
+                "app-dir": self.kernel.directory.path
+            }
         )
 
-        self.assertEqual(response.first(), False)
+        self.log('Starting fake test remote server')
+        command = ["docker", "compose", "-f", ".wex/docker/docker-compose.test-remote.yml", "up", "-d"]
+
+        execute_command_sync(
+            self.kernel,
+            command)
+
+        self.log('Stopping fake test remote server')
+        command = ["docker", "compose", "-f", ".wex/docker/docker-compose.test-remote.yml", "down"]
+
+        execute_command_sync(
+            self.kernel,
+            command)
+
