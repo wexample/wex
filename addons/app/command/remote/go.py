9d3f24b7fc8d1cd7803f3cfbc3bd97dab73053ba
weeger <romain.weeger@gmail.com>
2024-01-04 11:50:40 +0100

    Rework remote/go

--- addons/app/command/remote/go.py
+++ addons/app/command/remote/go.py
@@ -1,8 +1,8 @@
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, cast
 
+from addons.app.command.remote.exec import app__remote__exec
 from addons.app.decorator.app_command import app_command
-from addons.app.helper.remote import remote_get_environment_ip
-from src.const.globals import COMMAND_TYPE_ADDON
+from src.const.globals import COMMAND_TYPE_ADDON, SHELL_DEFAULT
 from src.core.response.InteractiveShellCommandResponse import (
     InteractiveShellCommandResponse,
 )
@@ -23,18 +23,15 @@ if TYPE_CHECKING:
 def app__remote__go(
     manager: "AppAddonManager", app_dir: str, environment: str
 ) -> Optional[InteractiveShellCommandResponse]:
-    domain_or_ip = remote_get_environment_ip(manager.kernel, environment)
+    result = manager.kernel.run_function(
+        app__remote__exec,
+        {
+            "app-dir": app_dir,
+            "environment": environment,
+            "terminal": True,
+            "command": f'sh -c "cd /var/www/{environment}/{manager.get_config("global.name").get_str()}/ && {SHELL_DEFAULT}"'
+        }
+    ).first()
 
-    if not domain_or_ip:
-        return
-
-    app_name = manager.get_config("global.name").get_str()
-    return InteractiveShellCommandResponse(
-        manager.kernel,
-        [
-            "ssh",
-            "-t",
-            domain_or_ip,
-            f"\"sudo sh -c 'cd /var/www/{environment}/{app_name}/ && /bin/bash'\"",
-        ],
-    )
+    if result:
+        return cast(InteractiveShellCommandResponse, result)
