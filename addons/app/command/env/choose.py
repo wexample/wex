c57b223ca6c82f1791cf227c93fb26f451eb66f2
weeger <romain.weeger@gmail.com>
2024-01-07 21:16:58 +0100

    App choose

---
+++ addons/app/command/env/choose.py
@@ -0,0 +1,35 @@
+from InquirerPy.base.control import Choice
+from typing import List, cast, Optional, TYPE_CHECKING
+from src.helper.prompt import prompt_choice
+from addons.app.const.app import (
+    APP_ENV_LOCAL,
+    APP_ENVS
+)
+from addons.app.decorator.app_command import app_command
+from src.core.response.queue_collection.QueuedCollectionStopResponse import (
+    AbortResponse,
+)
+from addons.app.command.env.set import app__env__set
+
+if TYPE_CHECKING:
+    from addons.app.AppAddonManager import AppAddonManager
+
+
+@app_command(help="Ask user about which environment to use")
+def app__env__choose(manager: "AppAddonManager", app_dir: str) -> Optional[AbortResponse]:
+    env = prompt_choice(
+        "Choose app environment",
+        cast(List[str | Choice], APP_ENVS),
+        APP_ENV_LOCAL,
+    )
+
+    # User said "no" or chose "abort"
+    if not env:
+        manager.log("Abort")
+        return AbortResponse(manager.kernel, "USER_ABORT_CONFIGURATION")
+
+    manager.kernel.run_function(
+        app__env__set, {
+            'app_dir': manager.get_app_dir(),
+            "environment": env
+        })
---
+++ addons/app/tests/command/env/choose.py
@@ -0,0 +1,8 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestAppCommandEnvChoose(AbstractTestCase):
+    def test_choose(self) -> None:
+        # response = self.kernel.run_function(app__env__choose)
+        # Is interactive.
+        self.assertTrue(True)
