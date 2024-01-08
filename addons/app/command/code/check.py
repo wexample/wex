d4cc8bc6ee28c7bec827ef7ed5a45ccfcfab0e8a
weeger <romain.weeger@gmail.com>
2024-01-08 22:13:52 +0100

    Fix code commands

--- addons/app/command/code/check.py
+++ addons/app/command/code/check.py
@@ -1,14 +1,14 @@
 from typing import TYPE_CHECKING
 
 from src.const.globals import COMMAND_TYPE_ADDON
-from src.decorator.command import command
+from addons.app.decorator.app_command import app_command
 
 if TYPE_CHECKING:
     from src.core.Kernel import Kernel
 
 
-@command(help="Validate the code of current application", command_type=COMMAND_TYPE_ADDON)
-def app__code__check(kernel: "Kernel") -> None:
+@app_command(help="Validate the code of current application", command_type=COMMAND_TYPE_ADDON)
+def app__code__check(kernel: "Kernel", app_dir: str) -> None:
     """
     This method is a placeholder to allow local app command attachment.s
     """
--- addons/app/command/code/format.py
+++ addons/app/command/code/format.py
@@ -1,14 +1,14 @@
 from typing import TYPE_CHECKING
 
 from src.const.globals import COMMAND_TYPE_ADDON
-from src.decorator.command import command
+from addons.app.decorator.app_command import app_command
 
 if TYPE_CHECKING:
     from src.core.Kernel import Kernel
 
 
-@command(help="Format the code of current application", command_type=COMMAND_TYPE_ADDON)
-def app__code__format(kernel: "Kernel") -> None:
+@app_command(help="Format the code of current application", command_type=COMMAND_TYPE_ADDON)
+def app__code__format(kernel: "Kernel", app_dir: str) -> None:
     """
     This method is a placeholder to allow local app command attachment.s
     """
