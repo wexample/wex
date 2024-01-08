18254a612c33bf137e3182cddd2e5ce00cc8ec1e
weeger <romain.weeger@gmail.com>
2024-01-08 21:38:25 +0100

    Create attachable code commands

---
+++ addons/app/tests/command/code/check.py
@@ -0,0 +1,6 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestAppCommandCodeCheck(AbstractTestCase):
+    def test_check(self) -> None:
+        self.assertTrue(True)
---
+++ addons/app/tests/command/code/format.py
@@ -0,0 +1,6 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestAppCommandCodeFormat(AbstractTestCase):
+    def test_format(self) -> None:
+        self.assertTrue(True)
