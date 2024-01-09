ec0baf180ee391c89d56d9b978aa272b22d5b0e6
weeger <romain.weeger@gmail.com>
2024-01-09 08:55:36 +0100

    App test run

---
+++ addons/app/command/test/run.py
@@ -0,0 +1,11 @@
+from src.const.globals import COMMAND_TYPE_ADDON
+from addons.app.decorator.app_command import app_command
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.core.Kernel import Kernel
+
+
+@app_command(help="Description", command_type=COMMAND_TYPE_ADDON)
+def app__test__run(kernel: "Kernel") -> None:
+    return None
---
+++ addons/app/tests/command/test/run.py
@@ -0,0 +1,6 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestAppCommandTestRun(AbstractTestCase):
+    def test_run(self) -> None:
+        self.assertTrue(True)
