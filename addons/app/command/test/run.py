9c0a45d6eb5575905bb70ce700c678d9087b46fd
weeger <romain.weeger@gmail.com>
2024-01-09 09:01:32 +0100

    App test run

--- addons/app/command/test/run.py
+++ addons/app/command/test/run.py
@@ -7,5 +7,5 @@ if TYPE_CHECKING:
 
 
 @app_command(help="Description", command_type=COMMAND_TYPE_ADDON)
-def app__test__run(kernel: "Kernel") -> None:
+def app__test__run(kernel: "Kernel", app_dir: str) -> None:
     return None
