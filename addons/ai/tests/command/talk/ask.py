7e55ce8e2147ce09c26c5d8cf02e4681f10fb628
weeger <romain.weeger@gmail.com>
2024-01-14 02:25:47 +0100

    AI ask

---
+++ addons/ai/tests/command/talk/ask.py
@@ -0,0 +1,7 @@
+from tests.AbstractTestCase import AbstractTestCase
+
+
+class TestAiCommandTalkAsk(AbstractTestCase):
+    def test_ask(self) -> None:
+        # Do not test as it is a paid / external service.
+        self.assertTrue(True)
