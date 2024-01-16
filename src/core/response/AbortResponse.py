9f0065037a68bd1f480aa6cfa1295e9e6bf4416c
weeger <romain.weeger@gmail.com>
2024-01-16 18:08:06 +0100

    Logging

--- src/core/response/AbortResponse.py
+++ src/core/response/AbortResponse.py
@@ -1,9 +1,34 @@
 from __future__ import annotations
 
-from abc import ABC
-
 from src.core.response.AbstractEmptyResponse import AbstractEmptyResponse
+
+from abc import ABC
+from typing import TYPE_CHECKING
+
+from src.core.CommandRequest import CommandRequest
+from src.core.response.AbstractResponse import AbstractResponse
+from src.const.globals import (
+    KERNEL_RENDER_MODE_TERMINAL,
+    VERBOSITY_LEVEL_MAXIMUM,
+)
+
+if TYPE_CHECKING:
+    from src.const.types import OptionalCoreCommandArgsDict
 
 
 class AbortResponse(AbstractEmptyResponse, ABC):
-    pass
+
+    def render_content(
+        self,
+        request: CommandRequest,
+        render_mode: str = KERNEL_RENDER_MODE_TERMINAL,
+        args: OptionalCoreCommandArgsDict = None,
+    ) -> AbstractResponse:
+        # Nothing to do
+        self.kernel.io.log(f"Aborting : {self.reason}", verbosity=VERBOSITY_LEVEL_MAXIMUM)
+
+        return super().render_content(
+            request,
+            render_mode,
+            args,
+        )
