89eb9b7ce66cca40102faa61321165704c784344
weeger <romain.weeger@gmail.com>
2024-01-16 14:21:27 +0100

    Code reformat

--- addons/app/command/branch/ip.py
+++ addons/app/command/branch/ip.py
@@ -1,16 +1,22 @@
-from src.const.types import StringKeysDict
-from src.const.globals import COMMAND_TYPE_ADDON
-from src.decorator.option import option
 from typing import TYPE_CHECKING, Optional
+
 from addons.app.decorator.app_command import app_command
+from src.const.globals import COMMAND_TYPE_ADDON
+from src.const.types import StringKeysDict
+from src.decorator.option import option
 
 if TYPE_CHECKING:
     from addons.app.AppAddonManager import AppAddonManager
 
 
-@app_command(help="Return server ip for given branch as set in app config", command_type=COMMAND_TYPE_ADDON)
-@option('--branch', '-b', type=str, required=True, help="Branch name")
-def app__branch__ip(manager: "AppAddonManager", branch: str, app_dir: str) -> Optional[str]:
+@app_command(
+    help="Return server ip for given branch as set in app config",
+    command_type=COMMAND_TYPE_ADDON,
+)
+@option("--branch", "-b", type=str, required=True, help="Branch name")
+def app__branch__ip(
+    manager: "AppAddonManager", branch: str, app_dir: str
+) -> Optional[str]:
     environments_config = manager.get_config(f"env").get_dict()
 
     for env in environments_config:
