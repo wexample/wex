184312ab20eb3fb0fbc8f20f395af53cd0f9cfc7
weeger <romain.weeger@gmail.com>
2024-01-25 05:16:44 +0100

    Rework inheritance

--- addons/ai/src/assistant/AppAssistant.py
+++ addons/ai/src/assistant/AppAssistant.py
@@ -1,9 +1,7 @@
-import os
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
-from langchain.agents import AgentExecutor, create_react_agent
-from langchain.prompts import PromptTemplate
 from addons.ai.src.assistant.Assistant import Assistant
+from src.helper.file import file_read
 from src.const.types import StringKeysDict
 
 if TYPE_CHECKING:
@@ -16,37 +14,12 @@ class AppAssistant(Assistant):
 
         self.manager = manager
 
-    def assist(self, question: str) -> StringKeysDict:
-        prompt = PromptTemplate.from_file(f"{self.manager.kernel.directory.path}addons/ai/samples/prompts/react.txt")
-
-        agent = create_react_agent(
-            self.llm,
-            self.tools,
-            prompt=prompt
-        )
-
-        agent_executor = AgentExecutor(
-            agent=agent,
-            tools=self.tools,
-            verbose=True)
-
-        return agent_executor.invoke(
-            {"input": question}
-        )["output"]
-
-    def load_file(self, file_path: str) -> Optional[str]:
-        if not os.path.exists(file_path):
-            return None
-
-        with open(file_path, "r") as f:
-            return f.read()
-
     def load_example_patch(self, name: str) -> StringKeysDict:
         base_path = f"{self.manager.get_app_dir()}.wex/command/samples/examples/{name}/"
 
         return {
-            "prompt": self.load_file(f"{base_path}prompt.txt"),
-            "source": self.load_file(f"{base_path}source.py"),
-            "patch": self.load_file(f"{base_path}response.patch"),
-            "tree": self.load_file(f"{base_path}tree.yml"),
+            "prompt": file_read(f"{base_path}prompt.txt"),
+            "source": file_read(f"{base_path}source.py"),
+            "patch": file_read(f"{base_path}response.patch"),
+            "tree": file_read(f"{base_path}tree.yml"),
         }
--- addons/ai/src/assistant/Assistant.py
+++ addons/ai/src/assistant/Assistant.py
@@ -3,6 +3,10 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from addons.ai.src.tool.CommandTool import CommandTool
 from src.helper.registry import registry_get_all_commands
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain.prompts import PromptTemplate
+from src.const.types import StringKeysDict
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -23,8 +27,9 @@ class Assistant:
         self.tools = []
 
         for command_name in all_commands:
-            if ("ai_tool" in all_commands[command_name]["properties"]
-                and all_commands[command_name]["properties"]["ai_tool"] == True):
+            properties = all_commands[command_name]["properties"]
+
+            if "ai_tool" in properties and properties["ai_tool"]:
                 self.kernel.io.log(f"Loading tool {command_name}")
 
                 command_tool = CommandTool(
@@ -36,3 +41,21 @@ class Assistant:
                 self.tools.append(command_tool)
 
         self.kernel.io.log(f"Loaded {len(self.tools)} tools")
+
+    def assist(self, question: str) -> StringKeysDict:
+        prompt = PromptTemplate.from_file(f"{self.kernel.directory.path}addons/ai/samples/prompts/react.txt")
+
+        agent = create_react_agent(
+            self.llm,
+            self.tools,
+            prompt=prompt
+        )
+
+        agent_executor = AgentExecutor(
+            agent=agent,
+            tools=self.tools,
+            verbose=True)
+
+        return agent_executor.invoke(
+            {"input": question}
+        )["output"]
