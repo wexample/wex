7d8830ae618d652d5b95c7bbed67adf37a334913
weeger <romain.weeger@gmail.com>
2024-01-30 17:29:22 +0100

    Install redis

---
+++ addons/services_db/services/redis/command/service/install.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+from addons.app.decorator.app_command import app_command
+from src.const.globals import COMMAND_TYPE_SERVICE, PASSWORD_INSECURE
+
+if TYPE_CHECKING:
+    from addons.app.AppAddonManager import AppAddonManager
+
+
+@app_command(help="Install database service", command_type=COMMAND_TYPE_SERVICE)
+def redis__service__install(
+    manager: "AppAddonManager", app_dir: str, service: str
+) -> None:
+    name = manager.get_config("global.name").get_str()
+    manager.set_config(
+        f"service.{service}",
+        {
+            "host": f"{name}_redis",
+            "name": f"{name}",
+            "password": PASSWORD_INSECURE,
+            "port": 6379,
+            "user": "redis",
+        },
+    )
