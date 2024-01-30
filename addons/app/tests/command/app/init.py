3c1fcf7b4d0d78828834a3d3c8952e4d27da7916
weeger <romain.weeger@gmail.com>
2024-01-30 13:18:43 +0100

    Init fix

--- addons/app/tests/command/app/init.py
+++ addons/app/tests/command/app/init.py
@@ -3,4 +3,8 @@ from addons.app.tests.AbstractAppTestCase import AbstractAppTestCase
 
 class TestAppCommandAppInit(AbstractAppTestCase):
     def test_init(self) -> None:
+        # Test without any service
+        self.create_test_app(force_restart=True)
+
+        # Test with a service
         self.create_and_start_test_app(services=["php"], force_restart=True)
