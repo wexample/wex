547132c4b88a6206aedbb2e5388a2865a0052792
weeger <romain.weeger@gmail.com>
2024-01-31 11:20:52 +0100

    Abstract

--- src/core/response/queue_collection/QueuedCollectionStopCurrentStepResponse.py
+++ src/core/response/queue_collection/QueuedCollectionStopCurrentStepResponse.py
@@ -1,7 +1,36 @@
 from __future__ import annotations
 
+from src.const.types import OptionalCoreCommandArgsDict
+from src.const.globals import KERNEL_RENDER_MODE_TERMINAL
+from src.core.CommandRequest import CommandRequest
+from src.core.response.AbstractResponse import AbstractResponse
+from src.core.response.queue_collection.HasAttachedResponse import HasAttachedResponse
 from src.core.response.AbstractEmptyResponse import AbstractEmptyResponse
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from src.core.Kernel import Kernel
 
 
-class QueuedCollectionStopCurrentStepResponse(AbstractEmptyResponse):
-    pass
+class QueuedCollectionStopCurrentStepResponse(AbstractEmptyResponse, HasAttachedResponse):
+    def __init__(self, kernel: "Kernel", reason: str, response: Any = None) -> None:
+        AbstractEmptyResponse.__init__(self, kernel, reason)
+        HasAttachedResponse.__init__(self, kernel, response)
+
+    def render_content(
+        self,
+        request: CommandRequest,
+        render_mode: str = KERNEL_RENDER_MODE_TERMINAL,
+        args: OptionalCoreCommandArgsDict = None,
+    ) -> AbstractResponse:
+        self.render_content_append_response(
+            request,
+            render_mode,
+            args,
+        )
+
+        return super().render_content(
+            request,
+            render_mode,
+            args,
+        )
--- src/core/response/queue_collection/QueuedCollectionStopResponse.py
+++ src/core/response/queue_collection/QueuedCollectionStopResponse.py
@@ -1,21 +1,21 @@
 from __future__ import annotations
 
-from src.core.response.AbortResponse import AbortResponse
-from typing import TYPE_CHECKING, Any
+from src.const.types import OptionalCoreCommandArgsDict
 from src.const.globals import KERNEL_RENDER_MODE_TERMINAL
 from src.core.CommandRequest import CommandRequest
 from src.core.response.AbstractResponse import AbstractResponse
+from src.core.response.queue_collection.HasAttachedResponse import HasAttachedResponse
+from src.core.response.AbortResponse import AbortResponse
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
-    from src.const.types import OptionalCoreCommandArgsDict
+    from src.core.Kernel import Kernel
 
 
-class QueuedCollectionStopResponse(AbortResponse):
+class QueuedCollectionStopResponse(AbortResponse, HasAttachedResponse):
     def __init__(self, kernel: "Kernel", reason: str, response: Any = None) -> None:
-        super().__init__(kernel, reason)
-
-        self.reason = reason
-        self.response: Any = response
+        AbortResponse.__init__(self, kernel, reason)
+        HasAttachedResponse.__init__(self, kernel, response)
 
     def render_content(
         self,
@@ -23,16 +23,11 @@ class QueuedCollectionStopResponse(AbortResponse):
         render_mode: str = KERNEL_RENDER_MODE_TERMINAL,
         args: OptionalCoreCommandArgsDict = None,
     ) -> AbstractResponse:
-        # Allow to return a response even stopping.
-        if self.response:
-            response_wrapped = self.get_request().resolver.wrap_response(self.response)
-            response_wrapped.render_content(
-                request,
-                render_mode,
-                args,
-            )
-
-            self.output_bag = response_wrapped.output_bag.copy()
+        self.render_content_append_response(
+            request,
+            render_mode,
+            args,
+        )
 
         return super().render_content(
             request,
