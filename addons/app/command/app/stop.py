d195c92db56832d38bf45e263e2c51e6cb16f389
weeger <romain.weeger@gmail.com>
2024-02-05 07:44:23 +0100

    Rename job

--- addons/app/command/app/stop.py
+++ addons/app/command/app/stop.py
@@ -7,6 +7,7 @@ from addons.app.command.hosts.update import app__hosts__update
 from addons.app.const.app import APP_FILEPATH_REL_COMPOSE_RUNTIME_YML
 from addons.app.decorator.app_command import app_command
 from addons.app.helper.docker import docker_exec_app_compose_command
+from src.decorator.option import option
 from src.core.response.InteractiveShellCommandResponse import (
     InteractiveShellCommandResponse,
 )
@@ -26,8 +27,11 @@ from src.core.response.queue_collection.AbstractQueuedCollectionResponseQueueMan
 
 @as_sudo()
 @app_command(help="Stop the given app")
+@option("--fast", "-f", is_flag=True, required=False, help="Do not rewrite config")
 def app__app__stop(
-    manager: "AppAddonManager", app_dir: str
+    manager: "AppAddonManager",
+    app_dir: str,
+    fast: bool = False,
 ) -> QueuedCollectionResponse:
     kernel = manager.kernel
     name = manager.get_config("global.name").get_str()
@@ -107,13 +111,21 @@ def app__app__stop(
             app__hook__exec, {"app-dir": app_dir, "hook": "app/stop-post"}
         )
 
-    return QueuedCollectionResponse(
-        kernel,
-        [
+    if fast:
+        steps = [
+            # Just load docker compose
+            _app__app__stop__rm,
+        ]
+    else:
+        steps = [
             _app__app__stop__checkup,
             _app__app__stop__stop,
             _app__app__stop__rm,
             _app__app__stop__update_hosts,
             _app__app__stop__complete,
-        ],
+        ]
+
+    return QueuedCollectionResponse(
+        kernel,
+        steps,
     )
