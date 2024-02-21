8fd6d5fd4b66fa88767e9820db350b97432c9d37
weeger <romain.weeger@gmail.com>
2024-02-21 15:11:03 +0100

    Iterate over schema

--- src/core/file/DirectoryStructure.py
+++ src/core/file/DirectoryStructure.py
@@ -1,4 +1,5 @@
 import os
+from typing import Callable
 
 from src.core.file.AbstractFileSystemStructure import AbstractFileSystemStructure
 
@@ -17,3 +18,29 @@ class DirectoryStructure(AbstractFileSystemStructure):
 
     def get_parent_dir(self) -> str:
         return os.path.dirname(os.path.dirname(self.path)) + os.path.sep
+
+    def process_schema_recursive(
+        self,
+        action_function: Callable[[str, dict, str], None],
+    ) -> None:
+        self._process_schema_recursive(
+            item_name="",
+            schema=self.get_schema(),
+            action_function=action_function
+        )
+
+    def _process_schema_recursive(
+        self,
+        item_name: str,
+        schema: dict,
+        action_function: Callable[[str, dict, str], None],
+    ) -> None:
+        action_function(item_name, schema)
+
+        if "schema" in schema:
+            for child_item_name, child_schema in schema["schema"].items():
+                self._process_schema_recursive(
+                    item_name=os.path.join(item_name, child_item_name),
+                    schema=child_schema,
+                    action_function=action_function,
+                )
