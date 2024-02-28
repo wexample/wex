7b98c75f555bd32d17559656ebc2cd9420eaf6a4
weeger <romain.weeger@gmail.com>
2024-02-28 11:37:52 +0100

    Write version

--- addons/app/command/version/new_commit.py
+++ addons/app/command/version/new_commit.py
@@ -27,22 +27,10 @@ def app__version__new_commit(manager: "AppAddonManager", app_dir: str) -> None:
     except Exception as e:
         kernel.io.error("Git pull : " + str(e), trace=False)
 
-    # Get the last tag.
-    tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
-    if len(tags):
-        latest_tag = tags[-1]
-
-        if str(latest_tag) == new_version:
-            kernel.io.error(
-                f"The version {new_version} has been already tagged, you should create a new version.",
-                trace=False,
-            )
-
     kernel.io.log("Committing new version...")
     try:
         repo.git.add(A=True)
         repo.index.commit(f"New version v{new_version}")
-        repo.create_tag(f"{new_version}")
     except Exception as e:
         kernel.io.error("Git commit : " + str(e), trace=False)
 
