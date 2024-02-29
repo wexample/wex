0146333c5f4289c6a9860a0edf7b410725dccc80
weeger <romain.weeger@gmail.com>
2024-02-29 11:44:00 +0100

    Push recursively

--- addons/app/command/remote/push.py
+++ addons/app/command/remote/push.py
@@ -13,7 +13,7 @@ from addons.app.helper.remote import (
     remote_get_environment_ip,
     remote_get_login_command,
 )
-from src.decorator.as_sudo import as_sudo
+from addons.app.const.app import APP_ENV_PROD
 from src.const.globals import COMMAND_TYPE_ADDON, WEBHOOK_LISTEN_PORT_DEFAULT
 from src.const.types import FileSystemStructureSchema
 from src.core.response.DictResponse import DictResponse
@@ -24,7 +24,6 @@ from src.helper.string import string_to_snake_case
 if TYPE_CHECKING:
     from addons.app.AppAddonManager import AppAddonManager
 
-@as_sudo()
 @app_command(help="Description", command_type=COMMAND_TYPE_ADDON, should_be_valid=True)
 @option(
     "--environment",
@@ -49,6 +48,10 @@ def app__remote__push(
         manager.log(f"Unable to push to same environment {environment}")
         return None
 
+    if environment == APP_ENV_PROD:
+        manager.kernel.io.error(f"You don't want to do that on {environment}")
+        return None
+
     def _app__remote__push_copy_to_remote(
         item_name: str, schema: FileSystemStructureSchema
     ) -> None:
@@ -58,41 +61,53 @@ def app__remote__push(
         item_realpath = os.path.realpath(item_name)
         remote_path = remote_build_temp_push_dir(environment, manager.get_app_name())
 
-        # If item should be created, it will be checked on regular path.
+        # If item should be created, it will be checked on initialization.
         if os.path.exists(item_realpath):
             remote_item_path = os.path.join(remote_path, item_name)
             manager.log(
-                f"Sending file {item_realpath} to {connexion_address}:{remote_item_path}"
+                f"Sending item {item_realpath} to {connexion_address}:{remote_item_path}"
             )
 
             if os.path.isfile(item_realpath):
-                remote_item_dir = os.path.dirname(remote_item_path)
-            else:
-                remote_item_dir = remote_item_path
+                _create_remote_dir(os.path.dirname(remote_path))
+                _send_file(item_realpath, remote_item_path)
+            elif os.path.isdir(item_realpath):
+                _send_directory(item_realpath, remote_item_path)
 
-            manager.kernel.io.log(f"Creating remote dir {remote_item_dir}")
-            manager.kernel.run_function(
-                app__remote__exec,
-                {
-                    "app-dir": manager.get_app_dir(),
-                    "environment": environment,
-                    "command": f"mkdir -p {remote_item_dir}",
-                },
+    def _create_remote_dir(remote_item_dir: str):
+        manager.kernel.io.log(f"Creating remote dir {remote_item_dir}")
+        manager.kernel.run_function(
+            app__remote__exec,
+            {
+                "app-dir": manager.get_app_dir(),
+                "environment": environment,
+                "command": f"mkdir -p {remote_item_dir}",
+            },
+            # Do not enqueue execution
+            fast_mode=True
+        )
+
+    def _send_file(local_path: str, remote_path: str) -> None:
+        manager.kernel.io.log(f"Copying file to {remote_path}")
+        execute_command_sync(
+            manager.kernel,
+            (
+                remote_get_login_command(manager, environment)
+                + ["scp"]
+                + remote_get_connexion_options()
             )
+            + [local_path, f"{connexion_address}:{remote_path}"])
 
-            if os.path.isfile(item_realpath):
-                manager.kernel.io.log(f"Copying file {remote_item_dir}")
-                execute_command_sync(
-                    manager.kernel,
-                    (
-                        remote_get_login_command(manager, environment)
-                        + [
-                            "scp",
-                        ]
-                        + remote_get_connexion_options()
-                    )
-                    + [item_name, f"{connexion_address}:{remote_path}{item_name}"],
-                )
+    def _send_directory(local_dir: str, remote_dir: str) -> None:
+        for root, dirs, files in os.walk(local_dir):
+            relative_path = os.path.relpath(root, local_dir)
+            remote_root = os.path.join(remote_dir, relative_path)
+            _create_remote_dir(remote_root)
+
+            for file in files:
+                local_file_path = os.path.join(root, file)
+                remote_file_path = os.path.join(remote_root, file)
+                _send_file(local_file_path, remote_file_path)
 
     manager.get_directory().process_schema_recursive(_app__remote__push_copy_to_remote)
     user = manager.get_env_var(
