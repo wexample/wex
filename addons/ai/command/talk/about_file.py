54e2e6317e60010a70e96f1df85309e33af4f5dc
weeger <romain.weeger@gmail.com>
2024-04-05 10:09:49 +0200

    Talk about file command

--- addons/ai/command/talk/about_file.py
+++ addons/ai/command/talk/about_file.py
@@ -2,6 +2,8 @@ from src.const.globals import COMMAND_TYPE_ADDON
 from src.decorator.command import command
 from src.decorator.option import option
 from typing import TYPE_CHECKING
+from addons.ai.src.assistant.Assistant import Assistant
+from addons.ai.src.model.OpenAiModel import MODEL_NAME_OPEN_AI_GPT_4
 
 if TYPE_CHECKING:
     from src.core.Kernel import Kernel
@@ -10,4 +12,6 @@ if TYPE_CHECKING:
 @command(help="Description", command_type=COMMAND_TYPE_ADDON)
 @option('--file', '-f', type=str, required=True, help="File path")
 def ai__talk__about_file(kernel: "Kernel", file: str) -> None:
-    pass
+    assistant = Assistant(kernel, MODEL_NAME_OPEN_AI_GPT_4)
+
+    assistant.chat_about_file(file)
