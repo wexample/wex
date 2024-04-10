a59e115d97f547da217757a10060bfa430f2357a
weeger <romain.weeger@gmail.com>
2024-04-10 16:51:38 +0200

    Tests

--- addons/ai/tests/command/talk/ask.py
+++ addons/ai/tests/command/talk/ask.py
@@ -1,15 +1,51 @@
 from addons.ai.helper.chat import TEXT_ALIGN_RIGHT, chat_format_message
-from addons.ai.src.assistant.assistant import Assistant
+from addons.ai.src.assistant.assistant import Assistant, ASSISTANT_DEFAULT_COMMANDS, AI_COMMAND_PREFIX, \
+    ASSISTANT_COMMAND_EXIT
 from addons.ai.src.model.ollama_model import MODEL_NAME_OLLAMA_MISTRAL
 from tests.AbstractTestCase import AbstractTestCase
 
 
 class TestAiCommandTalkAsk(AbstractTestCase):
     def test_ask(self) -> None:
-        self._test_ask_formatting()
-        self._test_ask_assistant()
+        assistant = Assistant(self.kernel, MODEL_NAME_OLLAMA_MISTRAL)
 
-    def _test_ask_assistant(self) -> None:
+        self._test_ask_parsing(assistant)
+        self._test_ask_formatting(assistant)
+        self._test_ask_assistant(assistant)
+
+    def _test_ask_parsing(self, assistant: Assistant) -> None:
+        assistant.set_default_subject()
+
+        for command in ASSISTANT_DEFAULT_COMMANDS:
+            # Fail expected
+            if command != ASSISTANT_COMMAND_EXIT:
+                self._found_one_command(assistant, command, f"{command}", False)
+            self._found_one_command(assistant, command, f"Lorem{command}ipsum", False)
+            self._found_one_command(assistant, command, f"Lorem{command}", False)
+            self._found_one_command(assistant, command, f"{command}ipsum", False)
+            self._found_one_command(assistant, command, f"Lorem{AI_COMMAND_PREFIX}{command}ipsum", False)
+            self._found_one_command(assistant, command, f"Lorem{AI_COMMAND_PREFIX}{command}", False)
+            self._found_one_command(assistant, command, f"{AI_COMMAND_PREFIX}{command}ipsum", False)
+
+            # Success expected
+            self._found_one_command(assistant, command, f"{AI_COMMAND_PREFIX}{command}", True)
+            self._found_one_command(assistant, command, f"Lorem {AI_COMMAND_PREFIX}{command}", True)
+            self._found_one_command(assistant, command, f"{AI_COMMAND_PREFIX}{command} ipsum", True)
+            self._found_one_command(assistant, command, f"Lorem {AI_COMMAND_PREFIX}{command} ipsum", True)
+
+    def _found_one_command(
+        self,
+        assistant: Assistant,
+        command: str,
+        text: str,
+        should_success: bool
+    ):
+        user_input_splits = assistant.split_user_input_commands(text)
+        self.kernel.io.log(user_input_splits)
+        success = any(user_input_split["command"] == command for user_input_split in user_input_splits)
+        self.assertTrue(success == should_success, f"Command \"{command}\" found in text : \"{text}\"")
+
+    def _test_ask_assistant(self, assistant: Assistant) -> None:
         assistant = Assistant(self.kernel, MODEL_NAME_OLLAMA_MISTRAL)
 
         self.assertGreater(len(assistant.identities.values()), 1)
@@ -31,7 +67,7 @@ class TestAiCommandTalkAsk(AbstractTestCase):
 
             self.assertTrue(len(chunks) > 0)
 
-    def _test_ask_formatting(self) -> None:
+    def _test_ask_formatting(self, assistant: Assistant) -> None:
         message = chat_format_message(
             "Left align : Lorem ipsum dolor sit amet consecetur blah blah blah blah blah blah blah blah."
         )
