fd936c441e6e77cdf2a5dde0be8f082053253a0e
weeger <romain.weeger@gmail.com>
2024-04-17 09:44:27 +0200

    No fast mode in interactive command response

--- src/core/response/InteractiveShellCommandResponse.py
+++ src/core/response/InteractiveShellCommandResponse.py
@@ -10,8 +10,7 @@ from src.const.types import (
 )
 from src.core.CommandRequest import CommandRequest
 from src.core.response.AbstractResponse import AbstractResponse
-from src.helper.command import command_to_string, execute_command_sync
-from src.helper.process import process_post_exec
+from src.helper.command import execute_command_tree_sync
 
 if TYPE_CHECKING:
     from src.core.Kernel import Kernel
@@ -31,6 +30,7 @@ class InteractiveShellCommandResponse(AbstractResponse):
         self.interactive_data = True
         self.ignore_error = ignore_error
         self.workdir = workdir
+        self.success = None
 
     def render_content(
         self,
@@ -38,31 +38,18 @@ class InteractiveShellCommandResponse(AbstractResponse):
         render_mode: str = KERNEL_RENDER_MODE_TERMINAL,
         args: OptionalCoreCommandArgsDict = None,
     ) -> AbstractResponse:
-        if self.kernel.fast_mode:
-            # When using fast mode, we need to preserve consistency between shell executions.
-            # Ex : ['echo', '"OK"'] should return OK without quotes.
-            # As we don't use shlex to wrap arguments,
-            # we need to enable shell=True here.
+        success, content = execute_command_tree_sync(
+            self.kernel,
+            command_tree=self.shell_command,
+            working_directory=self.workdir,
+            ignore_error=self.ignore_error,
+            interactive=True
+        )
 
-            success, content = execute_command_sync(
-                self.kernel,
-                command_to_string(self.shell_command),
-                working_directory=self.workdir,
-                ignore_error=self.ignore_error,
-                shell=True,
-            )
+        self.success = success
 
-            self.success = success
-
-            # Output data as it was printed in a shell.
-            self.output_bag.append(os.linesep.join(content))
-
-        # Do not add to render bag, but append only once.
-        elif not self.rendered:
-            if self.ignore_error:
-                self.shell_command += ["||", "true"]
-
-            process_post_exec(self.kernel, self.shell_command, self.workdir)
+        # Output data as it was printed in a shell.
+        self.output_bag.append(os.linesep.join(content))
 
         return self
 
