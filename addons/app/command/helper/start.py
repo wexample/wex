3a5893164bfed45bd68969aced5cb2e733734325
weeger <romain.weeger@gmail.com>
2024-04-17 09:43:35 +0200

    Helper start is not enqueued anymore

--- addons/app/command/helper/start.py
+++ addons/app/command/helper/start.py
@@ -1,6 +1,6 @@
 import getpass
 import os.path
-from typing import TYPE_CHECKING, Optional, cast
+from typing import TYPE_CHECKING, Optional, cast, Any
 
 from click import Choice
 
@@ -9,14 +9,8 @@ from addons.app.command.app.init import app__app__init
 from addons.app.command.app.start import app__app__start
 from addons.app.command.app.started import app__app__started
 from addons.app.const.app import HELPER_APPS_LIST
+from src.helper.prompt import prompt_progress_steps
 from src.core.response.AbstractResponse import AbstractResponse
-from src.core.response.queue_collection.AbstractQueuedCollectionResponseQueueManager import (
-    AbstractQueuedCollectionResponseQueueManager,
-)
-from src.core.response.queue_collection.QueuedCollectionStopResponse import (
-    QueuedCollectionStopResponse,
-)
-from src.core.response.QueuedCollectionResponse import QueuedCollectionResponse
 from src.decorator.as_sudo import as_sudo
 from src.decorator.command import command
 from src.decorator.option import option
@@ -59,15 +53,13 @@ def app__helper__start(
     group: Optional[str] = None,
     port: Optional[int] = None,
     port_secure: Optional[int] = None,
-) -> QueuedCollectionResponse:
+) -> str:
     manager: AppAddonManager = cast(AppAddonManager, kernel.addons["app"])
     # App name is same of main service
     helper_service_name = name
     helper_app_path = manager.get_helper_app_path(name, env)
 
-    def _app__helper__start__create(
-        queue: AbstractQueuedCollectionResponseQueueManager,
-    ) -> AbstractResponse | QueuedCollectionStopResponse:
+    def _app__helper__start__create() -> Any:
         nonlocal env
         kernel.io.log("Starting helper app")
 
@@ -80,9 +72,7 @@ def app__helper__start(
                     "app-dir": helper_app_path,
                 },
             ).first():
-                return QueuedCollectionStopResponse(
-                    kernel, "HELPER_APP_ALREADY_STARTED", response=helper_app_path
-                )
+                return False
 
         kernel.io.log(f"Creating helper app dir : {helper_app_path}")
         os.makedirs(helper_app_path, exist_ok=True)
@@ -97,9 +87,7 @@ def app__helper__start(
             },
         )
 
-    def _app__helper__start__checkup(
-        queue: AbstractQueuedCollectionResponseQueueManager,
-    ) -> None:
+    def _app__helper__start__checkup() -> None:
         def _callback() -> None:
             nonlocal user
             nonlocal port
@@ -123,9 +111,7 @@ def app__helper__start(
         if not create_network:
             manager.set_config("docker.create_network", create_network)
 
-    def _app__helper__start__start(
-        queue: AbstractQueuedCollectionResponseQueueManager,
-    ) -> AbstractResponse:
+    def _app__helper__start__start() -> AbstractResponse:
         nonlocal env
 
         return kernel.run_function(
@@ -139,7 +125,7 @@ def app__helper__start(
             },
         )
 
-    return QueuedCollectionResponse(
+    prompt_progress_steps(
         kernel,
         [
             _app__helper__start__create,
@@ -147,3 +133,5 @@ def app__helper__start(
             _app__helper__start__start,
         ],
     )
+
+    return helper_app_path
