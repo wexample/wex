1c82c55cd331279dfa10ee755ea7fe145d3960a0
weeger <romain.weeger@gmail.com>
2024-04-22 12:32:54 +0200

    Progress bar

--- addons/ai/src/assistant/interaction_mode/dir_search_interaction_mode.py
+++ addons/ai/src/assistant/interaction_mode/dir_search_interaction_mode.py
@@ -1,11 +1,13 @@
 import os.path
 from typing import Optional, TYPE_CHECKING, List, cast, Dict
 
+import click
 from langchain_core.document_loaders import BaseLoader  # type: ignore
 
 from addons.ai.src.assistant.interaction_mode.abstract_vector_store_interaction_mode import \
     AbstractVectorStoreInteractionMode
 from addons.ai.src.assistant.utils.user_prompt_section import UserPromptSection
+from src.const.globals import VERBOSITY_LEVEL_QUIET
 from src.helper.file import file_is_utf8_encoding
 
 if TYPE_CHECKING:
@@ -16,6 +18,8 @@ class DirSearchInteractionMode(AbstractVectorStoreInteractionMode):
     def __init__(self, subject: "AbstractChatSubject"):
         super().__init__(subject)
         self.init_vector_store()
+        # Use a temporary trick to not vectorize any file
+        self.last_stored_path: Optional[str] = None
 
     @staticmethod
     def name() -> str:
@@ -44,14 +48,25 @@ class DirSearchInteractionMode(AbstractVectorStoreInteractionMode):
         if not os.path.exists(subject.dir_path):
             return f'Dir does not exists'
 
-        for root, dirs, files in os.walk(subject.dir_path):
-            for file in files:
-                file_path = os.path.join(root, file)
+        if self.last_stored_path != subject.dir_path:
+            storable_files = []
 
-                if file_is_utf8_encoding(file_path):
-                    self.vector_store_file(file_path)
-                else:
-                    self.kernel.io.warn(f"Non UTF-8 encoding detected on file {file_path}")
+            for root, dirs, files in os.walk(subject.dir_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+
+                    if file_is_utf8_encoding(file_path):
+                        storable_files.append(file_path)
+                    else:
+                        self.kernel.io.warn(f"Non UTF-8 encoding detected on file {file_path}")
+
+            verbosity = self.kernel.verbosity
+            self.kernel.verbosity = VERBOSITY_LEVEL_QUIET
+            with click.progressbar(storable_files) as bar:
+                for file in bar:
+                    self.vector_store_file(file)
+            self.kernel.verbosity = verbosity
+            self.last_stored_path = subject.dir_path
 
         # Avoid empty input error.
         if not prompt_section.prompt:
