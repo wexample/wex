930ec1b52e169e28bc8cd656046c6ee130f179e4
weeger <romain.weeger@gmail.com>
2024-04-23 12:52:26 +0200

    XML Output parser

--- addons/ai/src/assistant/interaction_mode/formated_data_interaction_mode.py
+++ addons/ai/src/assistant/interaction_mode/formated_data_interaction_mode.py
@@ -5,10 +5,14 @@ from langchain_core.output_parsers import BaseOutputParser
 from addons.ai.src.assistant.interaction_mode.abstract_vector_store_interaction_mode import AbstractInteractionMode
 from addons.ai.src.assistant.utils.user_prompt_section import UserPromptSection
 
+FORMATED_DATA_FORMAT_COMMA_SEPARATED = 'comma-separated'
 FORMATED_DATA_FORMAT_JSON = 'json'
+FORMATED_DATA_FORMAT_XML = 'xml'
 FORMATED_DATA_FORMAT_YAML = 'yaml'
 FORMATED_DATA_FORMATS = [
+    FORMATED_DATA_FORMAT_COMMA_SEPARATED,
     FORMATED_DATA_FORMAT_JSON,
+    FORMATED_DATA_FORMAT_XML,
     FORMATED_DATA_FORMAT_YAML
 ]
 
@@ -25,9 +29,15 @@ class FormatedDataInteractionMode(AbstractInteractionMode):
                 "No markdown code block.")
 
     def get_output_parser(self, prompt_section: UserPromptSection) -> Optional[BaseOutputParser]:
-        if FORMATED_DATA_FORMAT_JSON in prompt_section.options:
+        if FORMATED_DATA_FORMAT_COMMA_SEPARATED in prompt_section.options:
+            from langchain_core.output_parsers import CommaSeparatedListOutputParser
+            return CommaSeparatedListOutputParser()
+        elif FORMATED_DATA_FORMAT_JSON in prompt_section.options:
             from langchain_core.output_parsers import JsonOutputParser
             return JsonOutputParser()
+        elif FORMATED_DATA_FORMAT_XML in prompt_section.options:
+            from langchain.output_parsers import XMLOutputParser
+            return XMLOutputParser()
         elif FORMATED_DATA_FORMAT_YAML in prompt_section.options:
             from langchain.output_parsers import YamlOutputParser
             from langchain_core.pydantic_v1 import BaseModel, Extra
@@ -43,9 +53,12 @@ class FormatedDataInteractionMode(AbstractInteractionMode):
         prompt_section: UserPromptSection,
         chain_response: Any
     ) -> str:
-        import yaml
 
         if FORMATED_DATA_FORMAT_YAML in prompt_section.options:
+            import yaml
             return yaml.dump(chain_response.dict())
+        elif FORMATED_DATA_FORMAT_XML in prompt_section.options:
+            import dicttoxml
+            return dicttoxml.dicttoxml(chain_response).decode()
 
         return str(chain_response)
