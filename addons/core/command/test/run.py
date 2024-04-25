b5b872c5a66ba52e160041b20da07aca57f03c4c
weeger <romain.weeger@gmail.com>
2024-04-25 10:09:38 +0200

    No more queue in test run

--- addons/core/command/test/run.py
+++ addons/core/command/test/run.py
@@ -11,10 +11,6 @@ from src.const.types import StringsList
 from src.core.response.InteractiveShellCommandResponse import (
     InteractiveShellCommandResponse,
 )
-from src.core.response.QueuedCollectionResponse import (
-    QueuedCollectionResponse,
-    QueuedCollectionResponseCollection,
-)
 from src.core.response.queue_collection.AbstractQueuedCollectionResponseQueueManager import (
     AbstractQueuedCollectionResponseQueueManager,
 )
@@ -24,6 +20,7 @@ from src.decorator.command import command
 from src.decorator.option import option
 from src.helper.command import execute_command_sync
 from src.helper.module import module_load_from_file
+from src.helper.prompt import prompt_progress_steps
 
 if TYPE_CHECKING:
     from src.core.Kernel import Kernel
@@ -44,7 +41,7 @@ if TYPE_CHECKING:
 )
 def core__test__run(
     kernel: "Kernel", command: Optional[str] = None, debug: bool = False
-) -> QueuedCollectionResponse:
+) -> None:
     def _remote_compose(command_part: StringsList) -> InteractiveShellCommandResponse:
         test_env = _app__env__get(
             kernel, kernel.directory.path, key="TEST_REMOTE_ENV", default="pipeline"
@@ -64,9 +61,7 @@ def core__test__run(
             workdir=kernel.directory.path,
         )
 
-    def _start_remote(
-        queue: AbstractQueuedCollectionResponseQueueManager,
-    ) -> InteractiveShellCommandResponse:
+    def _start_remote() -> InteractiveShellCommandResponse:
         return _remote_compose(
             [
                 "up",
@@ -74,9 +69,7 @@ def core__test__run(
             ]
         )
 
-    def _wait_remote(
-        queue: AbstractQueuedCollectionResponseQueueManager,
-    ) -> None:
+    def _wait_remote() -> None:
         success = False
         while not success:
             success, content = execute_command_sync(
@@ -94,19 +87,17 @@ def core__test__run(
             kernel.io.log("Test remote server starting...")
             time.sleep(10)
 
-    def _stop_remote(
-        queue: AbstractQueuedCollectionResponseQueueManager,
-    ) -> InteractiveShellCommandResponse:
+    def _stop_remote() -> InteractiveShellCommandResponse:
         return _remote_compose(
             [
                 "down",
             ]
         )
 
-    def _cleanup(queue: AbstractQueuedCollectionResponseQueueManager) -> None:
+    def _cleanup() -> None:
         kernel.run_function(core__test__cleanup)
 
-    def _run_tests(queue: AbstractQueuedCollectionResponseQueueManager) -> None:
+    def _run_tests() -> None:
         kernel.io.log("Starting test suite..")
 
         loader = unittest.TestLoader()
@@ -149,8 +140,6 @@ def core__test__run(
         if not result.wasSuccessful():
             sys.exit(1)
 
-    steps: QueuedCollectionResponseCollection
-
     # Debug focus on speed.
     if debug:
         steps = [_run_tests]
@@ -164,7 +153,7 @@ def core__test__run(
             _stop_remote,
         ]
 
-    return QueuedCollectionResponse(
+    prompt_progress_steps(
         kernel,
         steps,
     )
