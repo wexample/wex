81674d3b9f259d54b1ed2c1bb292557c54bffe4a
weeger <romain.weeger@gmail.com>
2024-04-30 15:32:35 +0200

    Chat interaction mode

--- addons/ai/src/assistant/interaction_mode/chat_interaction_mode.py
+++ addons/ai/src/assistant/interaction_mode/chat_interaction_mode.py
@@ -14,6 +14,13 @@ class ChatInteractionMode(DefaultInteractionMode):
     def get_initial_prompt(self, prompt_section: UserPromptSection) -> Optional[str]:
         return (
             "Converse with the user like in a human to human chatting."
-            "No unnecessary introductions or explanations. "
+            "Simply respond promptly as a human would if they received a message."
+            "Never say that you are an assistant, or your specific role."
+            "Never offer to assist the user, you are not an assistant, "
+            "you are a human who has his own existence elsewhere."
+            "No unnecessary introductions, explanations, detail or conclusion, juste reply tit for tat, with kindness. "
+            "Never use more than three sentences."
+            "Never use any kind of ordered or unordered lists."
+            "Never detail subjects if user does not ask you to do so."
             "Use simple and concise sentence like in a real conversation, even when you explain something. "
         )
