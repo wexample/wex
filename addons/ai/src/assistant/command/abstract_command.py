3b2e4d509e3a14f26b1c7d2d765226bf59925720
weeger <romain.weeger@gmail.com>
2024-05-02 08:05:12 +0200

    Basic commands

--- addons/ai/src/assistant/command/abstract_command.py
+++ addons/ai/src/assistant/command/abstract_command.py
@@ -1,25 +1,33 @@
 from abc import abstractmethod
-from typing import Type, List, TYPE_CHECKING
+from typing import Type, List, TYPE_CHECKING, Optional
 
 from addons.ai.src.assistant.interaction_mode.abstract_interaction_mode import AbstractInteractionMode
 from addons.ai.src.assistant.interaction_response.abstract_interaction_response import AbstractInteractionResponse
 from addons.ai.src.assistant.interaction_response.null_interaction_response import NullInteractionResponse
+from addons.ai.src.assistant.utils.abstract_assistant_child import AbstractAssistantChild
 
 if TYPE_CHECKING:
     from addons.ai.src.assistant.utils.user_prompt_section import UserPromptSection
 
 
-class AbstractCommand(object):
-    def __init__(self):
-        self.options: List[str] = []
+class AbstractCommand(AbstractAssistantChild):
+    description: str = False
+    sticky: bool = False
+    root_only: str = False
+    options: List[str] = []
 
     @staticmethod
     def name() -> str:
         pass
 
-    def execute(self, prompt_section: "UserPromptSection") -> AbstractInteractionResponse:
+    def execute(self, prompt_section: Optional["UserPromptSection"] = None) -> AbstractInteractionResponse:
         return NullInteractionResponse()
 
     @abstractmethod
     def get_interaction_mode(self, prompt_section: "UserPromptSection") -> Type[AbstractInteractionMode]:
         pass
+
+    def is_active(self, current_prompt: str) -> bool:
+        if self.root_only:
+            return not self.assistant.text_has_a_command(current_prompt)
+        return True
--- addons/ai/src/assistant/command/chat_command.py
+++ addons/ai/src/assistant/command/chat_command.py
@@ -1,17 +1,26 @@
-from typing import Type, TYPE_CHECKING
+from typing import Type, TYPE_CHECKING, Optional
 
 from addons.ai.src.assistant.command.abstract_command import AbstractCommand
 from addons.ai.src.assistant.interaction_mode.abstract_interaction_mode import AbstractInteractionMode
 from addons.ai.src.assistant.interaction_mode.chat_interaction_mode import ChatInteractionMode
+from addons.ai.src.assistant.interaction_response.abstract_interaction_response import AbstractInteractionResponse
+from addons.ai.src.assistant.interaction_response.string_interaction_response import StringInteractionResponse
 
 if TYPE_CHECKING:
     from addons.ai.src.assistant.utils.user_prompt_section import UserPromptSection
 
 
 class ChatCommand(AbstractCommand):
+    description: str = "Short chat response"
+    sticky: bool = True
+    root_only: str = True
+
     @staticmethod
     def name() -> str:
         return "chat"
 
     def get_interaction_mode(self, prompt_section: "UserPromptSection") -> Type[AbstractInteractionMode]:
         return ChatInteractionMode
+
+    def execute(self, prompt_section: Optional["UserPromptSection"] = None) -> AbstractInteractionResponse:
+        return StringInteractionResponse("YAY")
