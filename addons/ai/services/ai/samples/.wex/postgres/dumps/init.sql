c014ab9d3aa8fd45198fe5975b1191d712c27a18
weeger <romain.weeger@gmail.com>
2024-05-05 16:07:30 +0200

    Assistant sql database

--- addons/ai/services/ai/samples/.wex/postgres/dumps/init.sql
+++ addons/ai/services/ai/samples/.wex/postgres/dumps/init.sql
@@ -1,10 +1,17 @@
 DROP TABLE IF EXISTS wex_ai.assistant_conversation_item;
 DROP TABLE IF EXISTS wex_ai.assistant_conversation;
+DROP TABLE IF EXISTS wex_ai.user;
+
+CREATE TABLE wex_ai.user (
+    id SERIAL PRIMARY KEY,
+    password VARCHAR(255)
+);
 
 CREATE TABLE wex_ai.assistant_conversation (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT REFERENCES wex_ai.user(id)
 );
 
 CREATE TABLE wex_ai.assistant_conversation_item (
