15535639ace65d81281f89197d17f483b0fe60f7
weeger <romain.weeger@gmail.com>
2024-06-25 09:37:20 +0200

    Configure for testing

--- .wex/docker/test_remote/test_remote-entrypoint.local.sh
+++ .wex/docker/test_remote/test_remote-entrypoint.local.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Installing local project to test remote"
+source /opt/wex/.wex/python/venv/bin/activate
 bash /opt/wex/cli/install
 
 . /opt/wex/.wex/docker/test_remote/test_remote-entrypoint.sh
