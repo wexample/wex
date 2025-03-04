5cb77a0769c830e325a81425d45ab269e175e636
weeger <romain.weeger@gmail.com>
2025-03-04 22:25:21 +0100

    Test dump

--- __main__.py
+++ __main__.py
@@ -4,14 +4,15 @@
 if __name__ == '__main__':
     try:
         from wexample_wex_core.common.kernel import Kernel
+        from wexample_wex_core.addons.default.default_addon_manager import DefaultAddonManager
 
         (Kernel(
             entrypoint_path=__file__,
-        )
-         .setup()
+            addons=[
+                DefaultAddonManager
+            ])
          .exec_argv())
     except Exception as e:
         from wexample_app.helpers.debug import debug_handle_app_error
 
         debug_handle_app_error(e)
-
