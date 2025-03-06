da09ade24fb029831df15ba1437be29243ba0138
weeger <romain.weeger@gmail.com>
2025-03-06 09:14:51 +0100

    setup

--- __main__.py
+++ __main__.py
@@ -6,11 +6,10 @@ if __name__ == '__main__':
         from wexample_wex_core.common.kernel import Kernel
         from wexample_wex_core.addons.default.default_addon_manager import DefaultAddonManager
 
-        (Kernel(
-            entrypoint_path=__file__,
-            addons=[
-                DefaultAddonManager
-            ])
+        (Kernel(entrypoint_path=__file__)
+         .setup(addons=[
+            DefaultAddonManager
+        ])
          .exec_argv())
     except Exception as e:
         from wexample_app.helpers.debug import debug_handle_app_error
