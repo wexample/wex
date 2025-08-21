011dc735994a20f031c91596cb0ef4b37643e9d5
weeger <romain.weeger@gmail.com>
2025-08-21 19:11:29 +0200

    Rectify

--- .wex/python/files_state/wex_workdir.py
+++ .wex/python/files_state/wex_workdir.py
@@ -1,5 +1,30 @@
+from typing import Optional
+
+from wexample_config.const.types import DictConfig
 from wexample_wex_addon_dev_python.workdir.python_workdir import PythonWorkdir
 
 
 class WexWorkdir(PythonWorkdir):
-    pass
+    def prepare_value(self, raw_value: Optional[DictConfig] = None) -> DictConfig:
+        from wexample_filestate.const.disk import DiskItemType
+
+        raw_value = super().prepare_value(
+            raw_value=raw_value
+        )
+
+        children = raw_value["children"]
+        children.extend(
+            [
+                {
+                    "name": "src",
+                    "type": DiskItemType.DIRECTORY,
+                    "should_exist": True,
+                    "children": [
+                        self._create_init_children_factory(),
+                        self._create_python_file_children_filter(),
+                    ],
+                }
+            ]
+        )
+
+        return raw_value
