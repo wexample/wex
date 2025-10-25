47301ac830662fd18b847e6266b2529bfedc5e2c
weeger <romain.weeger@gmail.com>
2025-10-25 22:27:18 +0200

    Rect

--- src/helper/module.py
+++ src/helper/module.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
-from typing import cast
-from typing import TYPE_CHECKING
+
+from typing import TYPE_CHECKING, cast
 
 if TYPE_CHECKING:
     from importlib.abc import Loader
 
 
 def module_load_from_file(file_path: str, module_name: str) -> Loader:
-    from importlib.abc import Loader
     from importlib import util
+    from importlib.abc import Loader
     from importlib.machinery import ModuleSpec
 
     spec = util.spec_from_file_location(module_name, file_path)
