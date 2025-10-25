049a8cb7629678d1f4f72d60a4d90bbce3171f90
weeger <romain.weeger@gmail.com>
2025-10-25 22:27:10 +0200

    Rect

--- src/helper/module.py
+++ src/helper/module.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import cast
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from importlib.abc import Loader
 
@@ -9,6 +10,7 @@ def module_load_from_file(file_path: str, module_name: str) -> Loader:
     from importlib.abc import Loader
     from importlib import util
     from importlib.machinery import ModuleSpec
+
     spec = util.spec_from_file_location(module_name, file_path)
     assert isinstance(spec, ModuleSpec)
     module = util.module_from_spec(spec)
