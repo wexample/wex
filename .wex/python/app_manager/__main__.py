dc4cc8c1aad395c043ce4a7ceaed0d23696e5da3
weeger <romain.weeger@gmail.com>
2025-10-25 21:39:41 +0200

    Rect

---
+++ .wex/python/app_manager/__main__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Main entry point for the application."""
+from wexample_wex_core.common.app_manager_kernel import AppManagerKernel
+
+if __name__ == "__main__":
+    try:
+        from wexample_wex_addon_dev_python.python_addon_manager import (
+            PythonAddonManager,
+        )
+        from wexample_wex_core.addons.default.default_addon_manager import (
+            DefaultAddonManager,
+        )
+        from wexample_wex_core.common.kernel import Kernel
+        from wexample_wex_addon_filestate.filestate_addon_manager import FilestateAddonManager
+        from wexample_wex_addon_app.app_addon_manager import AppAddonManager
+
+        (
+            AppManagerKernel(
+                entrypoint_path=__file__,
+            )
+            .setup(addons=[
+                AppAddonManager,
+                DefaultAddonManager,
+            ])
+            .exec_argv()
+        )
+    except Exception as e:
+        from wexample_app.helpers.debug import debug_handle_app_error
+
+        debug_handle_app_error(e)
---
+++ .wex/python/app_manager/app_workdir.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from wexample_wex_addon_dev_python.workdir.python_package_workdir import PythonPackageWorkdir
+
+
+class AppWorkdir(PythonPackageWorkdir):
+    pass
